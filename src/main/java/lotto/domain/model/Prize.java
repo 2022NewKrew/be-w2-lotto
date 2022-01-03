64ed47795e85a6532fbef0f7b33e1d713de7f34c
kusakina0608 <platina.kusakina@gmail.com>
2022-01-03 18:39:07 +0900

    Prize 열거체 추가

---
+++ src/main/java/lotto/domain/model/Prize.java
@@ -0,0 +1,33 @@
+package lotto.domain.model;
+
+import java.util.Comparator;
+
+public enum Prize {
+    DIV4(4, 3, 5000),
+    DIV3(3, 4, 50000),
+    DIV2(2, 5, 1500000),
+    DIV1(1, 6, 2000000000);
+
+    private final int divisions;
+    private final int numberOfMatches;
+    private final int winnings;
+
+    Prize(int divisions, int numberOfMatches, int winnings) {
+        this.divisions = divisions;
+        this.numberOfMatches = numberOfMatches;
+        this.winnings = winnings;
+    }
+
+    public int getDivisions() {
+        return divisions;
+    }
+
+    public int getNumberOfMatches() {
+        return numberOfMatches;
+    }
+
+    public int getWinnings() {
+        return winnings;
+    }
+}
+
