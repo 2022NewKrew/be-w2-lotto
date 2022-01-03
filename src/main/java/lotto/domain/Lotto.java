b56020e9ad64f73f2d0d1c64964e981dfaf5b47d
jun.dev <jun.dev@kakaocorp.com>
2022-01-03 11:44:18 +0900

    feat: add createLotto
    creating random lotto values using Collections.shuffle

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,30 @@
 package lotto.domain;
 
-public class Lotto {
-    private
-    public Lotto(Long payment){
+import java.text.CollationElementIterator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import static lotto.domain.LottoSetting.*;
+
+public class Lotto {
+    private List<LottoNumber> lottos;
+
+    public Lotto(Integer payment){
+        Integer lottoCount = (payment / LOTTO_PRICE);
+
+        for(int i = 0 ; i < lottoCount ; i++){
+            lottos.add(createLotto());
+        }
+    }
+
+
+    private LottoNumber createLotto(){
+        List<Integer> newLotto = new ArrayList<>();
+        for(int i = 1 ; i <= 45 ; i++){
+            newLotto.add(i);
+        }
+        Collections.shuffle(newLotto);
+        return new LottoNumber(newLotto.subList(0, LOTTO_LENGTH - 1));
     }
 }
--- src/main/java/lotto/domain/LottoSetting.java
+++ src/main/java/lotto/domain/LottoSetting.java
@@ -1,6 +1,12 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class LottoSetting {
-    final static public Integer PRICE = 1000; //each price of lotto
+    final static public Integer LOTTO_PRICE = 1000; //each price of lotto
     final static public Integer LOTTO_LENGTH = 6; //each lotto length
+    final static public List<Integer> LOTTO_NUMBER_ELEMENT = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10) ); //45까지 한번에 초기화 못할까..?
 }
