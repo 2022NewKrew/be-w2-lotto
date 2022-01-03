cc3b031b835652da53323f10124c475e0aa02332
green.day <green.day@kakaocorp.com>
2022-01-03 11:25:30 +0900

    feat: main클래스 구현, InputView를 통해서 금액 입력받기

---
+++ src/main/java/LottoMain.java
@@ -0,0 +1,9 @@
+import view.InputView;
+
+public class LottoMain {
+    public static void main(String[] args) {
+        int money = InputView.getMoney();
+
+        System.out.println(money);
+    }
+}
