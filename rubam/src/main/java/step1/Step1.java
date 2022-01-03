80b3db3e415119c9de0593b933f55a33f7657ac1
wooky9633 <wooky9633@naver.com>
2022-01-04 04:05:09 +0900

    rubam lotto Step1 impl (#76)
    * rubam step1 lotto impl
    
    * add gradle setting files
    
    Co-authored-by: rubam.kong <rubam.kong@kakaocorp.com>

---
+++ rubam/src/main/java/step1/Step1.java
@@ -0,0 +1,11 @@
+package step1;
+
+import step1.domain.LottoAdmin;
+
+public class Step1 {
+
+    public static void main(String[] args) {
+        LottoAdmin.initLotto();
+        LottoAdmin.LottoResult();
+    }
+}
