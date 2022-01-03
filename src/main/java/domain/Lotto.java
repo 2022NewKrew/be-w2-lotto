2c00ea0cf58bc83cf8d1e790058112409509ab3b
benlee73 <55429965+benlee73@users.noreply.github.com>
2022-01-03 22:10:43 +0900

    로또 1단계 구현 (#55)
    * Chore : first commit
    
    * Add : 구입금액 입력 메서드 구현
    
    * Add : LottoPaper, Lotto 클래스 생성 / 금액에 맞춰 로또 자동 생성
    
    * Feature : 로또 갯수 및 번호 출력
    
    * Feature : 당첨 번호 입력 받기
    
    * Style : public -> private
    
    * Feature : 당첨 번호 입력 및 당첨 결과 확인 기능 추가
    
    * Chore : gitignore 실험
    
    * Chore : Document 포맷
    
    * Chore : 주석 추가
    
    Co-authored-by: ben.ny <ben.ny@kakaocorp.com>

---
+++ src/main/java/domain/Lotto.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Lotto {
+    private static final List<Integer> BALLS = Stream.iterate(1, n -> n + 1)
+            .limit(45)
+            .collect(Collectors.toList());
+    private final ArrayList<Integer> numbers;
+
+    public Lotto() {
+        Collections.shuffle(BALLS);
+        numbers = new ArrayList<>(BALLS.subList(0, 6));
+        Collections.sort(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return numbers.stream()
+                .map(Objects::toString)
+                .collect(Collectors.joining(", "));
+    }
+
+    public int sameWithWinningNum(List<Integer> winningNum) {
+        return (int) numbers.stream()
+                .filter(winningNum::contains)
+                .count();
+    }
+}
---
+++ src/main/java/view/LottoUI.java
@@ -0,0 +1,47 @@
+package view;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class LottoUI {
+    private static final String HOW_MONEY = "구입금액을 입력해 주세요.";
+    private static final String BUY_RESULT = "%d개를 구매했습니다.\n";
+    private static final String LAST_WINNING_NUM = "\n지난 주 당첨 번호를 입력해주세요.";
+    private static final String WINNING_RESULT = "당첨 통계\n---------\n3개 일치 (5000원)- %d개\n4개 일치 (50000원)- %d개\n5개 일치 (1500000원)- %d개\n6개 일치 (2000000000원)- %d개\n총 수익률은 %d%%입니다.";
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static long inputMoney() {
+        System.out.println(HOW_MONEY);
+        long money;
+        try {
+            String input = scanner.nextLine();
+            money = Long.parseLong(input);
+        } catch (Exception e) {
+            money = 0;
+        }
+        return money;
+    }
+
+    public static List<Integer> inputWinningNum() {
+        System.out.println(LAST_WINNING_NUM);
+        String input = scanner.nextLine();
+
+        String replace = input.replace(" ", "");
+
+        return Arrays.stream(replace.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public static void outputLotto(int count, String lotto) {
+        System.out.printf(BUY_RESULT, count);
+        System.out.println(lotto);
+    }
+
+    public static void outputWinningResult(HashMap<Integer, Integer> winningResult, int winRate) {
+        System.out.printf(WINNING_RESULT, winningResult.get(3), winningResult.get(4), winningResult.get(5), winningResult.get(6), winRate);
+    }
+}
