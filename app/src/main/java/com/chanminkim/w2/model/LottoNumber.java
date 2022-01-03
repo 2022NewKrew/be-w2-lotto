3015d0143110fbd2fa633626c5e0fe921b8e22fe
charlie.p1 <charlie.p1@kakaocorp.com>
2022-01-03 17:03:50 +0900

    Implement LottoNumber and LottoNumberTest

--- app/src/main/java/com/chanminkim/w2/model/LottoNumber.java
+++ app/src/main/java/com/chanminkim/w2/model/LottoNumber.java
@@ -1,4 +1,21 @@
 package com.chanminkim.w2.model;
 
+import com.chanminkim.w2.exception.InvalidLottoNumberException;
+import com.google.common.collect.Range;
+
 public class LottoNumber {
+    private static final Range<Integer> LOTTO_NUMBER_RANGE = Range.closed(1, 45);
+
+    private final int lottoNumber;
+
+    public LottoNumber(int lottoNumber) {
+        if (!LOTTO_NUMBER_RANGE.contains(lottoNumber)) {
+            throw new InvalidLottoNumberException(lottoNumber);
+        }
+        this.lottoNumber = lottoNumber;
+    }
+
+    public int getLottoNumber() {
+        return lottoNumber;
+    }
 }
