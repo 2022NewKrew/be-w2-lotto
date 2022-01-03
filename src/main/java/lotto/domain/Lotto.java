bfd6dc147ba5baa308892606c5ac944d1105189c
jun.dev <jun.dev@kakaocorp.com>
2022-01-03 12:10:10 +0900

    feat: add output message which print the number of lotto bought

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.view.LottoViewOutput;
+
 import java.text.CollationElementIterator;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -9,16 +11,25 @@ import static lotto.domain.LottoSetting.*;
 
 public class Lotto {
     private List<LottoNumber> lottos;
+    private LottoViewOutput lottoViewOutput;
 
     public Lotto(Integer payment){
         lottos = new ArrayList<>();
+        lottoViewOutput = new LottoViewOutput(this);
 
         Integer lottoCount = (payment / LOTTO_PRICE);
 
         for(int i = 0 ; i < lottoCount ; i++){
             lottos.add(createLotto());
         }
+        lottoViewOutput.printLottoCount();
     }
+
+    //getter
+    public List<LottoNumber> getLottos() {
+        return lottos;
+    }
+
 
 
     private LottoNumber createLotto(){
--- src/main/java/lotto/view/LottoViewOutput.java
+++ src/main/java/lotto/view/LottoViewOutput.java
@@ -1,4 +1,14 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
 public class LottoViewOutput {
+    Lotto lottoObject;
+    public LottoViewOutput(Lotto lottoObject){
+        this.lottoObject = lottoObject;
+    }
+
+    public void printLottoCount(){
+        System.out.println(lottoObject.getLottos().size() + "개를 구매했습니다.");
+    }
 }
