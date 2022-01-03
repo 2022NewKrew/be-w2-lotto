2ac340766cf845624b3b6ab737f40cabce55aeff
unknown <cih468@naver.com>
2022-01-03 15:41:47 +0900

    main에 구입 금액 입력받아 로또 구입 수량 출력하는 부분까지 생성

--- src/main/Java/lotto/Main.java
+++ src/main/Java/lotto/Main.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.com.kakao.LottoBundle;
+import lotto.controller.ConsoleInput;
+import lotto.view.ConsoleOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+            int lottoPurchaseMoney = ConsoleInput.getLottoPurchaseMoney();
+
+            LottoBundle lottoBundle = new LottoBundle(lottoPurchaseMoney);
+
+            ConsoleOutput.printLottoCount(lottoBundle.getCount());
     }
 }
