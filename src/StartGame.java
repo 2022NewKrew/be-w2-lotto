050436e0948929eeb7ff1fd1d7dcad57770ee5d1
Jaden <hhj801@gmail.com>
2022-01-04 04:05:17 +0900

    Step1 : 로또 1단계 요구사항 구현 (#86)
    - 구입 금액에 맞춰 로또 생성 후
    - 당첨 번호를 입력받아 매치되는 숫자 체크, 그에 맞는 상금 부여
    - 총 상금을 기반으로 수익률 계산
    - 로또 한장은 Lotto, 로또 게임은 LottoGame, UI는 UIView 클래스로 나누었음
    
    Co-authored-by: jaden <jaden.dev@kakaocorp.com>

---
+++ src/StartGame.java
@@ -0,0 +1,7 @@
+public class StartGame {
+    public static void main(String[] args) {
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.startLottoGame();
+        lottoGame.runLottoGame();
+    }
+}
