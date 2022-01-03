b2efbe3ee224c78e1098f6770959f582a7783deb
reynold-yoon <reynold.yoon@kakaocorp.com>
2022-01-03 20:36:40 +0900

    Add Feature: 로또 1단계 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,31 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static Scanner sc = new Scanner(System.in);
+
+    public static int inputTotalPrice() {
+        System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
+        int totalPrice = sc.nextInt();
+        sc.nextLine();
+        return totalPrice;
+    }
+
+    public static List<Integer> inputWinningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+        String inputString = sc.nextLine();
+        String[] splitStrings = inputString.split(", ");
+        System.out.println();
+
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String splitString : splitStrings) {
+            winningNumbers.add(Integer.valueOf(splitString));
+        }
+        return winningNumbers;
+    }
+}
