8ecc46a738e548dfbd46da6e55a8af6845ce212b
myles.nah <myles.nah@kakaocorp.com>
2022-01-03 18:47:03 +0900

    feat: 테스트 코드 추가

--- src/test/java/step1/domain/model/ResultTest.java
+++ src/test/java/step1/domain/model/ResultTest.java
@@ -1,4 +1,23 @@
+package step1.domain.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step1.exceptions.EmptyInputException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ResultTest {
-  
+    @DisplayName("결과 입력에 빈 칸만 입력 시 오류 확인")
+    @Test
+    public void resultWrongInputTestWithOnlyBlanks() {
+        String resultsStr = "        ";
+        assertThrows(EmptyInputException.class, () -> new Result(resultsStr));
+    }
+
+    @DisplayName("결과 입력에 ,만 입력 시 오류 확인")
+    @Test
+    public void resultWrongInputTestWithOnlyCommas() {
+        String resultsStr = ",,,,,";
+        assertThrows(EmptyInputException.class, () -> new Result(resultsStr));
+    }
 }
