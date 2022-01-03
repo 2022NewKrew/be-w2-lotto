c28353d4fabe179a3c33021ff1ab2d9d06488d12
faust.like <faust.like@kakaocorp.com>
2022-01-03 19:02:53 +0900

    feat: 출력부분 구현
    1. 단순히 출력만 할 수 있도록 OutputView 구현
    2. 각각의 복권 라인을 출력하는 메소드 구현
    3. 결과를 출력하는 3개의 메소드 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,17 @@
 import view.InputView;
+import view.OutputView;
 
 public class Main {
+    private static final int FIRST = 2000000000;
+    private static final int SECOND = 1500000;
+    private static final int THIRD = 50000;
+    private static final int FORTH = 5000;
+
     public static void main(String[] args) {
         int a = InputView.getNumLotto();
-        System.out.println(InputView.getWinNumber().toString());
+        OutputView.printLottoLine(InputView.getWinNumber());
+        OutputView.printPreResult();
+        OutputView.printResult(1, FIRST, 3);
+        OutputView.printYield(30);
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,25 @@
 package view;
 
+import java.util.List;
+
 public class OutputView {
+    private OutputView() {
+    }
+
+    public static void printLottoLine(List<Integer> lottoLine) {
+        System.out.println(lottoLine.toString());
+    }
+
+    public static void printPreResult() {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+    }
+
+    public static void printResult(int matchNum, int price, int lineNum) {
+        System.out.printf("%d개 일치 (%d원)- %d개\n", matchNum, price, lineNum);
+    }
+
+    public static void printYield(int yield) {
+        System.out.printf("총 수익률은 %d%%입니다.", yield);
+    }
 }
