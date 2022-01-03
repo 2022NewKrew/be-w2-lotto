16948a837f82310dd70494a7571b3e0387d1117e
Gunyoung-Kim <gun025bba@naver.com>
2022-01-03 18:12:25 +0900

    Result 클래스에 대한 테스트 코드 추가

--- app/src/test/java/be/w2/lotto/lottos/LottoTest.java
+++ app/src/test/java/be/w2/lotto/lottos/LottoTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
