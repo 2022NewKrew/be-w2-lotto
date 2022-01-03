b1d3e504475fc3f4a93009cf11f7c079016f6780
faust.like <faust.like@kakaocorp.com>
2022-01-03 18:34:12 +0900

    feat: InputView 구현
    1. 얼마를 지불할 것인지를 입력받는 부분 구현
    	1-1. 하지만 아직 예외처리가 안되어있다.
    2. 로또 당첨 리스트 구현
    	2-1. 예외처리까지 구현
    	2-2. 하지만 예외처리를 InputView에서 처리하는게 맞지 않아보임.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import view.InputView;
+
 public class Main {
-    public static void main(String[] args) { {
-        System.out.println("called");
+    public static void main(String[] args) {
+        int a = InputView.getNumLotto();
+        System.out.println(InputView.getWinNumber().toString());
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,55 @@
 package view;
 
+import java.util.*;
+
 public class InputView {
+    private static final Scanner sc = new Scanner(System.in);
+    private static final int PRICE = 1000;
+    private static final String SEPARATOR = ",";
+
+    private InputView() {
+    }
+
+    public static int getNumLotto() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int payAmount = sc.nextInt();
+        System.out.print(payAmount / PRICE);
+        System.out.println("개를 구매했습니다.");
+
+        return payAmount / PRICE;
+    }
+
+    public static List<Integer> getWinNumber() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        String WinNumbers = sc.next();
+        List<String> splitList = new ArrayList<>(Arrays.asList(WinNumbers.split(SEPARATOR)));
+
+        if (splitList.size() != 6) {
+            System.out.println("1부터 45까지의 숫자 6개를 ','로 나누어서 입력해주세요.");
+            return getWinNumber();
+        }
+
+        return convert2IntAndVerify(splitList);
+    }
+
+    private static List<Integer> convert2IntAndVerify(List<String> strList) {
+        List<Integer> retList = new ArrayList<>();
+
+        for (String str : strList) {
+            retList.add(Integer.valueOf(str));
+        }
+
+        return sortAndVerify(retList);
+    }
+
+    private static List<Integer> sortAndVerify(List<Integer> srcList) {
+        Collections.sort(srcList);
+
+        if (srcList.get(0) < 1 || srcList.get(5) > 45) {
+            System.out.println("1부터 45까지의 숫자 6개를 ','로 나누어서 입력해주세요.");
+            return getWinNumber();
+        }
+
+        return srcList;
+    }
 }
