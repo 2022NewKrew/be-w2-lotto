cc01cf94a2104ce3ff6e799ce5b348c6cd4ee2d1
kusakina0608 <platina.kusakina@gmail.com>
2022-01-03 18:20:20 +0900

    테스트 클래스 추가

--- src/test/java/util/CustomScannerTests.java
+++ src/test/java/util/CustomScannerTests.java
@@ -1,4 +1,37 @@
 package util;
 
+import lotto.util.CustomScanner;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 public class CustomScannerTests {
+    static InputStream sysInBackup = System.in;
+
+    @AfterAll
+    static void afterAll() {
+        System.setIn(sysInBackup);
+    }
+
+    @Test
+    void getMoneyTest() {
+        ByteArrayInputStream in = new ByteArrayInputStream("14000".getBytes());
+        System.setIn(in);
+
+        CustomScanner customScanner = new CustomScanner();
+
+        customScanner.getMoney();
+    }
+
+    @Test
+    void getWinningNumbersTest() {
+        ByteArrayInputStream in = new ByteArrayInputStream("1, 2, 3, 4, 5, 6".getBytes());
+        System.setIn(in);
+
+        CustomScanner customScanner = new CustomScanner();
+
+        customScanner.getWinningNumbers();
+    }
 }
