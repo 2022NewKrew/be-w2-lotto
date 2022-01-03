b52c01254400bbd61770b4f4f163ef4c376e5f6c
sukstar76 <sukstar76@gmail.com>
2022-01-03 19:15:25 +0900

    lotto result 수정
    rank enum 추가에 의한 수정

--- src/main/java/domain/LottoResult.java
+++ src/main/java/domain/LottoResult.java
@@ -1,11 +1,35 @@
 package domain;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoResult {
     private List<Integer> numbers;
+    private Map<Integer, Integer> countOfMatches;
+    private double profitRate;
 
     public LottoResult(List<Integer> numbers) {
         this.numbers = numbers;
+    }
+
+    public void setProfitRate(double profitRate) {
+        this.profitRate = profitRate;
+    }
+
+    public void setCountOfMatches(Map<Integer, Integer> countOfMatches) {
+        this.countOfMatches = countOfMatches;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public Map<Integer, Integer> getCountOfMatches() {
+        return countOfMatches;
+    }
+
+    public double getProfitRate() {
+        return profitRate;
     }
 }
