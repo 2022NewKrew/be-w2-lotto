8c81795b8612f87d29e74829ca83cb3784c4f8f1
kusakina0608 <platina.kusakina@gmail.com>
2022-01-03 18:19:27 +0900

    Application 클래스 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import lotto.manager.LottoManager;
+
 public class Application {
     public static void main(String[] args) {
-        
+        LottoManager.getInstance().run();
     }
 }
