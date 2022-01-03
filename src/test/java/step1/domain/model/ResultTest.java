0a31cf6315959c5dad5d4358fdfc4575d28d56af
myles.nah <myles.nah@kakaocorp.com>
2022-01-03 18:52:27 +0900

    feat: 단위 테스트 추가

--- src/test/java/step1/domain/model/ResultTest.java
+++ src/test/java/step1/domain/model/ResultTest.java
@@ -2,6 +2,7 @@ package step1.domain.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step1.exceptions.DifferentSizeException;
 import step1.exceptions.EmptyInputException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,5 +20,12 @@ class ResultTest {
     public void resultWrongInputTestWithOnlyCommas() {
         String resultsStr = ",,,,,";
         assertThrows(EmptyInputException.class, () -> new Result(resultsStr));
+    }
+
+    @DisplayName("결과 입력에 당첨 번호 개수와 다르게 입력 시 오류 확인")
+    @Test
+    public void resultWrongInputTestWithDifferentSize() {
+        String resultsStr = "1,2,3,3,23,45\n";
+        assertThrows(DifferentSizeException.class, () -> new Result(resultsStr));
     }
 }
