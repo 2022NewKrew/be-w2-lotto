6be895ce02c62b70d3742abe495d773cb144cee7
green.day <green.day@kakaocorp.com>
2022-01-03 18:03:16 +0900

    feat: 당첨 번호를 입력받을 수 있도록 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,26 @@
 package view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String COMMA = ",";
 
     public static int getMoney(){
         System.out.println("구입금액을 입력해 주세요.");
+
         return scanner.nextInt();
+    }
+
+    public static List<Integer> getWinningNumbers(){
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        String winnigNumbers = scanner.next();
+
+        return Arrays.stream(winnigNumbers.split(COMMA))
+                .map(Integer::parseInt)
+                .collect(Collectors.toUnmodifiableList());
     }
 }
