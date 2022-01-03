6913ef0ef9bc128fef1c65051c8e76e8fb3804b5
devwihtpug <devwithpug@kakao.com>
2022-01-03 18:46:54 +0900

    step-1: 로또 생성 로직 변경 (Random -> shuffle)

--- src/main/java/domain/LottoGenerator.java
+++ src/main/java/domain/LottoGenerator.java
@@ -1,41 +1,35 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoGenerator {
 
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final int MAX_LOTTO_COUNT = 6;
-    private static final Random random = new Random(System.currentTimeMillis());
 
     public static List<Lotto> generate(LottoGameInfo lottoGameInfo) {
         List<Lotto> lottoList = new ArrayList<>();
+        List<Integer> lottoNumbers = IntStream.rangeClosed(1, MAX_LOTTO_NUMBER)
+                .boxed()
+                .collect(Collectors.toList());
 
         for (int currentLotto = 0; currentLotto < lottoGameInfo.getPurchasedQuantity(); currentLotto++) {
-            Lotto lotto = createNewLotto();
+            Lotto lotto = createNewLotto(lottoNumbers);
             lottoList.add(lotto);
         }
         return lottoList;
     }
 
-    private static Lotto createNewLotto() {
-        List<Integer> numbers = new ArrayList<>();
-
-        while (numbers.size() < MAX_LOTTO_COUNT) {
-            addNextLottoNumber(numbers);
-        }
+    private static Lotto createNewLotto(List<Integer> lottoNumbers) {
+        Collections.shuffle(lottoNumbers);
+        List<Integer> numbers = new ArrayList<>(lottoNumbers.subList(0, MAX_LOTTO_COUNT));
         numbers.sort(Integer::compareTo);
 
         return new Lotto(numbers);
-    }
-
-    private static void addNextLottoNumber(List<Integer> numbers) {
-        int num = random.nextInt(MAX_LOTTO_NUMBER) + 1;
-        if (!numbers.contains(num)) {
-            numbers.add(num);
-        }
     }
 
 }
