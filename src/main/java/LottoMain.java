c8775dd21e9d6745b5ebeac0760323edc8510060
ben.ny <ben.ny@kakaocorp.com>
2022-01-03 11:12:46 +0900

    Add : 구입금액 입력 메서드 구현

---
+++ src/main/java/LottoMain.java
@@ -0,0 +1,13 @@
+import view.LottoUI;
+
+public class LottoMain {
+    public static void main(String[] args) {
+        long money = LottoUI.inputMoney();
+
+        // 금액을 넘기면 금액 만큼 로토 번호 생성
+        // UI로 생성한 로토 번호 출력
+
+        // 당첨번호 입력하면 arraylist 반환해서 저장
+        // 당첨 통계 계산 후 출력
+    }
+}
---
+++ src/main/java/view/LottoUI.java
@@ -0,0 +1,23 @@
+package view;
+
+import java.util.Scanner;
+
+public class LottoUI {
+    public static final String HOW_MONEY = "구입금액을 입력해 주세요.";
+    public static final String BUY_RESULT = "개를 구매했습니다.";
+    public static final String LAST_WINNING_NUM = "지난 주 당첨 번호를 입력해주세요.";
+    public static final String WINNING_RESULT = "당첨 통계\n---------\n3개 일치 (5000원)- %d개\n4개 일치 (50000원)- %d개\n5개 일치 (1500000원)- %d개\n6개 일치 (2000000000원)- %d개\n총 수익률은 %d%%입니다.";
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static long inputMoney(){
+        System.out.println(HOW_MONEY);
+        return scanner.nextLong();
+    }
+
+    // 구입 결과
+
+    // 지난 주 당첨 번호
+
+    // 당첨 통계
+
+}
