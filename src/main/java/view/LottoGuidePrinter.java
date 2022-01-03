75b637043122ab9fe19fa80249824112bff07342
binary.yun <binary.yun@kakaocorp.com>
2022-01-03 20:08:09 +0900

    refactor: 문자열 상수로 변환

--- src/main/java/view/LottoGuidePrinter.java
+++ src/main/java/view/LottoGuidePrinter.java
@@ -1,13 +1,14 @@
 package view;
 
 public class LottoGuidePrinter {
-    public static final String PURCHASE_AMOUNT_REQUEST = "구입 금액을 입력해 주세요.";
+    private static final String PURCHASE_AMOUNT_REQUEST = "구입 금액을 입력해 주세요.";
+    private static final String PURCHASE_QUANTITY = "개를 구매했습니다.";
 
     public static void requestPurchaseAmountInput() {
         System.out.println(PURCHASE_AMOUNT_REQUEST);
     }
 
     public static void alertPurchaseQuantity(int quantity) {
-        System.out.println(quantity + "개를 구매했습니다.");
+        System.out.println(quantity + PURCHASE_QUANTITY);
     }
 }
