072044ce68db98508977df48d88f04605195c5d3
yuchanleeme <yuchanlee.me@gmail.com>
2022-01-03 18:31:38 +0900

    feat: 로또 최종 결과 출력

--- src/main/java/lotto/view/LottoGameOutput.java
+++ src/main/java/lotto/view/LottoGameOutput.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
+import lotto.domain.LottoResult;
 import lotto.domain.PlayerLottoList;
+
+import java.util.List;
 
 public class LottoGameOutput {
 
@@ -8,4 +11,16 @@ public class LottoGameOutput {
         System.out.println(numberOfLotto + "개를 구매했습니다.");
         playerLottoList.getLottoList().forEach(lotto -> System.out.println(lotto.getNumbers()));
     }
+
+    public static void printLottoResults(List<LottoResult> lottoResults) {
+        lottoResults.forEach(LottoGameOutput::printOneLottoResult);
+    }
+
+    public static void printOneLottoResult(LottoResult lottoResult) {
+        String sb = lottoResult.matchingCounts + "개 일치 " +
+                "(" + lottoResult.reward + ")- " +
+                lottoResult.numberOfMatchingLotto + "개";
+        System.out.println(sb);
+    }
 }
+
