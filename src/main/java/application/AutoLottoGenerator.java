d77338ec7c50224764ce43bfcf29f9b9ff792517
binary.yun <binary.yun@kakaocorp.com>
2022-01-04 00:19:18 +0900

    refactor: 자동 로또 생성 로직의 매직넘버를 static 필드로 변경

--- src/main/java/application/AutoLottoGenerator.java
+++ src/main/java/application/AutoLottoGenerator.java
@@ -8,14 +8,14 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AutoLottoGenerator implements LottoGenerator {
-    private static final List<Integer> numbers = IntStream.range(1, 45).boxed().collect(Collectors.toList());
+    private static final List<Integer> numbers = IntStream.range(Lotto.MIN_LOTTO_NUMBER, Lotto.MAX_LOTTO_NUMBER + 1).boxed().collect(Collectors.toList());
 
     @Override
     public Lotto getLotto() {
         Collections.shuffle(numbers);
         return new Lotto(numbers.stream()
                 .distinct()
-                .limit(6)
+                .limit(Lotto.NUMBER)
                 .collect(Collectors.toList()));
     }
 }
