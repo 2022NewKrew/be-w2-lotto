7827dcec58ad5d9d17410516df5717b404fb6ae1
edward.baek <edward.baek@kakaocorp.com>
2022-01-03 19:18:39 +0900

    feat : 문자열 파싱하는 유틸 클래스 작성

---
+++ app/src/main/java/lotto/util/StringParsingUtil.java
@@ -0,0 +1,17 @@
+package lotto.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringParsingUtil {
+
+    private StringParsingUtil() {
+    }
+
+    public static List<String> parse(String stringList, String split) {
+        String[] list = stringList.split(split);
+        return Arrays.stream(list)
+                .collect(Collectors.toList());
+    }
+}
