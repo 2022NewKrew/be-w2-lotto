37b3834c91c7200375187e757a513b353c01a7d3
worldbright <worldbright0@gmail.com>
2022-01-03 18:34:54 +0900

    Step1 : add validation of lotto number validation if lotto number is digit

--- src/main/java/dto/LottoDTO.java
+++ src/main/java/dto/LottoDTO.java
@@ -14,13 +14,28 @@ public class LottoDTO {
     public LottoDTO() {
         List<Integer> numberForShuffle = new ArrayList<>(INIT_LOTTO);
         Collections.shuffle(numberForShuffle);
-
+        // 1~45의 숫자를 랜덤순서로 섞는다
         number = numberForShuffle.subList(0, 6);
+        // 랜덤 순서의 1~45 중 상위 6개를 뽑는다.
         Collections.sort(number);
+        // 랜덤하게 뽑은 로또번호를 정렬한다.
     }
 
     public LottoDTO(String numbers) {
-        number = Arrays.stream(numbers.split(",")).map(stringInt -> Integer.valueOf(stringInt.trim())).collect(Collectors.toList());
+        validationStringNumber(numbers);
+        
+        number = Arrays.stream(numbers.split(","))
+                        .map(stringInt -> Integer.valueOf(stringInt.trim()))
+                        .collect(Collectors.toList());
+    }
+
+    private void validationStringNumber(String numbers) {
+        String fixedNumbers = numbers.replaceAll(",","")
+                                    .replaceAll(" ", "")
+                                    .trim();
+        System.out.println(fixedNumbers);
+        if(!fixedNumbers.chars().allMatch(Character::isDigit) || numbers.split(",").length > 6)
+            throw new IllegalArgumentException("로또 번호가 유효하지 않습니다.");
     }
 
     public List<Integer> getNumber() {
@@ -31,6 +46,7 @@ public class LottoDTO {
         int ret = 0;
         for (Integer n : number) {
             ret += Boolean.compare(other.getNumber().contains(n), false);
+            //같은 숫자가 other에도 존재하면 1, 아니면 0을 ret에 더한다.
         }
         return ret;
     }
