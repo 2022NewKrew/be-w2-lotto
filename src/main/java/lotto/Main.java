80f6ff3876544af3fc912762edbabb5d4b4c092e
wiheeju <45173203+bb0711@users.noreply.github.com>
2022-01-03 16:13:21 +0900

    feat: 지난 주 당첨번호 입력 함수 추가

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -9,9 +9,12 @@ public class Main {
     public static void main(String[] args) {
         IO io = new IO();
         int itemCnt = io.start();
+
         LottoPack lottoPack = new LottoPack(itemCnt);
         List<List<Integer>> lottoNums = lottoPack.getNumList();
 
         io.printPurchasedLottos(lottoNums);
+
+        List<Integer> prevNums = io.enterPrevNums();
     }
 }
--- src/main/java/lotto/view/IO.java
+++ src/main/java/lotto/view/IO.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -8,7 +9,7 @@ public class IO {
     private static Scanner scanner = new Scanner(System.in);
 
     private static final String qPrice = "구입 금액을 입력해주세요.";
-    private static final String qPrevNums = "지난 주 당첨 금액을 입력해주세요.";
+    private static final String qPrevNums = "지난 주 당첨 번호을 입력해주세요. -- 띄어쓰기로 구별 ex) 1 2 3 4 5 6";
 
     private static final String aBuyNums = "%d개를 구매했습니다.";
     private static final String aResults = "당첨통계\n--------";
@@ -40,5 +41,16 @@ public class IO {
         for(List<Integer> lotto: lottos){
             System.out.println(Arrays.toString(lotto.toArray()));
         }
+    }
+
+    public List<Integer> enterPrevNums(){
+        System.out.println(qPrevNums);
+        List<Integer> prevNums = new ArrayList<Integer>();
+
+        while (scanner.hasNextInt() && prevNums.size()<5){
+            prevNums.add(scanner.nextInt());
+            System.out.println(Arrays.toString(prevNums.toArray()));
+        }
+        return prevNums;
     }
 }
