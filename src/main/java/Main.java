85314ba23dfcfdf77e5f2b7963f440a7557f70a2
kcm0147 <kcm0147@hanmail.net>
2022-01-03 23:58:37 +0900

    feat: Main 실행로직 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,29 @@
+import domain.LottoMachine;
+import domain.LottoPrice;
+import domain.LottoResult;
+import domain.LottoTicket;
+import view.InputView;
+import view.OutputView;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+        LottoPrice lottoPrice = InputView.inputLottoPrice();
+        LottoMachine lottoMachine = new LottoMachine();
+        List<LottoTicket> lottoTickets = lottoMachine.makeLottoTicket(lottoPrice);
+
+        OutputView.printLottoTickets(lottoTickets,wr);
+
+        LottoTicket winningTicket = InputView.inputWinningNumber();
+        LottoResult lottoResult = new LottoResult(winningTicket,lottoTickets,lottoPrice.getPrice());
+        OutputView.printLottoResult(lottoResult,wr);
+        wr.close();
     }
 }
