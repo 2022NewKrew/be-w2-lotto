45062b7aaf7ea2ae29e65cc469d041ab44e9bd13
frank.burger <frank.burger@kakaocorp.com>
2022-01-03 20:06:57 +0900

    feat: View 완성
    View만 완성했다.
    
    View가 제대로 작동하기 위해서는 각 domain에서 view에서 요구하는 함수를
    잘 구현해줘야 한다.
    
    역시 컴파일이 안 되어서, git commit 타입을 고민했지만, 이번에는
    그리고 이후에도 이런 경우에는 일단 feat으로 통일하고자 한다.
    지금 당장은 작동 안 하지만, 결론적으로는 새 기능을 추가한 게 맞다고 본다.

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -3,18 +3,41 @@ package view;
 import domain.Report;
 import domain.lottery.Ticket;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class View {
+    final static Scanner scanner = new Scanner(System.in);
+
     public int getBudgetByPrompt() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return scanner.nextInt();
     }
 
     public void showBoughtTickets(List<Ticket> tickets) {
+        System.out.println(tickets.size() + "개를 구매했습니다.");
+        for (var ticket : tickets) {
+            System.out.println(ticket.getNumbers());
+        }
     }
 
     public List<Integer> getResultNumbersByPrompt() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        List<Integer> numbers = new ArrayList<>();
+        for (var numberString : scanner.nextLine().split(",")) {
+            numbers.add(Integer.parseInt(numberString.strip()));
+        }
+        return numbers;
     }
 
     public void showReport(Report report) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.println("3개 일치 (" + report.getFourthPrizeValue() + "원)- " + report.getFourthPrizeCount() + "개");
+        System.out.println("3개 일치 (" + report.getThirdPrizeValue() + "원)- " + report.getThirdPrizeCount() + "개");
+        System.out.println("3개 일치 (" + report.getSecondPrizeValue() + "원)- " + report.getSecondPrizeCount() + "개");
+        System.out.println("3개 일치 (" + report.getFirstPrizeValue() + "원)- " + report.getFirstPrizeCount() + "개");
+        System.out.println("총 수익률은 " + report.getProfitRate() + "%입니다.");
     }
 }
