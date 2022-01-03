4e4fc93dfb3002ba3550fb755be17bf9629c9aee
rienkim <rienkim25@gmail.com>
2022-01-04 04:05:04 +0900

    로또 1단계 구현 (#71)
    * Add gradle files
    
    * Implement step 1
    
    Co-authored-by: pi.raspberry <pi.raspberry@kakaocorp.com>

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,41 @@
+package lotto;
+
+import java.util.*;
+
+public class Lotto {
+    private final ArrayList<Integer> lottoNumbers;
+
+    public Lotto() {
+        Random rand = new Random();
+        ArrayList<Integer> balls = new ArrayList<>();
+        for (int i = 0; i < 45; i++) {
+            balls.add(i);
+        }
+        Collections.shuffle(balls);
+        lottoNumbers = new ArrayList<>(balls.subList(0, 6));
+        Collections.sort(lottoNumbers);
+    }
+
+    public Lotto(List<Integer> lottoNumbers) {
+        this.lottoNumbers = new ArrayList<>();
+        this.lottoNumbers.addAll(lottoNumbers);
+    }
+
+    public ArrayList<Integer> getLottoNumbers() {
+        return lottoNumbers;
+    }
+
+    public int countMatch(Lotto lotto) {
+        int count = 0;
+        for (int number: lotto.lottoNumbers) {
+            if(lottoNumbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String toString() {
+        return lottoNumbers.toString();
+    }
+}
