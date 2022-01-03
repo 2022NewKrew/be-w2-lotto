ad19c8a11da61fb72db494e5500d719f19916dcc
punsoo <sonu.sohn@kakaocorp.com@gmail.com>
2022-01-04 00:47:10 +0900

    feat:LottoController 수정

---
+++ src/main/java/controller/LottoController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import domain.*;
+import view.InputView;
+
+public class LottoController {
+    public void run() {
+        Lottos lottos = LottoStore.purchase(new Money(InputView.enterPurchasingAmount()));
+        ResultView.printLottoNumbers(lottos);
+        WinningNumbers winningNumbers = new WinningNumbers(InputView.enterWinningNumbers(), InputView.enterBonusNumber());
+        ResultView.printResult(new WinningStatistics(lottos, winningNumbers));
+    }
+}
