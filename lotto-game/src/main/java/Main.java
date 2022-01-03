03c2e0a40e5f564c4fdbb8164b9eadd06d14a601
benn.diagram <benn.diagram@kakaocorp.com>
2022-01-03 19:29:14 +0900

    로또 1단계 구현

---
+++ lotto-game/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import game.LottoGame;
+
+public class Main {
+    private static final LottoGame lottoGame = new LottoGame();
+
+    public static void main(String[] args) {
+        lottoGame.start();
+    }
+}
