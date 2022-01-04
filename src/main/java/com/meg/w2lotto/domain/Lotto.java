65f94994292f3f928a32c46d712755ec98954826
meg.j <meg.j@kakaocorp.com>
2022-01-04 20:23:08 +0900

    로또생성분리, Prize enum 클래스 작성

--- src/main/java/com/meg/w2lotto/domain/Lotto.java
+++ src/main/java/com/meg/w2lotto/domain/Lotto.java
@@ -6,23 +6,14 @@ import java.util.List;
 
 public class Lotto {
 
-    public static final int NUMCOUNT = 6;
-    public static final int NUMRANGE = 45;
+    public static final int SIZE = 6;
+    public static final int NUMMIN = 1;
+    public static final int NUMMAX = 45;
     public static final int COST = 1000;
     private List<Integer> numbers;
 
-    Lotto() {
-        makeNumbers();
-    }
-
-    private void makeNumbers() {
-        List<Integer> randnums = new ArrayList<>(NUMRANGE);
-        for (int i = 1; i <= NUMRANGE; i++) {
-            randnums.add(i);
-        }
-        Collections.shuffle(randnums);
-        numbers = randnums.subList(0, NUMCOUNT);
-        Collections.sort(numbers);
+    public Lotto(List<Integer> numbers) {
+        this.numbers = numbers;
     }
 
     public List<Integer> getNumbers() {
--- src/main/java/com/meg/w2lotto/view/OutputView.java
+++ src/main/java/com/meg/w2lotto/view/OutputView.java
@@ -1,29 +1,33 @@
 package com.meg.w2lotto.view;
 
 import com.meg.w2lotto.domain.Lotto;
+import com.meg.w2lotto.domain.Prize;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
-    public static void printPurchaseMessage(int count) {
+    public static final void printPurchaseMessage(int count) {
         System.out.println(count + "개를 구매했습니다.");
     }
 
-    public static void printLottoNumber(List<Integer> numbers) {
+    public static final void printLottoNumber(List<Integer> numbers) {
         System.out.println(numbers.toString());
     }
 
-    public static void printResult(List<Integer> prizes, List<Integer> correctCounts) {
+    public static final void printResult(Map<Prize, Integer> correctCounts) {
         System.out.println("\n당첨 통계");
         System.out.println("---------");
 
-        for (int i = 3; i <= Lotto.NUMCOUNT; i++) {
-            System.out.println(String.format("%d개 일치 (%d원)- %d개", i, prizes.get(i), correctCounts.get(i)));
+        for (Prize prize : Prize.values()) {
+            System.out.print(prize.getMatchCount()+ "개 일치");
+            if (prize.getIsBonus()) System.out.print(", 보너스 볼 일치");
+            System.out.println(String.format(" (%d원)- %d개", prize.getWinningMoney(), correctCounts.get(prize)));
         }
     }
 
-    public static void printRateOfReturn(int rateOfReturn) {
+    public static final void printRateOfReturn(int rateOfReturn) {
         System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
     }
 }
