bdbd3c5454f545e07f35698454bd835e2af7d7c6
= <=>
2022-01-04 15:36:44 +0900

    Enum 클래스 추가 & 보너스 볼 처리

--- app/src/main/java/lotto/LottoBuyer.java
+++ app/src/main/java/lotto/LottoBuyer.java
@@ -8,14 +8,23 @@ import java.util.Map;
 public class LottoBuyer {
     private static final int LOTTO_PRICE = 1000;
     private final List<Lotto> lottoList = new ArrayList<>();
-    private final Map<Integer, Integer> result = new HashMap<>();
+    private final Map<Integer, Integer> result = new HashMap<>(Map.of(
+            3,0,
+            4,0,
+            5,0,
+            6,0,
+            7,0
+    ));
+    private int purchaseAmount; // 구매 금액
 
     public final void start() {
         Input.openScanner();
-        buyLotto(Input.getMoney());
+        purchaseAmount = Input.getMoney();
+        buyLotto(purchaseAmount);
         OutputView.printLottoList(lottoList);
         checkAllLotto();
         OutputView.printResult(result);
+        OutputView.printYield(calculateYield());
         Input.closeScanner();
     }
 
@@ -35,19 +44,28 @@ public class LottoBuyer {
 
     public final void checkAllLotto() {
         final List<Integer> winningNumbers = Input.getLastWinningNumbers();
+        Integer bonusBall = Input.getBonusBall();
+
         for(Lotto lotto : lottoList) {
-            int matched = lotto.match(winningNumbers);
+            int matched = lotto.match(winningNumbers, bonusBall);
             putResult(matched);
         }
     }
 
     private final void putResult(int key) {
         if(key < 3) return;
-
-        if(!result.containsKey(key)){
-            result.put(key, 1);
-            return;
-        }
         result.put(key, result.get(key) + 1);
+    }
+
+    private final int totalReward() {
+        int totalReward = 0;
+        for(Integer key : result.keySet()){
+            totalReward += result.get(key) * LottoPrize.of(key).getReward();
+        }
+        return totalReward;
+    }
+
+    private final double calculateYield() { // 수익률 계산
+        return (double) (totalReward() - purchaseAmount) / purchaseAmount * 100;
     }
 }
