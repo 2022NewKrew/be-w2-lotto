381a79b766e776ec6c9df0be9a7ba806de7b5463
jindo.min <jindo.min@kakaocorp.com>
2022-01-04 18:45:57 +0900

    [feat] utils 파일 추가 - random

---
+++ src/main/java/utils/RandomMaker.java
@@ -0,0 +1,19 @@
+package utils;
+
+import java.util.Random;
+
+public class RandomMaker {
+    private final Random random;
+
+    public RandomMaker() {
+        this.random = new Random();
+    }
+
+    public RandomMaker(Random random) {
+        this.random = random;
+    }
+
+    public Integer getRandomNumber(int min, int max) {
+        return random.nextInt(max - min + 1) + min;
+    }
+}
