5cba3a9007112775f0d1414ca2ce977fc309a715
= <=>
2022-01-04 15:36:44 +0900

    Enum 클래스 추가 & 보너스 볼 처리

--- app/src/main/java/lotto/Lotto.java
+++ app/src/main/java/lotto/Lotto.java
@@ -16,10 +16,16 @@ public class Lotto {
         return lottoNumbers.toString();
     }
 
-    public int match(List<Integer> winningNumbers) {
+    public int match(List<Integer> winningNumbers, Integer bonusBall) {
         int count = 0;
         for(Integer num : winningNumbers)
             count += lottoNumbers.contains(num) ? 1 : 0;
+
+        if(count == 6)
+            return 7;
+
+        if(lottoNumbers.contains(bonusBall))
+            count += 1;
         return count;
     }
 }
