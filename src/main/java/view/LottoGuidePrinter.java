6d31bae88256690c7c477de94e96d56cd70b6ef9
binary.yun <binary.yun@kakaocorp.com>
2022-01-04 10:47:46 +0900

    refactor: 출력 문자열을 전부 상수 필드로 변환

--- src/main/java/view/LottoGuidePrinter.java
+++ src/main/java/view/LottoGuidePrinter.java
@@ -8,10 +8,14 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LottoGuidePrinter {
+    private static final String COMMA = ", ";
+    private static final String OPEN_BRACKET = "[";
+    private static final String CLOSING_BRACKET = "]";
     private static final String PURCHASE_AMOUNT_REQUEST = "구입 금액을 입력해 주세요.";
     private static final String PURCHASE_QUANTITY = "개를 구매했습니다.\n";
     private static final String WINNING_LOTTO_REQUEST = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String WINNING_STATISTICS = "당첨 통계\n---------";
+    private static final String MATCH_COUNT = "%d개 일치 (%d원) - %d개\n";
     private static final String TOTAL_RETURN = "총 수익률: %.0f%%입니다.";
 
     public static void requestPurchaseAmountInput() {
@@ -32,7 +36,7 @@ public class LottoGuidePrinter {
     public static void printLotto(Lotto lotto) {
         System.out.println(lotto.getNumbers().stream()
                 .map(String::valueOf)
-                .collect(Collectors.joining(", ", "[", "]"))
+                .collect(Collectors.joining(COMMA, OPEN_BRACKET, CLOSING_BRACKET))
         );
     }
 
@@ -43,7 +47,7 @@ public class LottoGuidePrinter {
     public static void printLottoResult(int purchaseAmount, MatchResult matchResult) {
         System.out.println(WINNING_STATISTICS);
         for (Map.Entry<Integer, Integer> e : matchResult.getCount().entrySet()) {
-            System.out.printf("%d개 일치 (%d원) - %d개\n", e.getKey(), MatchResult.PRICES.get(e.getKey()), e.getValue());
+            System.out.printf(MATCH_COUNT, e.getKey(), MatchResult.PRICES.get(e.getKey()), e.getValue());
         }
         System.out.printf(TOTAL_RETURN, (matchResult.getTotalPrice() / ((double)purchaseAmount)) * 100);
     }
