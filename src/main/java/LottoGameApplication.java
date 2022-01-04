2725503ff315005c111e78cfbe709fd291ac3209
bryan.ik <bryan.ik@kakaocorp.com>
2022-01-04 17:06:35 +0900

    FEAT : 로또 게임의 컨트롤러 수정
    - 사용자의 입력을 받으면, 이를 컨트롤러 단에서 검증해서 로직을 수행하도록 함
    	* 기존에 사용했던 Validator 를 사용하지 않고, 입력 자체의 오류는 바로 try - catch 문으로 처리하도록 함
    	* 당첨 번호를 잘못 입력하거나, 보너스 번호를 잘못 입력하는 경우는 LottoMachine 에 전달해 문제가 있는 지 확인해 입력을 검증
    - 문제가 있는 입력의 경우 null 을 사용하지 않고, Optional 을 활용함으로써 NPE 이 발생하지 않도록 함

--- src/main/java/LottoGameApplication.java
+++ src/main/java/LottoGameApplication.java
@@ -1,9 +1,12 @@
 import lotto.controller.LottoGame;
+import lotto.domain.LottoMachine;
+import lotto.view.LottoGameView;
 
 public class LottoGameApplication {
 
     public static void main(String[] args) {
-        LottoGame lottoGame = new LottoGame();
+        LottoGame lottoGame = new LottoGame(new LottoMachine(),
+            new LottoGameView());
         lottoGame.run();
     }
 }
