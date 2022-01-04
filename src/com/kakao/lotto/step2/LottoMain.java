368c82258f15d316bd295bfe55ad33c3983c2d82
2018008613 <48302945+2018008613@users.noreply.github.com>
2022-01-05 04:05:14 +0900

    Step1 내용 수정 및 Step2 구현 (#198)
    * 로또 1단계 수정
    
    * 로또 1단계 수정
    
    * 당첨 번호 입력 예외처리 추가
    
    * gitignore 적용
    
    * LottoMain 구조 변경
    
    * LottoMain에서 LOTTO_PRICE 상수 제거
    
    * 수익률 계산 LottoResult로 옮김
    
    * Stream을 이용한 코드 변환
    
    * Enum을 이용한 results 구현
    
    Co-authored-by: tony.ahn <tony.ahn@kakaocorp.com>

---
+++ src/com/kakao/lotto/step2/LottoMain.java
@@ -0,0 +1,53 @@
+package com.kakao.lotto.step2;
+
+import com.kakao.lotto.step2.domain.Lotto;
+import com.kakao.lotto.step2.domain.LottoResult;
+import com.kakao.lotto.step2.view.InputLotto;
+import com.kakao.lotto.step2.view.LottoPrinter;
+import com.kakao.lotto.step2.view.LottoResultPrinter;
+
+import java.util.List;
+
+public class LottoMain {
+
+    private int bonusNumber;
+    private int lottoNumber;
+    List<Lotto> lottos;
+    List<Integer> winningNumbers;
+    private InputLotto inputLotto = new InputLotto();
+    LottoPrinter lottoPrinter = new LottoPrinter();
+
+    public void inputPrice() {
+        lottoNumber = inputLotto.getLottoNumber();
+        lottoPrinter.printBuyLottoNumber(lottoNumber);
+    }
+
+    public void makeLotto() {
+        lottos = Lotto.makeLottos(lottoNumber);
+        lottoPrinter.printLottos(lottos);
+    }
+
+    public void inputWinningNumbers() {
+        winningNumbers = inputLotto.getWinningNumbers();
+    }
+
+    public void inputBonusNumber() {
+        bonusNumber = inputLotto.getBonusNumber();
+    }
+
+    public void printResult() {
+        LottoResult lottoResult = new LottoResult(lottos, winningNumbers, bonusNumber);
+        LottoResultPrinter lottoResultPrinter = new LottoResultPrinter(lottoResult);
+        lottoResultPrinter.printResults();
+        lottoResultPrinter.printProfitRate();
+    }
+
+    public static void main(String[] args) {
+        LottoMain lottoMain = new LottoMain();
+        lottoMain.inputPrice();
+        lottoMain.makeLotto();
+        lottoMain.inputWinningNumbers();
+        lottoMain.inputBonusNumber();
+        lottoMain.printResult();
+    }
+}
---
+++ src/com/kakao/lotto/step2/view/InputLotto.java
@@ -0,0 +1,100 @@
+package com.kakao.lotto.step2.view;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class InputLotto {
+
+    private int LOTTO_PRICE = 1000;
+
+    private Scanner scanner = new Scanner(System.in);
+
+    private void checkNegative(int number) throws Exception {
+        if(number < 0)
+            throw new Exception("음수 발생");
+    }
+
+    private int inputPrice() throws Exception {
+        System.out.println("구입 금액을 입력해주세요.");
+        int price = Integer.parseInt(scanner.nextLine());
+        checkNegative(price);
+        return price;
+    }
+
+    // 구입 금액을 입력받습니다. 정수가 아니거나 음수를 입력받았을 경우 다시 입력받습니다.
+    public int getLottoNumber() {
+        while(true) {
+            try {
+                return inputPrice() / LOTTO_PRICE;
+            } catch (Exception exception) {
+                scanner = new Scanner(System.in);
+                System.out.println("0 이상의 정수를 입력하세요");
+            }
+        }
+    }
+
+    private void checkSize(List<Integer> winningNumbers) throws Exception {
+        if(winningNumbers.size() != 6)
+            throw new Exception("6개의 번호를 입력해주세요");
+    }
+
+    private void checkDuplicate(List<Integer> winningNumbers) throws Exception {
+        Set<Integer> winningSet = new HashSet<>(winningNumbers);
+        if(winningNumbers.size() != winningSet.size())
+            throw new Exception("중복 요소가 있습니다.");
+    }
+
+    private void checkNumbers(List<Integer> winningNumbers) throws Exception {
+        if((int)winningNumbers.stream().filter(number -> number > 0 && number <= 45).count()
+                != winningNumbers.size())
+            throw new Exception("1에서 45 사이의 숫자를 입력하세요");
+    }
+
+    private List<Integer> inputWinningNumbers() throws Exception {
+        System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
+        List<Integer> winningNumbers = Pattern.compile(", ").splitAsStream(scanner.nextLine())
+                .map(string -> Integer.valueOf(string)).collect(Collectors.toList());
+        checkSize(winningNumbers);
+        checkDuplicate(winningNumbers);
+        checkNumbers(winningNumbers);
+        Collections.sort(winningNumbers);
+        return winningNumbers;
+    }
+
+    // 6개의 당첨 번호를 입력받습니다. 올바른 입력이 들어올 때까지 반복문을 돕니다.
+    public List<Integer> getWinningNumbers() {
+        while(true) {
+            try {
+                return inputWinningNumbers();
+            } catch(Exception exception) {
+                System.out.println("1 ~ 45 사이의 정수를 ', '로 구분해 중복되지 않게 6개 입력해주세요.");
+           }
+        }
+    }
+
+    private void checkNumber(int number) throws Exception {
+        if(number < 1 || number > 45)
+            throw new Exception("1 ~ 45 사이의 수를 입력하시오");
+    }
+
+    private int inputBonusNumber() throws Exception {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        int bonusNumber = Integer.parseInt(scanner.nextLine());
+        checkNumber(bonusNumber);
+        return bonusNumber;
+    }
+
+    // 구입 금액을 입력받습니다. 정수가 아니거나 음수를 입력받았을 경우 다시 입력받습니다.
+    public int getBonusNumber() {
+        while(true) {
+            try {
+                return inputBonusNumber();
+            } catch (Exception exception) {
+                scanner = new Scanner(System.in);
+                System.out.println("1 ~ 45 범위의 정수를 입력하세요");
+            }
+        }
+    }
+
+}
