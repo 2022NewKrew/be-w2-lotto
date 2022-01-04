cfcf300d94d925abdc570eedc59b4356b21cffb9
pride.chicken <pride.chicken@kakaocorp.com>
2022-01-04 15:35:17 +0900

    [feature] view 구조 변경 및 2등 추가
    - 2등, 1등의 처리를 다르게 해줘야 하므로, 메소드 분리해서 구현

--- src/main/java/view/LottoView.java
+++ src/main/java/view/LottoView.java
@@ -8,9 +8,11 @@ import java.util.List;
 public class LottoView {
 
     private static final int MIN_COUNT_LOTTO_WINNINGS = 3;
-    private static final int MAX_COUNT_LOTTO_WINNINGS = 6;
+    private static final int MAX_COUNT_LOTTO_WINNINGS = 7;
 
-    private Person buyer;
+    private final Person buyer;
+    List<Integer> winnings;
+    List<Integer> countWinnings;
 
     public LottoView(Person buyer) {
         this.buyer = buyer;
@@ -28,14 +30,24 @@ public class LottoView {
     }
 
     public void showResult() {
+        winnings = buyer.getLottoWinnings();
+        countWinnings = buyer.getCorrectList();
         StringBuilder result = new StringBuilder("당첨 통계\n---------\n");
-        List<Integer> winnings = buyer.getLottoWinnings();
-        List<Integer> countWinnings = buyer.getCorrectList();
 
         for (int i = MIN_COUNT_LOTTO_WINNINGS; i <= MAX_COUNT_LOTTO_WINNINGS; i++) {
-            result.append(i).append("개 일치 (").append(winnings.get(i)).append("원) - ").append(countWinnings.get(i)).append("개\n");
+            result.append(makeLottoString(i));
         }
         result.append("총 수입률은 ").append(buyer.getIncomeRate()).append("% 입니다.");
         System.out.println(result);
+    }
+
+    private String makeLottoString(int rank) {
+        if (rank == 6) {
+            return "5개 일치, 보너스 볼 일치(" + winnings.get(rank) + "원) - " + countWinnings.get(rank) + "개\n";
+        }
+        if (rank == 7) {
+            return rank - 1 + "개 일치 (" + winnings.get(rank) + "원) - " + countWinnings.get(rank) + "개\n";
+        }
+        return rank + "개 일치 (" + winnings.get(rank) + "원) - " + countWinnings.get(rank) + "개\n";
     }
 }
