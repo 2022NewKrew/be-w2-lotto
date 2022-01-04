531fa082dcc8a90fc630a8f76eaa56150d735a86
pride.chicken <pride.chicken@kakaocorp.com>
2022-01-04 15:28:51 +0900

    [feature] LottoGame에서 보너스 볼 입력받기
    - 보너스 볼을 입력받는 메소드 추가

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -42,7 +42,6 @@ public class LottoGame {
         scanner.nextLine();
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         lottoWinningNumbers = scanner.nextLine();
-        scanner.close();
     }
 
     private void consoleInputPrice(){
