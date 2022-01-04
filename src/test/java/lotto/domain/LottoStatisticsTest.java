2fd53af3c8f3d7a34a3dd9bcaeeb21044fca6db3
anika.dovob <anika.dovob@kakaocorp.com>
2022-01-04 18:59:17 +0900

    test: 2등 당첨 테스트 추가

--- src/test/java/lotto/domain/LottoStatisticsTest.java
+++ src/test/java/lotto/domain/LottoStatisticsTest.java
@@ -18,8 +18,9 @@ class LottoStatisticsTest {
         }
 
         LottoNumbers lottoNumbers = LottoNumbers.from(numbers);
+        LottoNumber bonusNumber = LottoNumber.from(7);
 
-        WinningNumbers winningNumbers = WinningNumbers.from(lottoNumbers);
+        WinningNumbers winningNumbers = WinningNumbers.from(lottoNumbers, bonusNumber);
 
         List<LottoTicket> tickets = new ArrayList<>();
         tickets.add(new LottoTicket(lottoNumbers));
@@ -40,8 +41,9 @@ class LottoStatisticsTest {
         }
 
         LottoNumbers lottoNumbers = LottoNumbers.from(numbers);
+        LottoNumber bonusNumber = LottoNumber.from(7);
 
-        WinningNumbers winningNumbers = WinningNumbers.from(lottoNumbers);
+        WinningNumbers winningNumbers = WinningNumbers.from(lottoNumbers, bonusNumber);
 
         List<LottoTicket> tickets = new ArrayList<>();
         tickets.add(new LottoTicket(lottoNumbers));
@@ -51,5 +53,33 @@ class LottoStatisticsTest {
         LottoStatistics lottoStatistics = new LottoStatistics(winningNumbers, lottoTickets);
 
         assertThat(lottoStatistics.calculateRevenueRate(new Money(1000))).isEqualTo(200000000);
+    }
+
+    @DisplayName("2등 당첨 테스트")
+    @Test
+    public void secondWinning() {
+        Set<LottoNumber> numbers = new HashSet<>();
+        for (int i = 1; i <= 6; i++) {
+            numbers.add(LottoNumber.from(i));
+        }
+
+        LottoNumbers lottoNumbers = LottoNumbers.from(numbers);
+        LottoNumber bonusNumber = LottoNumber.from(7);
+
+        WinningNumbers winningNumbers = WinningNumbers.from(lottoNumbers, bonusNumber);
+
+        Set<LottoNumber> ticketNumbers = new HashSet<>();
+        for (int i = 2; i <= 7; i++) {
+            ticketNumbers.add(LottoNumber.from(i));
+        }
+        LottoNumbers ticketLottoNumbers = LottoNumbers.from(ticketNumbers);
+        List<LottoTicket> tickets = new ArrayList<>();
+        tickets.add(new LottoTicket(ticketLottoNumbers));
+
+        LottoTickets lottoTickets = LottoTickets.from(tickets);
+
+        LottoStatistics lottoStatistics = new LottoStatistics(winningNumbers, lottoTickets);
+
+        assertThat(lottoStatistics.calculateRevenueRate(new Money(1000))).isEqualTo(3000000);
     }
 }
