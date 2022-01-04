2ab1356bf01f75325394e0dd74257f1fe429bb97
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-01-04 15:47:03 +0900

    로또 1단계 구현 (#44)

---
+++ src/main/java/lotto/domain/RandomLottoNumberGenerator.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RandomLottoNumberGenerator {
+    public static final int NUMBER_POOL_SIZE = 45;
+    public static final int NUMBERS_TO_PICK = 6;
+
+    private static final Random random = new Random();
+    private static final List<Integer> nextRandomNumbers = IntStream.rangeClosed(1, NUMBER_POOL_SIZE)
+            .boxed().collect(Collectors.toList());
+
+    public static Set<Integer> generateRandomLottoNumbers() {
+        Collections.shuffle(nextRandomNumbers);
+        return new HashSet<>(nextRandomNumbers.subList(0, NUMBERS_TO_PICK));
+    }
+}
---
+++ src/main/java/lotto/view/LottoPrinter.java
@@ -0,0 +1,44 @@
+package lotto.view;
+
+import lotto.domain.Gambler;
+import lotto.domain.LottoShop;
+import lotto.domain.LottoTicket;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static lotto.domain.LottoShop.PRICE;
+import static lotto.domain.LottoShop.PRIZES;
+
+public class LottoPrinter {
+
+    // TODO: 메서드 분리
+    public void PrintLottoMatchingResult(LottoShop lottoShop, Gambler gambler) {
+        Set<Integer> winnerNumber = lottoShop.getWinnerNumber();
+        List<LottoTicket> tickets = gambler.getTickets();
+
+        // <Matched, Occurrence>
+        Map<Integer, Long> matchingResult = tickets.stream()
+                .map(ticket -> ticket.matchWithWinnerNumber(winnerNumber))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println("당첨 통계");
+        System.out.println("--------------");
+        long prizeSum = 0L;
+        for (var entry: matchingResult.entrySet()) {
+            Integer matched = entry.getKey();
+            Long occurrence = entry.getValue();
+
+            Long prize = PRIZES.get(matched);
+            prizeSum += occurrence * prize;
+
+            System.out.printf("%d개 일치 (%d원) - %d개%n", matched, prize, occurrence);
+        }
+
+        int purchaseCosts = PRICE * tickets.size();
+        System.out.printf("총 수익률은 %2.0f%%입니다.%n", (float)prizeSum / purchaseCosts);
+    }
+}
