68f0e6325da660e44a2cd724cd8d42dd9e75d1fd
jb.jang <jb.jang@kakaocorp.com>
2022-01-04 17:27:48 +0900

    feat: 테스트 의존성 설정 및 Lotto test 틀 추가

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoTest {
+
+    @Test
+    void numberListToString() {
+    }
+
+    @Test
+    void checkLotto() {
+    }
+
+    @Test
+    void hasBonusNumber() {
+    }
+}
