8150338160bfa643aa506d1c10669b4d126ec5f1
david.dh <david.dh@kakaocorp.com>
2022-01-04 15:25:56 +0900

    피드백 반영, 패키지 이동, 변수명 수정, 수익률 계산 수정, 당첨 관련 부분 enum으로 분리

---
+++ src/main/java/com/david/lotto/LottoCalculate.java
@@ -0,0 +1,61 @@
+package com.david.lotto;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class LottoCalculate {
+
+    private final Map<Rank, Integer> totalCount = new LinkedHashMap<>();
+    private double revenue;
+
+    public LottoCalculate() {
+        totalCount.put(Rank.FIRST, 0);
+        totalCount.put(Rank.SECOND, 0);
+        totalCount.put(Rank.THIRD, 0);
+        totalCount.put(Rank.FOURTH, 0);
+        totalCount.put(Rank.FIFTH, 0);
+    }
+
+    private void addCount(int countOfMatch, boolean matchBonus) {
+        if (countOfMatch > 2) {
+            Rank rank = Rank.valueOf(countOfMatch, matchBonus);
+            int curCount = totalCount.get(rank);
+            totalCount.put(rank, curCount + 1);
+        }
+    }
+
+    private void calculateCount(List<Lotto> lottoList, List<Integer> winningNumber, int bonusNumber) {
+        for (Lotto lotto : lottoList) {
+            addCount(lotto.calculateCountOfMatch(winningNumber), lotto.checkBonusNumber(bonusNumber));
+        }
+    }
+
+    private void calculateRevenue() {
+        for (Rank rank : totalCount.keySet()) {
+            revenue += (rank.getWinningMoney() * totalCount.get(rank));
+        }
+    }
+
+    public double calculateProfitRate(List<Lotto> lottoList, List<Integer> winningNumber, int count, int bonusNumber) {
+        calculateCount(lottoList, winningNumber, bonusNumber);
+        calculateRevenue();
+        int amount = count * LottoMachine.lottoPrice;
+        return ((revenue - amount) / amount) * 100;
+    }
+
+    private String formatString(Rank rank) {
+        if (rank.equals(Rank.SECOND)) {
+            return String.format("%d개 일치, 보너스 볼 일치(%d원)- %d개", rank.getCountOfMatch(), rank.getWinningMoney(), totalCount.get(rank));
+        }
+        return String.format("%d개 일치 (%d원)- %d개", rank.getCountOfMatch(), rank.getWinningMoney(), totalCount.get(rank));
+    }
+
+    @Override
+    public String toString() {
+        return totalCount.keySet().stream()
+                .map(this::formatString)
+                .collect(Collectors.joining("\n"));
+    }
+}
