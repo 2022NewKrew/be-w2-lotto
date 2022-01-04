67aacd37047a70afcf49c868f3b571817710ea07
binary.yun <binary.yun@kakaocorp.com>
2022-01-05 02:18:27 +0900

    test: 어플리케이션의 테스트 케이스 추가

--- src/test/java/application/AutoLottoGeneratorTest.java
+++ src/test/java/application/AutoLottoGeneratorTest.java
@@ -1,37 +1,17 @@
 package application;
 
-import domain.Ball;
-import domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class AutoLottoGeneratorTest {
-    private static final int MIM_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
-    private final LottoGenerator lottoGenerator = new AutoLottoGenerator();
 
-    @DisplayName("생성한 로또가 전부 다른 값인지 검증")
-    @RepeatedTest(50)
-    void isLottoNumberDuplicated() {
-        Lotto lotto = lottoGenerator.getLotto();
-        List<Ball> lottoNumbers = lotto.getBalls();
-
-        assertThat(lottoNumbers.stream().distinct().count())
-                .isEqualTo(lottoNumbers.size());
+    @DisplayName("정상적으로 자동 로또가 생성되는지 검증")
+    @Test
+    void isValidLottoCreated() {
+        LottoGenerator lottoGenerator = new AutoLottoGenerator();
+        assertDoesNotThrow(lottoGenerator::getLotto);    // 정상적(중복X, 유효 범위)으로 로또를 생성하면 예외가 발생하지 않는다
     }
 
-    @DisplayName("생성된 로또의 값이 1~45의 자연수인지 검증")
-    @RepeatedTest(50)
-    void isLottoNumberValid() {
-        Lotto lotto = lottoGenerator.getLotto();
-        List<Ball> lottoNumbers = lotto.getBalls();
-
-        assertThat(lottoNumbers.stream()
-                .anyMatch(e -> !(e.getNumber() >= MIM_LOTTO_NUMBER && e.getNumber() <= MAX_LOTTO_NUMBER)))
-                .isFalse();
-    }
 }
