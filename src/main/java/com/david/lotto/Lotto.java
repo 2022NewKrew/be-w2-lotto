81dfc6961bbb197bed62cddc70ac2ca13ef58da3
david.dh <david.dh@kakaocorp.com>
2022-01-04 15:23:47 +0900

    피드백 반영, 패키지 이동, 메소드명 수정, 보너스 숫자 확인 추가

---
+++ src/main/java/com/david/lotto/Lotto.java
@@ -0,0 +1,37 @@
+package com.david.lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Lotto {
+
+    private static final List<Integer> allLottoNumber = IntStream.range(1, 46).boxed().collect(Collectors.toList());
+    private final List<Integer> lottoNumber = new ArrayList<>();
+
+    public Lotto() {
+        synchronized (this) {
+            Collections.shuffle(allLottoNumber);
+            for (int i = 0; i < 6; i++) {
+                int number = allLottoNumber.get(i);
+                lottoNumber.add(number);
+            }
+        }
+        Collections.sort(lottoNumber);
+    }
+
+    public int calculateCountOfMatch(List<Integer> winningNumber) {
+        return (int) lottoNumber.stream().filter(winningNumber::contains).count();
+    }
+
+    public boolean checkBonusNumber(int bonusNumber) {
+        return lottoNumber.contains(bonusNumber);
+    }
+
+    @Override
+    public String toString() {
+        return lottoNumber.toString();
+    }
+}
