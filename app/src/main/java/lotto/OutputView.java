8eba75f3d5d694884ec7c188d8c7bc237a1580c1
= <=>
2022-01-04 15:36:44 +0900

    Enum 클래스 추가 & 보너스 볼 처리

--- app/src/main/java/lotto/OutputView.java
+++ app/src/main/java/lotto/OutputView.java
@@ -4,21 +4,25 @@ import java.util.List;
 import java.util.Map;
 
 public class OutputView {
-    public static final void printCountLotto(int countLotto) {
+    public static void printCountLotto(int countLotto) {
         System.out.println(countLotto + "개를 구매했습니다.");
     }
 
-    public static final void printLottoList(List<Lotto> lottoList) {
+    public static void printLottoList(List<Lotto> lottoList) {
         for(Lotto lotto : lottoList) {
             System.out.println(lotto);
         }
     }
 
-    public static final void printResult(Map<Integer, Integer> result) {
-        System.out.println("당첨 통계\n-------\n");
-        result.forEach((key, value) -> {
-            System.out.println(key + "개 일치 - " + value + "개");
-        });
+    public static void printResult(Map<Integer, Integer> result) {
+        System.out.println("당첨 통계\n-------");
+        for(Integer key : result.keySet()) {
+            LottoPrize lottoPrize = LottoPrize.of(key);
+            System.out.println(lottoPrize.getMessage() + " (" + lottoPrize.getReward()+ "원)- " + result.get(key) + "개");
+        }
+    }
 
+    public static void printYield(double yield) {
+        System.out.printf("총 수익률은 %.2f%s 입니다.", yield, "%"); // ?
     }
 }
