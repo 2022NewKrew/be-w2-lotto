6d9fde8919f37e726258dbe74042cbc6aa39bdec
punsoo <68231412+punsoo@users.noreply.github.com>
2022-01-04 22:09:50 +0900

    Step1, 2 리뷰 요청드립니다. (#94)
    * docs:.gitignore 파일 추가
    
    * feat:LottoNumber 구현
    
    * feat:WinningNumbers 구현
    
    * feat:Rank 구현
    
    * feat:Lotto 구현
    
    * feat:WinningStatistics 구현
    
    * feat:Lottos 일급 컬렉션 구현
    
    * feat:Money 구현
    
    * feat:LottoStore 구현
    
    * feat:LottoController 구현
    
    * feat:LottoController 수정
    
    * feat:InputView 구현
    
    * feat:ResultView 구현
    
    * feat: 수익률 단위 수정
    
    * refactor: 접근 제어자 순서 수정
    
    * refactor: forEach 수정
    
    Co-authored-by: punsoo <sonu.sohn@kakaocorp.com@gmail.com>

---
+++ src/main/java/domain/Rank.java
@@ -0,0 +1,45 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+    MISS(0, 0, "꽝"),
+    FIFTH(3, 5_000, "3개 일치"),
+    FOURTH(4, 50_000, "4개 일치"),
+    THIRD(5, 1_500_000, "5개 일치"),
+    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIRST(6, 2_000_000_000, "6개 일치");
+
+    private final long hit;
+    private final long amount;
+    private final String message;
+
+    Rank(long hit, long amount, String message) {
+        this.hit = hit;
+        this.amount = amount;
+        this.message = message;
+    }
+
+    private static boolean filterWhenHitIs5(boolean bonusNumber, long amount) {
+        return bonusNumber ? amount == 30_000_000 : amount == 1_500_000;
+    }
+
+    public static Rank from(long hit, boolean bonusNumber) {
+        return Arrays.stream(values())
+                .filter(rank -> hit == 5 ? filterWhenHitIs5(bonusNumber, rank.amount) : rank.hit == hit)
+                .findFirst()
+                .orElse(MISS);
+    }
+
+    public long amount() {
+        return this.amount;
+    }
+
+    public long hit() {
+        return this.hit;
+    }
+
+    public String message() {
+        return this.message;
+    }
+}
