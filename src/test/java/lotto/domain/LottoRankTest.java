92d1d5110e278af257a193cb471d092f612267f8
mylo.l <mylo.l@kakaocorp.com>
2022-01-04 21:28:07 +0900

    Step2. LottoRankTest 추가

--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -1,12 +1,27 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 class LottoRankTest {
 
+    @DisplayName("2등 반환 확인")
     @Test
-    void valueOf() {
+    void valueOfSecond() {
+        assertThat(LottoRank.valueOf(5, true)).isEqualTo(LottoRank.SECOND);
+    }
+
+    @DisplayName("3등 반환 확인")
+    @Test
+    void valueOfThird() {
+        assertThat(LottoRank.valueOf(5, false)).isEqualTo(LottoRank.THIRD);
+    }
+
+    @DisplayName("꽝 확인")
+    @Test
+    void valueOfNull() {
+        assertThat(LottoRank.valueOf(2, false)).isEqualTo(null);
     }
 }
