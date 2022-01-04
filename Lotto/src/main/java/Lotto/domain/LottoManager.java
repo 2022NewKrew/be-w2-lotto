1fd16c353f324ba3a32a95765bd1eaafd534425d
JungRyung <soondori92@gmail.com>
2022-01-04 22:12:34 +0900

    step2 commit

--- Lotto/src/main/java/Lotto/domain/LottoManager.java
+++ Lotto/src/main/java/Lotto/domain/LottoManager.java
@@ -2,45 +2,32 @@ package Lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Collections;
 
 public class LottoManager {
     List<Ticket> tickets;
-    List<Integer> numberCards;
     List<Integer> results;
     private int investmentAmount;
     private int numberOfTicket;
+    private int bonusNumber;
 
     public LottoManager(){
-        this.tickets = new ArrayList<Ticket>();
-        this.makeNumberCards();
+        this.tickets = new ArrayList<>();
     }
 
     public List<Ticket> getTickets(){return this.tickets;}
     public List<Integer> getResults(){return this.results;}
     public int getInvestmentAmount(){return this.investmentAmount;}
-    public int getNumberOfTicket(){return this.numberOfTicket;}
+    public int getBonusNumber(){return this.bonusNumber;}
 
     public void setResults(List<Integer> results){this.results = results;}
-
-    // 숫자카드 덱 만들기 메소드
-    private void makeNumberCards(){
-        this.numberCards = new ArrayList<>();
-        for(int num=1;num<=45;num++) this.numberCards.add(num);
-    }
-
-    // 숫자카드 섞기 메소드
-    private void shuffleNumberCards(){
-        Collections.shuffle(this.numberCards);
-    }
+    public void setBonusNumber(int bonusNumber){this.bonusNumber = bonusNumber;}
 
     // 가격에 맞게 로또를 구매하는 메소드
     public void buyTickets(int price){
         this.investmentAmount = price;
         this.numberOfTicket = price / 1000;
         for(int num=0; num<numberOfTicket; num++) {
-            this.shuffleNumberCards();
-            this.tickets.add(new Ticket(this.numberCards));
+            this.tickets.add(new Ticket());
         }
     }
 }
--- Lotto/src/main/java/Lotto/domain/LottoStatistics.java
+++ Lotto/src/main/java/Lotto/domain/LottoStatistics.java
@@ -1,35 +1,33 @@
 package Lotto.domain;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoStatistics {
-    private int matched3num;
-    private int matched4num;
-    private int matched5num;
-    private int matched6num;
-    private int profit;
-    private int rateOfProfit;
+    private final Map<Rank, Integer> countOfWining = new LinkedHashMap<>();
+    private long profit;
+    private long rateOfProfit;
 
-    public LottoStatistics(){
-        this.matched3num = 0;
-        this.matched4num = 0;
-        this.matched5num = 0;
-        this.matched6num = 0;
+    public LottoStatistics(int investmentAmount, List<Ticket> tickets, List<Integer> results, int bonusNumber){
+        this.countOfWining.put(Rank.FIRST, 0);
+        this.countOfWining.put(Rank.SECOND, 0);
+        this.countOfWining.put(Rank.THIRD, 0);
+        this.countOfWining.put(Rank.FOURTH, 0);
+        this.countOfWining.put(Rank.FIFTH, 0);
         this.rateOfProfit = 0;
+        this.calculateStatistics(investmentAmount, tickets, results, bonusNumber);
     }
 
-    public int getMatched3num(){return this.matched3num;}
-    public int getMatched4num(){return this.matched4num;}
-    public int getMatched5num(){return this.matched5num;}
-    public int getMatched6num(){return this.matched6num;}
-    public int getRateOfProfit(){return this.rateOfProfit;}
+    public Map<Rank, Integer> getCountOfWining(){return this.countOfWining;}
+    public long getRateOfProfit(){return this.rateOfProfit;}
 
-    // 맞은 숫자의 개수에 따라 멤버변수의 개수를 증가하는 메소드
-    private void increaseMatchedNum(int num){
-        if(num == 3){ this.matched3num++; return;}
-        if(num == 4){ this.matched3num++; return;}
-        if(num == 5){ this.matched3num++; return;}
-        if(num == 6){ this.matched3num++; return;}
+    // 맞은 숫자의 개수에 따라 카운트를 증가하는 메소드
+    private void increaseMatchedNum(int matchedNum, boolean isMatchedBonus){
+        if(matchedNum > 2){
+            Rank rank = Rank.value(matchedNum, isMatchedBonus);
+            countOfWining.put(rank, countOfWining.get(rank) + 1);
+        }
     }
 
     // 해당 숫자가 복권에 있는 숫자면 1을 반환, 그렇지 않으면 0을 반환하는 메소드
@@ -49,13 +47,17 @@ public class LottoStatistics {
 
     // 구입한 복권들로 얻은 수익을 계산하는 메소드
     private void calculateProfit(){
-        this.profit = this.matched3num*5000 + this.matched4num*50000 + this.matched5num*1500000 + this.matched6num*2000000000;
+        this.profit = this.countOfWining.get(Rank.FIFTH) * Rank.FIFTH.getEarnedMoney()
+                + this.countOfWining.get(Rank.FOURTH) * Rank.FOURTH.getEarnedMoney()
+                + this.countOfWining.get(Rank.THIRD) * Rank.THIRD.getEarnedMoney()
+                + this.countOfWining.get(Rank.SECOND) * Rank.SECOND.getEarnedMoney()
+                + this.countOfWining.get(Rank.FIRST) * Rank.FIRST.getEarnedMoney();
     }
 
     // 로또 통계를 계산하는 메소드
-    public void calculateStatistics(int investmentAmount, List<Ticket> tickets, List<Integer> results){
+    private void calculateStatistics(int investmentAmount, List<Ticket> tickets, List<Integer> results, int bonusNumber){
         for(Ticket ticket : tickets) {
-            this.increaseMatchedNum(matchingResults(ticket.getTicket(), results));
+            this.increaseMatchedNum(matchingResults(ticket.getTicket(), results), ticket.checkBonusNum(bonusNumber));
         }
         this.calculateProfit();
         this.rateOfProfit = this.profit * 100 / investmentAmount;
