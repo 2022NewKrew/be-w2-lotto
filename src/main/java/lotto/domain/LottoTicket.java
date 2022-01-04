90d34354fd6281ae3f2acf19a166d0c7c47312d0
flip.chae <flip.chae@kakaocorp.com>
2022-01-04 20:02:20 +0900

    refactor: LottoTicket 기능을 LottoNumbers로 분리

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,54 +1,19 @@
 package lotto.domain;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 public class LottoTicket {
 
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
-    private static final int MAX_LOTTO_SIZE = 6;
+    private final LottoNumbers lottoNumbers;
 
-    private List<Integer> lottoNumbers;
-
-    public LottoTicket() {
-        createLottoTicket();
+    public LottoTicket(LottoNumbers lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
     }
 
-    private void createLottoTicket() {
-        List<Integer> randomLottoNumbers = randomLottoNumbers();
-        lottoNumbers = (randomLottoNumbers.subList(0, MAX_LOTTO_SIZE).stream()
-                .sorted()
-                .collect(Collectors.toList()));
+    public int getLottoMatchCount(LottoNumbers winningNumbers) {
+        return lottoNumbers.getLottoMatchCount(winningNumbers);
     }
 
-    private List<Integer> randomLottoNumbers() {
-        List<Integer> randomLottoNumbers = IntStream.rangeClosed(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)
-                .boxed()
-                .collect(Collectors.toList());
-        Collections.shuffle(randomLottoNumbers);
-        return randomLottoNumbers;
-    }
-
-    public int getLottoMatchCount(List<Integer> winningNumbers) {
-        int result = 0;
-        for (Integer winningNumber : winningNumbers) {
-            result = getResultCount(result, winningNumber);
-        }
-        return result;
-    }
-
-    private int getResultCount(int result, int winningNumber) {
-        if (hasWinningNumber(winningNumber)) {
-            return result + 1;
-        }
-        return result;
-    }
-
-    private Boolean hasWinningNumber(int winningNumber) {
-        return lottoNumbers.contains(winningNumber);
+    public boolean hasBonusNumber(int bonusNumber) {
+        return lottoNumbers.contains(bonusNumber);
     }
 
     @Override
