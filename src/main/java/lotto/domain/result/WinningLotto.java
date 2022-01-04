e4b6ca8f73e39da9aa6bc850aff071b340fa9a20
yuchanleeme <yuchanlee.me@gmail.com>
2022-01-04 19:39:01 +0900

    feat: WinningLotto 유효성 검사 추가
    - 당첨 번호 개수
    - 중복 당첨 번호

--- src/main/java/lotto/domain/result/WinningLotto.java
+++ src/main/java/lotto/domain/result/WinningLotto.java
@@ -2,13 +2,16 @@ package lotto.domain.result;
 
 import lotto.domain.lotto.Lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningLotto implements Lotto {
 
     private final List<Integer> numbers;
 
     public WinningLotto(List<Integer> list) {
+        validateNumbers(list);
         this.numbers = list;
     }
 
@@ -17,4 +20,14 @@ public class WinningLotto implements Lotto {
         return numbers;
     }
 
+    private void validateNumbers(List<Integer> list){
+        if(list.size() != Lotto.LOTTO_NUMBER_COUNT_MAX){
+            throw new IllegalArgumentException("당첨 번호 입력 개수가 유효하지 않습니다.");
+        }
+
+        Set<Integer> chkDuplicationSet = new HashSet<>(list);
+        if(chkDuplicationSet.size() != Lotto.LOTTO_NUMBER_COUNT_MAX){
+            throw new IllegalArgumentException("중복 된 당첨 번호가 있습니다.");
+        }
+    }
 }
