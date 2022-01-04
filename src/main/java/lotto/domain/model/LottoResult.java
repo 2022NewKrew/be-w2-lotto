c22d8383779c4062d15f9978d32083f959b1ff3d
kusakina0608 <platina.kusakina@gmail.com>
2022-01-04 18:57:43 +0900

    LottoResult 클래스 재구성
    로또 결과와 관계없는 멤버 제거

--- src/main/java/lotto/domain/model/LottoResult.java
+++ src/main/java/lotto/domain/model/LottoResult.java
@@ -1,5 +1,8 @@
 package lotto.domain.model;
 
+import lotto.domain.constant.Prize;
+import lotto.domain.constant.Rank;
+
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -7,43 +10,27 @@ import java.util.List;
 import java.util.Map;
 
 public class LottoResult {
-    private static Map<Integer, Prize> intToPrizeMapping = new HashMap<>();
-
-    private List<Integer> winningNumbers;
     Map<Prize, Integer> winningResult;
 
-    static {
-        for (Prize prize : Prize.values()) {
-            intToPrizeMapping.put(prize.getNumberOfMatches(), prize);
-        }
-    }
-
-    public LottoResult(List<Integer> winningNumbers) {
-        this.winningNumbers = winningNumbers;
+    public LottoResult(List<Rank> rankList) {
         winningResult = new HashMap<>();
-        Arrays.stream(Prize.values()).forEach(prize -> winningResult.put(prize, 0));
-    }
-
-    public void countNumberOfMatches(Lotto lotto) {
-        int matched = lotto.getNumbers().stream().reduce(0, (count, number) -> {
-            return count + (winningNumbers.contains(number) ? 1 : 0);
-        });
-        Prize prize = intToPrizeMapping.get(matched);
-        if (prize != null) {
-            winningResult.put(prize, winningResult.get(prize) + 1);
-        }
+        Arrays.stream(Prize.values())
+                .filter(prize -> prize != Prize.FAILED)
+                .forEach(prize -> winningResult.put(prize, 0));
+        rankList.stream()
+                .map(rank -> Prize.of(rank))
+                .forEach(prize -> winningResult.put(prize, winningResult.get(prize) + 1));
     }
 
     public double getEarningsRate(int investment) {
         BigInteger totalPrize = winningResult.entrySet().stream()
                 .filter(item -> item.getValue() > 0)
                 .map(item -> new BigInteger(Integer.toString(item.getKey().getWinnings() * item.getValue())))
-                .reduce(new BigInteger("0"), (total, prize) -> total.add(prize));
+                .reduce(new BigInteger("0"), (total, winnings) -> total.add(winnings));
         return totalPrize.doubleValue() / (new BigInteger(Integer.toString(investment))).doubleValue();
     }
 
     public Integer getCount(Prize prize) {
         return winningResult.get(prize);
     }
-
 }
