03bbfd59badfc04ea12580f699d435f2badb5b12
geatrigger-kim <geatrigger.kim@kakaocorp.com>
2022-01-04 19:28:45 +0900

    Add unit test

---
+++ src/test/java/lotto/domain/LottoStatisticTest.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class LottoStatisticTest {
+    LottoStatistic lottoStatistic;
+    @BeforeEach
+    void setUp() {
+        lottoStatistic = new LottoStatistic();
+    }
+
+    @Test
+    @DisplayName("통계 초기화 후 수익률 계산")
+    void initialize() {
+        List<Lotto> userLottos = new ArrayList<Lotto>();
+        userLottos.add(new Lotto("1, 2, 3, 4, 5, 6"));
+        userLottos.add(new Lotto("4, 5, 6, 7, 8, 9"));
+        userLottos.add(new Lotto("4, 5, 6, 7, 11, 12"));
+        Lotto pastWinningLotto = new Lotto("4, 5, 6, 7, 8, 9");
+        lottoStatistic.initialize(userLottos, pastWinningLotto);
+        Map winningCountMap = new HashMap<Integer, Long>();
+        winningCountMap.put(6, 1L);
+        winningCountMap.put(5, 0L);
+        winningCountMap.put(4, 1L);
+        winningCountMap.put(3, 1L);
+        assertThat(lottoStatistic.getWinningCountMap()).isEqualTo(winningCountMap);
+        assertThat(lottoStatistic.getProfitPercentage()).isEqualTo(66668400);
+    }
+
+}
---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class LottoTest {
+    Lotto lotto;
+    @BeforeEach
+    void setUp() {
+        lotto = new Lotto();
+    }
+
+    @Test
+    @DisplayName("겹치는 숫자 개수세기")
+    void calculateEqualCount() {
+        lotto.initialize("1, 2, 3, 4, 5, 6");
+        Lotto anotherLotto = new Lotto();
+        anotherLotto.initialize("4, 5, 6, 7, 8, 9");
+        assertThat(Lotto.calculateEqualCount(lotto, anotherLotto)).isEqualTo(3);
+    }
+}
