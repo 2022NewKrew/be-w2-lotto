5ef11aecd24d960a40448b31ef8f63d838458941
GoonLee <auteur.kor@gmail.com>
2022-01-04 14:17:47 +0900

    Apply code review: use underbar in the integer literals

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -2,10 +2,10 @@ package domain;
 
 public enum Rank {
     MISS(0, 0, "3개 미만 일치 (0원)"),
-    FOURTH(3, 5000, "3개 일치 (5000원)"),
-    THIRD(4, 50000, "4개 일치 (50000원)"),
-    SECOND(5, 1500000, "5개 일치 (1500000원)"),
-    FIRST(6, 2000000000, "6개 일치 (2000000000원)");
+    FOURTH(3, 5_000, "3개 일치 (5000원)"),
+    THIRD(4, 50_000, "4개 일치 (50000원)"),
+    SECOND(5, 1_500_000, "5개 일치 (1500000원)"),
+    FIRST(6, 2_000_000_000, "6개 일치 (2000000000원)");
 
     private final int matchCount;
     private final long prize;
