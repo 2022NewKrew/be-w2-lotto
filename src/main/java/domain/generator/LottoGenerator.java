bbc5a76f5e063a06e531f27e4eb556ed7e6201fd
roger.lee <2wjdwo97@gmail.com>
2022-01-04 12:41:10 +0900

    Make lotto generator generate lotto with constant values

--- src/main/java/domain/generator/LottoGenerator.java
+++ src/main/java/domain/generator/LottoGenerator.java
@@ -5,19 +5,20 @@ import domain.Lotto;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoGenerator {
-    private static final List<Integer> lottoNumbers = new ArrayList<Integer>(
-            List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-                    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-                    41, 42, 43, 44, 45));
+    private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_START_NUM = 1;
+    private static final int LOTTO_END_NUM = 45;
 
     public static Lotto generate() {
+        List<Integer> lottoNumbers = IntStream.rangeClosed(LOTTO_START_NUM, LOTTO_END_NUM).boxed()
+                .collect(Collectors.toList());
         Collections.shuffle(lottoNumbers);
 
-        List<Integer> generatedLottoNumbers = new ArrayList<>(lottoNumbers.subList(0, 6));
+        List<Integer> generatedLottoNumbers = new ArrayList<>(lottoNumbers.subList(0, LOTTO_SIZE));
         Collections.sort(generatedLottoNumbers);
 
         return new Lotto(generatedLottoNumbers);
