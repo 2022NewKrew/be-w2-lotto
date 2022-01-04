2dc0f310e449bf4282bdeac8e73438649ab3ac60
edward.baek <edward.baek@kakaocorp.com>
2022-01-04 19:07:38 +0900

    refactor : 결과를 내는 방법을 수정

--- app/src/test/java/lotto/domain/winning/WinningResultTest.java
+++ app/src/test/java/lotto/domain/winning/WinningResultTest.java
@@ -1,8 +1,9 @@
 package lotto.domain.winning;
 
 import lotto.domain.lotto.Lotto;
-import lotto.dto.WinningResultResponse;
+import lotto.dto.WinningResultOutput;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -10,22 +11,46 @@ import java.util.*;
 public class WinningResultTest {
 
     @Test
+    @DisplayName("당첨자 확인 테스트")
     void lottoWinningResultTest() {
         //given
         WinningNumber winningNumber = new WinningNumber(Arrays.asList(5, 12, 25, 1, 2, 3));
-        List<Lotto> lottos = new ArrayList<>();
-        lottos.add(Lotto.createLotto(() -> Arrays.asList(5, 12, 25, 32, 35, 39)));
-        lottos.add(Lotto.createLotto(() -> Arrays.asList(5, 23, 32, 37, 38, 43)));
+        BonusNumber bonusNumber = new BonusNumber(4);
+        List<Lotto> lotteries = new ArrayList<>();
+        lotteries.add(Lotto.createLotto(() -> Arrays.asList(5, 12, 25, 32, 35, 39)));
+        lotteries.add(Lotto.createLotto(() -> Arrays.asList(5, 23, 32, 37, 38, 43)));
         int lottoPrice = 2000;
 
-        Map<Integer, Integer> expected = new HashMap<>();
-        expected.put(3, 1);
-        expected.put(4, 0);
-        expected.put(5, 0);
-        expected.put(6, 0);
+        Map<String, Long> expected = new HashMap<>();
+        expected.put("FIFTH", 1L);
+        expected.put("UNRANKED", 1L);
         //when
-        WinningResult winningResult = new WinningResult(winningNumber);
-        WinningResultResponse result = winningResult.winningResultRequest(lottos, lottoPrice);
+        WinningResult winningResult = new WinningResult(winningNumber, bonusNumber);
+        WinningResultOutput result = winningResult.winningResultRequest(lotteries, lottoPrice);
+        //then
+        Assertions.assertThat(result.getWinningResult()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("보너스 당첨자 확인 테스트")
+    void bonusLottoWinningResultTest() {
+        //given
+        WinningNumber winningNumber = new WinningNumber(Arrays.asList(5, 12, 25, 1, 2, 3));
+        BonusNumber bonusNumber = new BonusNumber(4);
+        List<Lotto> lotteries = new ArrayList<>();
+        lotteries.add(Lotto.createLotto(() -> Arrays.asList(5, 12, 25, 32, 35, 39)));
+        lotteries.add(Lotto.createLotto(() -> Arrays.asList(5, 12, 25, 1, 2, 4)));
+        lotteries.add(Lotto.createLotto(() -> Arrays.asList(5, 23, 32, 37, 38, 43)));
+        int lottoPrice = 3000;
+
+        Map<String, Long> expected = new HashMap<>();
+        expected.put("FIFTH", 1L);
+        expected.put("SECOND", 1L);
+        expected.put("UNRANKED", 1L);
+
+        //when
+        WinningResult winningResult = new WinningResult(winningNumber, bonusNumber);
+        WinningResultOutput result = winningResult.winningResultRequest(lotteries, lottoPrice);
         //then
         Assertions.assertThat(result.getWinningResult()).isEqualTo(expected);
     }
