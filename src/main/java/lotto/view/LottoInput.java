d655753028f1a468106f6746fdbd5419ec801d6f
geatrigger-kim <geatrigger.kim@kakaocorp.com>
2022-01-04 15:30:00 +0900

    Change dummy input to scanner input

--- src/main/java/lotto/view/LottoInput.java
+++ src/main/java/lotto/view/LottoInput.java
@@ -2,14 +2,23 @@ package lotto.view;
 
 import lotto.domain.Lotto;
 
+import java.util.Scanner;
+
 public class LottoInput {
+    Scanner sc;
+
+    public LottoInput() {
+        sc = new Scanner(System.in);
+    }
+
     public int enterMoney() {
-        return 14000;
+        return sc.nextInt();
     }
 
     public Lotto enterPastWinningLotto() {
         Lotto lotto = new Lotto();
-        String lottoString = "1, 2, 3, 4, 5, 6";
+//        String lottoString = "1, 2, 3, 4, 5, 6";
+        String lottoString = sc.next();
         lotto.initialize(lottoString);
         return lotto;
     }
