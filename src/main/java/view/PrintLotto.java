ba6ba4432a458f6270c4993ac86667dbfe2cd85e
pi.raspberry <pi.raspberry@kakaocorp.com>
2022-01-04 20:39:30 +0900

    Fix typo for 2nd winning money
    - 2등 당첨금액 출력이 잘못되어 수정함.

--- src/main/java/view/PrintLotto.java
+++ src/main/java/view/PrintLotto.java
@@ -56,7 +56,7 @@ public class PrintLotto {
         System.out.println("3개 일치 (5000원)- " + matchCounts.get(4) + "개");
         System.out.println("4개 일치 (50000원)- " + matchCounts.get(3) + "개");
         System.out.println("5개 일치 (1500000원)- " + matchCounts.get(2) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치(1500000원)- " + matchCounts.get(1) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치(30000000원)- " + matchCounts.get(1) + "개");
         System.out.println("6개 일치 (2000000000원)- " + matchCounts.get(0) + "개");
         System.out.println("총 수익률은 " + calcProfit(matchCounts) * 100 / money + "%입니다.");
     }
