608ca88f4c9770030f3272525e06ba680a82d45c
Gunyoung-Kim <gun025bba@naver.com>
2022-01-04 11:31:16 +0900

    RewardForCorrect class reward에 절사표현 추가

--- app/src/main/java/be/w2/lotto/exceptions/NonValidPurchaseAmountException.java
+++ app/src/main/java/be/w2/lotto/exceptions/NonValidPurchaseAmountException.java
@@ -1,7 +1,5 @@
 package be.w2.lotto.exceptions;
 
-import be.w2.lotto.exceptions.BusinessException;
-
 public class NonValidPurchaseAmountException extends BusinessException {
     public NonValidPurchaseAmountException(String message) {
         super(message);
--- app/src/main/java/be/w2/lotto/result/RewardForCorrect.java
+++ app/src/main/java/be/w2/lotto/result/RewardForCorrect.java
@@ -2,10 +2,10 @@ package be.w2.lotto.result;
 
 public enum RewardForCorrect {
 
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 1500000),
-    SIX(6, 2000000000);
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    SIX(6, 2_000_000_000);
 
     private int howManyCorrect;
     private int reward;
