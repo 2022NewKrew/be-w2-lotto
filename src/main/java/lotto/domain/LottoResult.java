fb0f30e58e780d4a1451e6fde5961b58c005093d
anika.dovob <anika.dovob@kakaocorp.com>
2022-01-04 15:56:54 +0900

    refactor: 숫자에 언더바 사용 및 valueOf 메서드의 반복적인 if문 제거

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
+import java.util.stream.Stream;
+
 public enum LottoResult {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
     NONE(0, 0);
 
     private final int matchCount;
@@ -16,19 +18,10 @@ public enum LottoResult {
     }
 
     public static LottoResult valueOf(int matchCount) {
-        if (matchCount == 6) {
-            return LottoResult.FIRST;
-        }
-        if (matchCount == 5) {
-            return LottoResult.SECOND;
-        }
-        if (matchCount == 4) {
-            return LottoResult.THIRD;
-        }
-        if (matchCount == 3) {
-            return LottoResult.FOURTH;
-        }
-        return LottoResult.NONE;
+        return Stream.of(LottoResult.values())
+                .filter(result -> result.getMatchCount() == matchCount)
+                .findAny()
+                .orElse(LottoResult.NONE);
     }
 
     public int getReward() {
