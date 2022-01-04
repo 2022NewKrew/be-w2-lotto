d6accc5231c5bccda1576a4cf9e665eafd1f3893
jb.jang <jb.jang@kakaocorp.com>
2022-01-04 12:53:20 +0900

    feat: 보너스 볼 입력 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -21,6 +21,11 @@ public class InputView {
         return userInput;
     }
 
+    public static int getBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return sc.nextInt();
+    }
+
     public static List<Integer> getWinningNumberList() {
         List<Integer> result = new ArrayList<>();
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
