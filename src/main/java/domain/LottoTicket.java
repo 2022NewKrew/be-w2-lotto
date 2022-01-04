3d2de678d7d12ed9a2b1a33cf9a0fcf38aa6078d
eastclo <goldeastcloud@gmail.com>
2022-01-05 05:55:02 +0900

    3단계 구현 - 로또 구매 상태에 대한 enum과  LottoTicket에 해당 정보 추가

--- src/main/java/domain/LottoTicket.java
+++ src/main/java/domain/LottoTicket.java
@@ -6,12 +6,18 @@ import java.util.List;
 public final class LottoTicket {
 
     private final List<Integer> numbers;
+    private final LottoStatus purchaseWay;
 
-    public LottoTicket(List<Integer> numbers) {
+    public LottoTicket(List<Integer> numbers, LottoStatus purchaseWay) {
         this.numbers = Collections.unmodifiableList(numbers);
+        this.purchaseWay = purchaseWay;
     }
 
     public List<Integer> getLottoNumbers() {
         return numbers;
+    }
+
+    public LottoStatus getPurchaseWay() {
+        return purchaseWay;
     }
 }
