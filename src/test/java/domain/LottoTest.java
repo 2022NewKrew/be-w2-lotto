98b315d58a1dd9562d2e9a7f3c629508492255cb
binary.yun <binary.yun@kakaocorp.com>
2022-01-04 20:03:37 +0900

    test: Ball 테스트 추가

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -9,11 +9,13 @@ import java.util.stream.Collectors;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
-    public static final int MIN_LOTTO_NUMBER = 1;
-    public static final int MAX_LOTTO_NUMBER = 45;
-    private final Lotto lotto = new Lotto(createBall(List.of(1, 2, 3, 4, 5, 6)));
+    private final Lotto lotto = new Lotto(createBallList(List.of(1, 2, 3, 4, 5, 6)));
 
-    private List<Ball> createBall(List<Integer> list) {
+    private Ball createBall(int number) {
+        return new Ball(number);
+    }
+    
+    private List<Ball> createBallList(List<Integer> list) {
         return list.stream()
                 .map(Ball::new)
                 .collect(Collectors.toList());
@@ -26,5 +28,18 @@ class LottoTest {
 
         assertThat(numbers.size())
                 .isEqualTo(Lotto.NUMBER);
+    }
+    
+    @DisplayName("특정 숫자를 포함하고 있는지 검증")
+    @Test
+    void isContainBall() {
+        Ball includingBall = createBall(1);
+        Ball notIncludingBall = createBall(45);
+
+        boolean contain = lotto.containBall(includingBall);
+        boolean notContain = lotto.containBall(notIncludingBall);
+
+        assertThat(contain).isTrue();
+        assertThat(notContain).isFalse();
     }
 }
