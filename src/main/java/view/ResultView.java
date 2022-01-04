03d44356faa024a9cfe0dc003fbd93f14d95acc9
punsoo <sonu.sohn@kakaocorp.com@gmail.com>
2022-01-05 05:06:36 +0900

    feat:ResultView 수동 구매 출력 포함

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -4,14 +4,15 @@ import domain.Lottos;
 import domain.WinningStatistics;
 
 public class ResultView {
-    private static final String MESSAGE_PURCHASING_COUNT = "개를 구입하셨습니다.";
+    private static final String MESSAGE_PURCHASING_COUNT = "수동으로 %s장, 자동으로 %s개를 구매했습니다.";
     private static final String MESSAGE_WINNING_RESULT = String.format("%s%s%s", "당첨 통계", System.lineSeparator(), "---------");
     private static final String MESSAGE_RANK_COUNT = "%s (%d원)- %d개";
     private static final String MESSAGE_TOTAL_PROFITS = "총 수익률은 %d%%입니다.";
     private ResultView() {}
 
-    public static void printLottoNumbers(Lottos lottos) {
-        System.out.println(lottos.lottos().size() + MESSAGE_PURCHASING_COUNT);
+    public static void printLottoNumbers(Lottos lottos, long manualLottoCount) {
+        System.out.println();
+        System.out.println(String.format(MESSAGE_PURCHASING_COUNT, manualLottoCount, lottos.size() - manualLottoCount));
         lottos.lottos()
                 .forEach(lotto -> System.out.println(lotto.lottoNumbers()));
         System.out.println();
