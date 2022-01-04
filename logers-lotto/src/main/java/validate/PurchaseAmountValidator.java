98e47ff3201706c7f6fbb17a0fb07422a7a43cd6
rogers.t <rogers.t@kakaocorp.com>
2022-01-04 21:47:17 +0900

    구매금액 Validator 구현 및 테스트 완료

--- logers-lotto/src/main/java/validate/PurchaseAmountValidator.java
+++ logers-lotto/src/main/java/validate/PurchaseAmountValidator.java
@@ -4,19 +4,20 @@ import domain.Lotto;
 
 public class PurchaseAmountValidator extends Validator {
     @Override
-    protected boolean supports(Object value) {
-        return value instanceof Integer;
+    protected boolean supports(Object param) {
+        return param instanceof Integer;
     }
 
     @Override
-    public void validateValue(Object value) {
-        Integer number = (Integer) value;
-        if(number % Lotto.PRICE != 0){
+    protected void validateValue(Object param) throws IllegalArgumentException {
+        Integer price = (Integer) param;
+
+        if(price % Lotto.PRICE != 0){
             throw new IllegalArgumentException(
                     "로또 가격의 단위는 ".concat(String.valueOf(Lotto.PRICE)).concat("입니다."));
         }
 
-        if(number <= 0){
+        if(price <= 0){
             throw new IllegalArgumentException("구매 금액은 양수로 적어주세요.");
         }
     }
