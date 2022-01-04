8e9d223068fe66c47fcd7d65a918cfe36c089e71
= <=>
2022-01-04 13:06:00 +0900

    결과 확인 및 출력

--- app/src/main/java/lotto/LottoBuyer.java
+++ app/src/main/java/lotto/LottoBuyer.java
@@ -1,16 +1,21 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoBuyer {
     private static final int LOTTO_PRICE = 1000;
     private final List<Lotto> lottoList = new ArrayList<>();
+    private final Map<Integer, Integer> result = new HashMap<>();
 
     public final void start() {
         Input.openScanner();
         buyLotto(Input.getMoney());
         OutputView.printLottoList(lottoList);
+        checkAllLotto();
+        OutputView.printResult(result);
         Input.closeScanner();
     }
 
@@ -26,5 +31,23 @@ public class LottoBuyer {
         for(int i = 0; i < countLotto; i++) {
             lottoList.add(LottoMachine.generateLotto());
         }
+    }
+
+    public final void checkAllLotto() {
+        final List<Integer> winningNumbers = Input.getLastWinningNumbers();
+        for(Lotto lotto : lottoList) {
+            int matched = lotto.match(winningNumbers);
+            putResult(matched);
+        }
+    }
+
+    private final void putResult(int key) {
+        if(key < 3) return;
+
+        if(!result.containsKey(key)){
+            result.put(key, 1);
+            return;
+        }
+        result.put(key, result.get(key) + 1);
     }
 }
