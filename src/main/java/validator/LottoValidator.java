a8f60ab3481f2da3c778e84dbe323c08ff2d6e26
binary.yun <binary.yun@kakaocorp.com>
2022-01-05 01:37:23 +0900

    refactor: LottoValidator의 매직 넘버 상수화

--- src/main/java/validator/LottoValidator.java
+++ src/main/java/validator/LottoValidator.java
@@ -1,10 +1,12 @@
 package validator;
 
+import domain.Lotto;
+
 public class LottoValidator {
-    public static final String ILLEGAL_PURCHASE_AMOUNT_MESSAGE = "[ERROR] 1000원 단위의 숫자를 입력해 주세요.\n";
+    public static final String ILLEGAL_PURCHASE_AMOUNT_MESSAGE = "[ERROR] " + Lotto.PRICE + "원 단위의 숫자를 입력해 주세요.\n";
 
     public static void assertValidPurchaseAmount(int purchaseAmount) throws NumberFormatException  {
-        if ((purchaseAmount % 1000) != 0) {
+        if ((purchaseAmount % Lotto.PRICE) != 0) {
             throw new NumberFormatException(ILLEGAL_PURCHASE_AMOUNT_MESSAGE);
         }
     }
