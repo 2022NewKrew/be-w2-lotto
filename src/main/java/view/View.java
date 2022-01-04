35fdcab0b04fb5fab286c2128b4f89928894113e
frank.burger <frank.burger@kakaocorp.com>
2022-01-05 03:20:39 +0900

    refactor: View의 forEach 로직 모두 람다화

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -4,10 +4,10 @@ import domain.Prize;
 import domain.Report;
 import domain.lottery.Ticket;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class View {
     private final static Scanner scanner = new Scanner(System.in);
@@ -19,25 +19,22 @@ public class View {
 
     public void showBoughtTickets(List<Ticket> tickets) {
         System.out.println(tickets.size() + "개를 구매했습니다.");
-        tickets.forEach(ticket -> System.out.println(ticket.getNumbers()));
+        System.out.println(tickets.stream().map(ticket -> ticket.getNumbers().toString()).collect(Collectors.joining(System.lineSeparator())));
         System.out.println();
     }
 
     public List<Integer> getResultNumbersByPrompt() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        List<Integer> numbers = new ArrayList<>();
-        Arrays.asList(scanner.nextLine().split(",")).forEach(numberString -> numbers.add(Integer.parseInt(numberString.strip())));
-        return numbers;
+        return Arrays.asList(scanner.nextLine().split(",")).stream().map(String::strip).map(Integer::valueOf).collect(Collectors.toList());
     }
 
     public int getResultBonusBallByPrompt() {
         System.out.println("보너스 볼을 입력해 주세요.");
-        int bonusBall = Integer.parseInt(scanner.nextLine());
-        System.out.println();
-        return bonusBall;
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public void showReport(Report report) {
+        System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---------");
         System.out.println(Prize.FIFTH.getWinningMatchingCount() + "개 일치 (" + Prize.FIFTH.getValue() + "원)- " + report.prizeCount.get(Prize.FIFTH) + "개");
