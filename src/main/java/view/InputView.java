b05a60d10fd9380324b1b5f0ff2c45daef0e5aa8
GoonLee <auteur.kor@gmail.com>
2022-01-04 15:45:33 +0900

    Fix: remove the unused constant

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,8 +3,6 @@ package view;
 import java.util.Set;
 
 public interface InputView {
-    int LOTTERY_UNIT_PRICE = 1000;
-
     long getLotteryBoughtPrice();
 
     Set<Integer> getLotteryNumberList();
