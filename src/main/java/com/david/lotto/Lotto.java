9b18ec95823e3b01bd387db45788b5a49f147d4f
david.dh <david.dh@kakaocorp.com>
2022-01-04 17:49:23 +0900

    allLottoNumber변수 static 제거, 생성자 synchronized 제거

--- src/main/java/com/david/lotto/Lotto.java
+++ src/main/java/com/david/lotto/Lotto.java
@@ -8,16 +8,14 @@ import java.util.stream.IntStream;
 
 public class Lotto {
 
-    private static final List<Integer> allLottoNumber = IntStream.range(1, 46).boxed().collect(Collectors.toList());
+    private final List<Integer> allLottoNumber = IntStream.range(1, 46).boxed().collect(Collectors.toList());
     private final List<Integer> lottoNumber = new ArrayList<>();
 
     public Lotto() {
-        synchronized (this) {
-            Collections.shuffle(allLottoNumber);
-            for (int i = 0; i < 6; i++) {
-                int number = allLottoNumber.get(i);
-                lottoNumber.add(number);
-            }
+        Collections.shuffle(allLottoNumber);
+        for (int i = 0; i < 6; i++) {
+            int number = allLottoNumber.get(i);
+            lottoNumber.add(number);
         }
         Collections.sort(lottoNumber);
     }
