e251219c09aef5fe7361e58c2c5190c34e9d9bb1
eastclo <goldeastcloud@gmail.com>
2022-01-05 05:57:04 +0900

    3단계 구현 - 로또 수동 생성기 추가, 로또 생성시 상태 정보 인자로 넘기도록 수정

--- src/main/java/domain/ManualLottoGenerator.java
+++ src/main/java/domain/ManualLottoGenerator.java
@@ -7,19 +7,21 @@ import java.util.List;
 
 import static view.Sentence.*;
 
-public class WinningLottoGenerator implements LottoGenerator{
+public class ManualLottoGenerator implements LottoGenerator{
 
     private final LottoServiceInputController inputController;
 
-    public WinningLottoGenerator(LottoServiceInputController inputController) {
+    public ManualLottoGenerator(LottoServiceInputController inputController) {
         this.inputController = inputController;
+
     }
 
     @Override
     public LottoTicket getLottoTicket() {
-        List<Integer> numbers = inputController.getLastWeekWinningNumbers();
+        List<Integer> numbers = inputController.getManualLottoNumber();
+
         if(ConditionCheck.isValidLottoNumber(numbers)) {
-            return new LottoTicket(numbers, LottoStatus.WINNING);
+            return new LottoTicket(numbers, LottoStatus.MANUAL);
         }
 
         System.out.println(ERROR_INAPPROPRIATE_LOTTO_NUMBER.getString());
