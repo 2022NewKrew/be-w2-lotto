dbe95625984a2ae8779a00b8982ccb72537e9bab
anika.dovob <anika.dovob@kakaocorp.com>
2022-01-04 18:30:12 +0900

    feat: WinningNumbers에 보너스 볼 추가

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -4,17 +4,26 @@ import java.util.Iterator;
 
 public class WinningNumbers implements Iterable<LottoNumber> {
     private final LottoNumbers winningNumbers;
+    private final LottoNumber bonusNumber;
 
-    private WinningNumbers(LottoNumbers winningNumbers) {
+    private WinningNumbers(LottoNumbers winningNumbers, LottoNumber bonusNumber) {
         this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
     }
 
-    public static WinningNumbers from(LottoNumbers winningNumbers) {
-        return new WinningNumbers(winningNumbers);
+    public static WinningNumbers from(LottoNumbers winningNumbers, LottoNumber bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 볼은 당첨 번호와 중복될 수 없습니다.");
+        }
+        return new WinningNumbers(winningNumbers, bonusNumber);
     }
 
     public LottoNumbers getWinningNumbers() {
         return winningNumbers;
+    }
+
+    public LottoNumber getBonusNumber() {
+        return bonusNumber;
     }
 
     @Override
--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.Test;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class WinningNumbersTest {
 
-    @DisplayName("지난 주 당첨 번호 모음 생성 테스트")
+    @DisplayName("지난 주 당첨 번호 모음과 보너스 볼 생성 테스트")
     @Test
     public void create() {
         Set<LottoNumber> numbers = new HashSet<>();
@@ -18,9 +19,26 @@ class WinningNumbersTest {
             numbers.add(LottoNumber.from(i));
         }
         LottoNumbers lottoNumbers = LottoNumbers.from(numbers);
+        LottoNumber bonusNumber = LottoNumber.from(7);
 
-        WinningNumbers winningNumbers = WinningNumbers.from(lottoNumbers);
+        WinningNumbers winningNumbers = WinningNumbers.from(lottoNumbers, bonusNumber);
 
         assertThat(winningNumbers.getWinningNumbers()).isEqualTo(lottoNumbers);
+        assertThat(winningNumbers.getBonusNumber()).isEqualTo(bonusNumber);
+    }
+
+    @DisplayName("보너스 볼이 당첨 번호와 중복될 경우 테스트")
+    @Test
+    public void createDuplicatedBonusNumber() {
+        Set<LottoNumber> numbers = new HashSet<>();
+        for (int i = 1; i <= 6; i++) {
+            numbers.add(LottoNumber.from(i));
+        }
+        LottoNumbers lottoNumbers = LottoNumbers.from(numbers);
+        LottoNumber bonusNumber = LottoNumber.from(6);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> WinningNumbers.from(lottoNumbers, bonusNumber))
+                .withMessage("보너스 볼은 당첨 번호와 중복될 수 없습니다.");
     }
 }
