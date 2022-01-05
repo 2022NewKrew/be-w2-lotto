c81ce36a0e607df985c193809783c5b9dff464d5
jb.jang <jb.jang@kakaocorp.com>
2022-01-05 11:17:33 +0900

    feat: InputView에 수동 구매 입력 추가 및 중복 로직 메소드 추출

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -21,14 +21,39 @@ public class InputView {
         return userInput;
     }
 
+    public static int getManualPurchaseCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        int userInput;
+        try {
+            userInput = Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("로또 수는 숫자로 입력해주세요.");
+            userInput = getPurchaseAmount();
+        }
+        return userInput;
+    }
+
+    public static List<List<Integer>> getManualPurchaseNumberList(int manualPurchaseCount) {
+        List<List<Integer>> result = new ArrayList<>();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        for (int i = 0; i < manualPurchaseCount; i++) {
+            result.add(inputToIntegerList());
+        }
+        return result;
+    }
+
     public static int getBonusNumber() {
         System.out.println("보너스 볼을 입력해 주세요.");
         return sc.nextInt();
     }
 
     public static List<Integer> getWinningNumberList() {
-        List<Integer> result = new ArrayList<>();
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return inputToIntegerList();
+    }
+
+    private static List<Integer> inputToIntegerList() {
+        List<Integer> result = new ArrayList<>();
         List<String> userInput = new ArrayList<>(Arrays.asList(sc.nextLine().split(SEPARATOR)));
         for (String stringNumber : userInput) {
             result.add(Integer.parseInt(stringNumber));
