eaff0ee5042e4db6a8fcacdea84e8185035ee435
rogers.t <rogers.t@kakaocorp.com>
2022-01-06 01:32:42 +0900

    feat : 당첨번호에 보너스볼 로직 추가

--- logers-lotto/src/main/java/domain/WinningNumbers.java
+++ logers-lotto/src/main/java/domain/WinningNumbers.java
@@ -7,30 +7,10 @@ import java.util.List;
 public class WinningNumbers {
     private final List<Integer> numbers;
 
-    public WinningNumbers(List<Integer> numbers) {
-        validate(numbers);
+    public WinningNumbers(List<Integer> numbers, int bonusNumber) {
+        Validator.validate(numbers, bonusNumber);
         this.numbers = new ArrayList<>(numbers);
         Collections.sort(this.numbers);
-    }
-
-    private void validate(List<Integer> numbers) {
-        validateNumberOfValues(numbers.size());
-        for(Integer number : numbers){
-            validateOneValue(number);
-        }
-    }
-
-    private void validateNumberOfValues(int numberOfValue){
-        if(numberOfValue != Lotto.NUMBER_OF_WRITE_NUMBER){
-            throw new IllegalArgumentException(
-                    "당첨번호의 갯수는 ".concat(String.valueOf(Lotto.NUMBER_OF_WRITE_NUMBER)).concat("개 입력해주세요."));
-        }
-    }
-
-    private void validateOneValue(Integer number) throws IllegalArgumentException{
-        if(number <= 0 || number > 45){
-            throw new IllegalArgumentException("당첨번호는 1이상 45 이하입니다.");
-        }
     }
 
     public int getMatchedNumber(Lotto lotto){
@@ -56,5 +36,44 @@ public class WinningNumbers {
 
     public List<Integer> getNumbers() {
         return Collections.unmodifiableList(numbers);
+    }
+
+
+    private static class Validator{
+        static void validate(List<Integer> numbers, int bonusNumber) {
+            validateNumbers(numbers);
+            validateBonusNumber(numbers, bonusNumber);
+        }
+
+        static void validateNumbers(List<Integer> numbers){
+            validateSize(numbers.size());
+            for(Integer number : numbers){
+                validateOneNumber(number);
+            }
+        }
+
+        static void validateBonusNumber(List<Integer> numbers, int bonusNumber){
+            validateOneNumber(bonusNumber);
+            validateNumbersDoesntHasBonus(numbers, bonusNumber);
+        }
+
+        static void validateSize(int numberOfValue){
+            if(numberOfValue != Lotto.NUMBER_OF_WRITE_NUMBER){
+                throw new IllegalArgumentException(
+                        "당첨번호의 갯수는 ".concat(String.valueOf(Lotto.NUMBER_OF_WRITE_NUMBER)).concat("개 입력해주세요."));
+            }
+        }
+
+        static void validateOneNumber(Integer number) throws IllegalArgumentException{
+            if(number <= 0 || number > 45){
+                throw new IllegalArgumentException("당첨번호는 1이상 45 이하입니다.");
+            }
+        }
+
+        static void validateNumbersDoesntHasBonus(List<Integer> numbers, int bonusNumber){
+            if(numbers.contains(bonusNumber)){
+                throw new IllegalArgumentException("보너스 숫자는 당첨번호들과 달라야 합니다.");
+            }
+        }
     }
 }
--- logers-lotto/src/main/java/dto/input/WinningNumbersDto.java
+++ logers-lotto/src/main/java/dto/input/WinningNumbersDto.java
@@ -5,27 +5,18 @@ import java.util.List;
 
 public class WinningNumbersDto {
     private final List<Integer> winningNumbers;
-<<<<<<< HEAD
     private final Integer bonusNumber;
 
     public WinningNumbersDto(List<Integer> winningNumbers, Integer bonusNumber) {
         this.winningNumbers = winningNumbers;
-        this.bonusNumber = bonusNumber;
-=======
 
-    public WinningNumbersDto(List<Integer> winningNumbers) {
-        this.winningNumbers = winningNumbers;
->>>>>>> 7f4f290 (refactor : ResultOutputDto 수정)
+        this.bonusNumber = bonusNumber;
     }
 
     public List<Integer> getWinningNumbers() {
         return Collections.unmodifiableList(winningNumbers);
     }
-<<<<<<< HEAD
-
     public int getBonusNumbers() {
         return this.bonusNumber;
     }
-=======
->>>>>>> 7f4f290 (refactor : ResultOutputDto 수정)
 }
