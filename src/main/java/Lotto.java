09ec1dd8d2b40619d611629368107ddc2209f3b4
GoonLee <auteur.kor@gmail.com>
2022-01-05 12:46:41 +0900

    Improve: add controller class

--- src/main/java/Lotto.java
+++ src/main/java/Lotto.java
@@ -1,38 +1,8 @@
-import domain.*;
-import view.ConsoleInputView;
-import view.ConsoleOutputView;
-import view.InputView;
-import view.OutputView;
-
-import java.util.List;
-import java.util.Set;
+import controller.LottoController;
 
 public class Lotto {
-    private static final int LOTTERY_NUMBER_START = 1;
-    private static final int LOTTERY_NUMBER_END = 45;
-    private static final int LOTTERY_NUMBER_COUNT = 6;
-
-    private static final int LOTTERY_UNIT_PRICE = 1000;
 
     public static void main(String[] args) {
-        Rule lotteryRule = new Rule.Builder()
-                .lotteryNumberStart(LOTTERY_NUMBER_START)
-                .lotteryNumberEnd(LOTTERY_NUMBER_END)
-                .lotteryNumberCount(LOTTERY_NUMBER_COUNT)
-                .lotteryUnitPrice(LOTTERY_UNIT_PRICE)
-                .build();
-        InputView inputView = new ConsoleInputView(lotteryRule);
-        OutputView outputView = new ConsoleOutputView();
-
-        long lotteryBoughtPrice = inputView.getLotteryBoughtPrice();
-        List<Lottery> lotteries = LotteryFactory.buildLotteries(lotteryBoughtPrice, lotteryRule);
-        outputView.printBoughtLotteries(lotteries);
-
-        Set<Integer> winningLotteryNumberSet = inputView.getLotteryNumberSet();
-        int winningLotteryBonusNumber = inputView.getBonusNumber();
-        WinningLottery winningLottery = new WinningLottery(winningLotteryNumberSet, winningLotteryBonusNumber);
-        Result result = Result.of(lotteryBoughtPrice, lotteries, winningLottery);
-
-        outputView.printResult(result);
+        LottoController.run();
     }
 }
