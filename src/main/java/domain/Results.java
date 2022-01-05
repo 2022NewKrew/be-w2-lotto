8fc2f9c3c6808c1324f4890973d66635059207c9
green.day <green.day@kakaocorp.com>
2022-01-05 11:13:58 +0900

    refactor: ResultGenerator 클래스 삭제, Results 클래스에서 Result 인스턴스 리스트 생성하도록 변경

--- src/main/java/domain/Results.java
+++ src/main/java/domain/Results.java
@@ -1,13 +1,31 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Results {
     private final List<Result> results;
 
     public Results(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNum) {
-        this.results = ResultGenerator.generate(lottos, winningNumbers, bonusNum);
+        this.results = generate(lottos,winningNumbers,bonusNum);
+    }
+
+    private static List<Result> generate(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNum) {
+        List<Result> results = new ArrayList<>();
+        List<Rank> resultRanks = lottos.stream()
+                .map(lotto -> lotto.getRank(winningNumbers, bonusNum))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+        for (Rank rank : Rank.values()) {
+            int count = (int) resultRanks.stream().filter(rank::equals).count();
+            results.add(new Result(rank, count));
+        }
+
+        return results;
     }
 
     public List<Result> getResults() {
