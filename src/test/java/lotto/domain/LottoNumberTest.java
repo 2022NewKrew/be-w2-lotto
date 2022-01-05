626d7979b9139ca2bedcc2adea30d1d65c2b818f
jun.dev <jun.dev@kakaocorp.com>
2022-01-05 10:53:25 +0900

    test: calculateMatchCount 테스트코드 추가

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class LottoNumberTest {
+
+    @Test
+    @DisplayName("올바른_당첨번호_일치_개수를_반환한다.")
+    void calculateMatchCount() {
+        List<Integer>listA = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+        List<Integer>listB = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+        LottoNumber lottoA = new LottoNumber(listA);
+        LottoNumber lottoB = new LottoNumber(listB);
+
+        assertThat(lottoA.calculateMatchCount(lottoB)).isEqualTo(6);
+    }
+}
