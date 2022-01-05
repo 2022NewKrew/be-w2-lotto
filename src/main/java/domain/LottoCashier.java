5afed778ca3edfc9fc181386d7fa2463547ff9be
Dongchan KIM <fromecha@gmail.com>
2022-01-05 16:30:00 +0900

    [feat] lottocashier 검증 추가

--- src/main/java/domain/LottoCashier.java
+++ src/main/java/domain/LottoCashier.java
@@ -1,9 +1,18 @@
 package domain;
 
+import exception.InvalidPurchaseAmount;
+
 public class LottoCashier {
     public static final int MINIMUM_PURCHASE_AMOUNT = 1000;
 
     public static int buyLottos(int purchasedAmount) {
+        validatePurchaseAmount(purchasedAmount);
         return purchasedAmount / MINIMUM_PURCHASE_AMOUNT;
+    }
+
+    private static void validatePurchaseAmount(int purchasedAmount) {
+        if (purchasedAmount < 1000) {
+            throw new InvalidPurchaseAmount(InvalidPurchaseAmount.MINIMUM_PURCHASE_AMOUNT);
+        }
     }
 }
