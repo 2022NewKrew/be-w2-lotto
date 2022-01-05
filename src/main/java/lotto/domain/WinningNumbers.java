a0825e31a712e47b249fdb9233252e5e8535ba82
anika.dovob <anika.dovob@kakaocorp.com>
2022-01-05 14:54:48 +0900

    refactor: WinningNumbers 유효성 검사를 생성자에서 수행하도록 수정

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -7,14 +7,14 @@ public class WinningNumbers implements Iterable<LottoNumber> {
     private final LottoNumber bonusNumber;
 
     private WinningNumbers(LottoNumbers winningNumbers, LottoNumber bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 볼은 당첨 번호와 중복될 수 없습니다.");
+        }
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
 
     public static WinningNumbers from(LottoNumbers winningNumbers, LottoNumber bonusNumber) {
-        if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 볼은 당첨 번호와 중복될 수 없습니다.");
-        }
         return new WinningNumbers(winningNumbers, bonusNumber);
     }
 
