17b58b336956c6d6005b62e084ff9b7085632e2b
edward.baek <edward.baek@kakaocorp.com>
2022-01-06 00:20:44 +0900

    refactor : lottostart를 여러개의 함수로 분리

--- app/src/main/java/lotto/controller/LottoController.java
+++ app/src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,10 @@
 package lotto.controller;
 
 import lotto.domain.LottoSystem;
-import lotto.domain.lotto.Lotto;
-import lotto.domain.lotto.RandomLottoStrategy;
-import lotto.domain.winning.BonusNumber;
-import lotto.domain.winning.WinningNumber;
+import lotto.domain.lotto.Lotteries;
+import lotto.domain.lotto.strategy.RandomLottoStrategy;
+import lotto.domain.lotto.number.BonusNumber;
+import lotto.domain.lotto.number.WinningNumber;
 import lotto.dto.WinningResultOutput;
 import lotto.util.StringParsingUtil;
 import lotto.view.IOView;
@@ -19,6 +19,10 @@ public class LottoController {
 
     private final Scanner sc;
     private final LottoSystem lottoSystem;
+    private Lotteries lotteries;
+    private WinningNumber winningNumber;
+    private BonusNumber bonusNumber;
+    private int purchasePrice;
 
     public LottoController(Scanner sc) {
         this.sc = sc;
@@ -26,20 +30,33 @@ public class LottoController {
     }
 
     public void lottoStart() {
-        int purchasePrice = IOView.inputToInt("구입금액을 입력해 주세요.", sc);
-        List<Lotto> lotteries = lottoSystem.buyLotto(purchasePrice);
-        LottoView.createLottoView(lotteries).printView();
+        buyLotto();
+        inputWinningNumber();
+        inputBonusNumber();
+        result();
+    }
 
-        String winningNumbers = IOView.inputToString("지난 주 당첨 번호를 입력해 주세요.", sc);
-        List<String> parsed = StringParsingUtil.parse(winningNumbers, ",");
-        WinningNumber winningNumber = lottoSystem.inputWinningNumber(
-                parsed.stream()
-                        .map(Integer::parseInt)
-                        .collect(Collectors.toList())
-        );
+    private void buyLotto() {
+        this.purchasePrice = IOView.inputToInt("구입금액을 입력해 주세요.", sc);
+        int manualCount = IOView.inputToInt("수동으로 구매할 로또 수를 입력해 주세요.", sc);
+
+        List<List<Integer>> manualLotteries = IOView.inputToDoubleList("수동으로 구매할 번호를 입력해 주세요.", sc, manualCount);
+
+        this.lotteries = lottoSystem.buyLotto(purchasePrice, manualLotteries);
+        LottoView.createLottoView(lotteries.forView(), manualCount).printView();
+    }
+
+    private void inputWinningNumber() {
+        List<Integer> winningNumbers = IOView.inputToList("지난 주 당첨 번호를 입력해 주세요.", sc);
+        this.winningNumber = lottoSystem.inputWinningNumber(winningNumbers);
+    }
+
+    private void inputBonusNumber() {
         int bonusNumberInput = IOView.inputToInt("보너스 볼을 입력해 주세요.", sc);
-        BonusNumber bonusNumber = lottoSystem.inputBonusNumber(bonusNumberInput);
+        this.bonusNumber = lottoSystem.inputBonusNumber(bonusNumberInput, winningNumber);
+    }
 
+    private void result() {
         WinningResultOutput result = lottoSystem.winningResult(lotteries, winningNumber, bonusNumber, purchasePrice);
         WinningResultView.createWinningResultView(result.getWinningResult(), result.getEarningRate()).printView();
     }
