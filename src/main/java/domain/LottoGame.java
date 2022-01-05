4ac92715cba9f5272a347b4637b7a168860a77de
pride.chicken <pride.chicken@kakaocorp.com>
2022-01-05 17:39:58 +0900

    [feature] 수동 복권 입력받는 메소드 구현
    - LottoGame에서 수동 복권 숫자, 번호를 입력받는 메소드를 구현했다.

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -2,6 +2,8 @@ package domain;
 
 import view.LottoView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LottoGame {
@@ -12,11 +14,13 @@ public class LottoGame {
     private int bonusNumber;
     private Person person;
     private LottoView lottoView;
+    private final List<String> manualLottoList = new ArrayList<>();
 
     private final Scanner scanner = new Scanner(System.in);
 
-    public LottoGame(){
+    public LottoGame() {
         consoleInputPrice();
+        consoleManualInput();
         init();
         lottoView.showLotto();
 
@@ -28,24 +32,38 @@ public class LottoGame {
         lottoView.showResult();
     }
 
-    private void init(){
-        person = new Person(inputPrice);
+    private void consoleManualInput() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        int manualLottoCount = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        for (int i = 0; i < manualLottoCount; i++) {
+            manualLottoList.add(scanner.nextLine());
+        }
+    }
+
+    private void init() {
+        if(manualLottoList.size() == 0){
+            person = new Person(inputPrice);
+            lottoView = new LottoView(person);
+            return;
+        }
+        person = new Person(inputPrice, manualLottoList);
         lottoView = new LottoView(person);
     }
 
-    private void consoleInputBonusNumber(){
+    private void consoleInputBonusNumber() {
         System.out.println("보너스 볼을 입력해 주세요.");
         this.bonusNumber = scanner.nextInt();
         scanner.close();
     }
 
     private void consoleInputLottoWinningNumbers() {
-        scanner.nextLine();
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         lottoWinningNumbers = scanner.nextLine();
     }
 
-    private void consoleInputPrice(){
+    private void consoleInputPrice() {
         System.out.println("구입금액을 입력해 주세요.");
         inputPrice = scanner.nextInt();
     }
