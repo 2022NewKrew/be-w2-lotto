0356ba22e5c78e9ca6be624e52a08a62e5ef278e
LeeA0 <lay0711@naver.com>
2022-01-06 01:21:35 +0900

    feat: 값 검증 메소드 추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -7,20 +7,30 @@ import java.util.stream.Collectors;
 import util.RandomUtil;
 
 public class Lotto {
+    private final static String NUMBER_SIZE_ERROR_MESSAGE = "6개의 번호를 입력해주세요.";
+
+    private static final int LOTTO_NUMBERS_SIZE = 6;
 
     private final List<LottoNumber> lottoNumbers;
 
-    public Lotto(List<LottoNumber> lottoNumbers) {
+    private Lotto(List<LottoNumber> lottoNumbers) {
         this.lottoNumbers = new ArrayList<>(lottoNumbers);
     }
 
     public static Lotto purchaseLotto() {
         List<Integer> shuffledNumbers = RandomUtil.generateRandomNumbers();
+        validate(shuffledNumbers);
         List<LottoNumber> lottoNumbers = shuffledNumbers.stream()
             .map(LottoNumber::new)
             .sorted()
             .collect(Collectors.toList());
         return new Lotto(lottoNumbers);
+    }
+
+    private static void validate(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(NUMBER_SIZE_ERROR_MESSAGE);
+        }
     }
 
     public List<LottoNumber> getLottoNumbers() {
--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -11,13 +11,13 @@ public class Lottos {
 
     private final List<Lotto> Lottos;
 
-    public Lottos(List<Lotto> Lottos) {
+    private Lottos(List<Lotto> Lottos) {
         this.Lottos = new ArrayList<>(Lottos);
     }
 
-    public static Lottos purchaseLottos(int countOfPurchaseLotto){
-        List<Lotto> lottos = IntStream.range(0,countOfPurchaseLotto)
-            .mapToObj(i->Lotto.purchaseLotto())
+    public static Lottos purchaseLottos(int countOfPurchaseLotto) {
+        List<Lotto> lottos = IntStream.range(0, countOfPurchaseLotto)
+            .mapToObj(i -> Lotto.purchaseLotto())
             .collect(Collectors.toList());
         return new Lottos(lottos);
     }
@@ -26,7 +26,7 @@ public class Lottos {
         return Collections.unmodifiableList(Lottos);
     }
 
-    public int size(){
+    public int size() {
         return this.Lottos.size();
     }
 }
