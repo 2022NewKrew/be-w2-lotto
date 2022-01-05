6455c938d6855e31a156d608198a30c2d651097d
ethan <ethan.337@kakaocorp.com>
2022-01-05 13:22:04 +0900

    build : add dependency spark and implement helloApp

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,11 +1,13 @@
 package lotto;
 
 import lotto.view.LottoView;
+import static spark.Spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        LottoView view = new LottoView();
-        view.start();
+//        LottoView view = new LottoView();
+//        view.start();
+        get("/hello", (req, res) -> "Hello World!");
     }
 }
