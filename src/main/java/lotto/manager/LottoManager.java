c1b30e219067c9ce49a628f7707e2b226dd986c5
Kina <38154862+kusakina0608@users.noreply.github.com>
2022-01-05 22:11:23 +0900

    [kina.lee] 로또 3단계 구현 (#272)
    * 프로젝트 생성 및 초기 설정
    
    * gradle dependency 추가(assertj)
    
    * Application 클래스 추가
    
    * 테스트 클래스 추가
    
    * Prize 열거체 추가
    
    * Lotto 클래스 추가
    
    * LottoResult 클래스 추가
    
    * CustomScanner 클래스 추가
    
    * LottoCliView 클래스 추가
    
    * LottoManager 클래스 추가
    
    * 코드 정리
    
    * 수익률 추가
    
    * Prize 열거체를 Prize, Rank로 세분화
    
    * CustomScanner에서 출력문 제거
    
    * 입력값 검증, Lotto 결과 확인을 위한 LottoChecker 클래스 추가
    
    * LottoResult 클래스 재구성
    
    로또 결과와 관계없는 멤버 제거
    
    * LottoMessage 열거체 추가
    
    프로그램에서 사용되는 문자열을 LottoMessage에서 관리하도록 변경.
    
    * LottoManager 클래스 변경
    
    * 수동 구매시 출력되는 메시지 추가
    
    * 수동 구매시 사용되는 생성자 추가
    
    * 수동 구매 기능 추가
    
    * Lotto를 상속받는 WinningLotto 클래스 추가

--- src/main/java/lotto/manager/LottoManager.java
+++ src/main/java/lotto/manager/LottoManager.java
@@ -1,6 +1,6 @@
 package lotto.manager;
 
-import lotto.domain.LottoChecker;
+import lotto.domain.WinningLotto;
 import lotto.domain.constant.LottoMessage;
 import lotto.domain.model.Lotto;
 import lotto.domain.model.LottoResult;
@@ -16,41 +16,78 @@ public class LottoManager {
     private CustomScanner customScanner;
     private LottoCliView lottoCliView;
 
-    private LottoChecker lottoChecker;
+    private WinningLotto winningLotto;
 
     private List<Lotto> lottoList;
     private LottoResult lottoResult;
 
     private int money;
+    int remainingMoney;
+    private int manualAmount;
 
     private LottoManager() {
         customScanner = new CustomScanner();
         lottoCliView = LottoCliView.getInstance();
-        lottoChecker = LottoChecker.getInstance();
+        lottoList = new ArrayList<>();
     }
 
     public static LottoManager getInstance() {
         return INSTANCE;
     }
 
-    private void purchaseLotto() {
+    private void getMoney() {
         try {
             lottoCliView.printMessage(LottoMessage.INPUT_MONEY.toString());
-            money = customScanner.readInt();
+            remainingMoney = money = customScanner.readInt();
         } catch (NumberFormatException e) {
             lottoCliView.printMessage(e.getMessage());
-            purchaseLotto();
+            getMoney();
         }
     }
 
-    private void printPurchasedLotto() {
-        lottoList = new ArrayList<>();
-        int remainingMoney = money;
+    private void getManualAmount() {
+        try {
+            lottoCliView.printMessage(LottoMessage.INPUT_MANUAL_AMOUNT.toString());
+            manualAmount = customScanner.readInt();
+            // TODO: 수동으로 구매할 로또 수 검증(투입금액으로 살 수 있는지)
+        } catch (NumberFormatException e) {
+            lottoCliView.printMessage(e.getMessage());
+            getManualAmount();
+        }
+    }
+
+    private void manualPurchaseAllLotto() {
+        lottoCliView.printMessage(LottoMessage.INPUT_MANUAL_NUMBERS.toString());
+        for (int i = 0; i < manualAmount && remainingMoney / 1000 > 0; i++) {
+            manualPurchaseLotto();
+
+        }
+    }
+
+    private void manualPurchaseLotto() {
+        List<Integer> numbers = customScanner.readCommaSeparatedInt();
+        try {
+            lottoList.add(new Lotto(numbers));
+            remainingMoney -= 1000;
+        } catch (IllegalArgumentException e) {
+            lottoCliView.printMessage(e.getMessage());
+            manualPurchaseLotto();
+        }
+    }
+
+    private void purchaseAutomaticLotto() {
         while (remainingMoney / 1000 > 0) {
             lottoList.add(new Lotto());
             remainingMoney -= 1000;
         }
-        lottoCliView.printMessage(lottoList.size() + LottoMessage.INPUT_MONEY_SUCCESS.toString());
+    }
+
+    private void showPurchasedResults() {
+        lottoCliView.printMessage(String.format(
+                LottoMessage.INPUT_MONEY_SUCCESS.toString(),
+                manualAmount,
+                lottoList.size() - manualAmount
+        ));
         lottoCliView.printLottoList(lottoList);
         lottoCliView.printMessage("");
     }
@@ -61,7 +98,7 @@ public class LottoManager {
             List<Integer> numbers = customScanner.readCommaSeparatedInt();
             lottoCliView.printMessage(LottoMessage.INPUT_BONUS_NUMBER.toString());
             int bonusNumber = customScanner.readInt();
-            lottoChecker.init(numbers, bonusNumber);
+            winningLotto = new WinningLotto(numbers, bonusNumber);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             initializeLottoChecker();
@@ -69,10 +106,13 @@ public class LottoManager {
     }
 
     public void run() {
-        purchaseLotto();
-        printPurchasedLotto();
+        getMoney();
+        getManualAmount();
+        manualPurchaseAllLotto();
+        purchaseAutomaticLotto();
+        showPurchasedResults();
         initializeLottoChecker();
-        lottoResult = lottoChecker.checkAll(lottoList);
+        lottoResult = winningLotto.checkAll(lottoList);
         lottoCliView.printLottoResult(lottoResult);
         lottoCliView.printEarningsRate(lottoResult.getEarningsRate(money));
     }
