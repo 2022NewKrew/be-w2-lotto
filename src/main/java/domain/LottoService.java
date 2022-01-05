a86a209ec23475451154ce4a303dd1382bc6db4a
green.day <green.day@kakaocorp.com>
2022-01-05 19:32:41 +0900

    feat: RandomLottoNumberGenerator 클래스 삭제 (필요없어보여서), LottoService에서 하도록 변경

--- src/main/java/domain/LottoService.java
+++ src/main/java/domain/LottoService.java
@@ -2,10 +2,14 @@ package domain;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class LottoService {
     private static final double COST_PER_LOTTO = 1000.0;
+    private static final Integer LOTTO_NUMBER_SIZE = 6;
+    private static final Integer MIN_NUMBER_RANGE = 1;
+    private static final Integer MAX_NUMBER_RANGE = 46;
 
     public LottoService() {
     }
@@ -24,7 +28,7 @@ public class LottoService {
     }
 
     private static List<Lotto> buyAutoLottos(int autoCount) {
-        return Stream.generate(RandomLottoNumberGenerator::generate)
+        return Stream.generate(LottoService::generateRandomLottoNumbers)
                 .limit(autoCount)
                 .map(Lotto::new)
                 .collect(Collectors.toUnmodifiableList());
@@ -38,6 +42,18 @@ public class LottoService {
 
     private static int calcLottoCount(int money) {
         return (int) Math.floor(money / COST_PER_LOTTO);
+    }
+
+    private static List<Integer> generateRandomLottoNumbers() {
+        List<Integer> lottoNumberList = IntStream.range(MIN_NUMBER_RANGE, MAX_NUMBER_RANGE)
+                .boxed()
+                .collect(Collectors.toList());
+
+        Collections.shuffle(lottoNumberList);
+
+        return lottoNumberList.stream()
+                .limit(LOTTO_NUMBER_SIZE)
+                .collect(Collectors.toUnmodifiableList());
     }
 
 
