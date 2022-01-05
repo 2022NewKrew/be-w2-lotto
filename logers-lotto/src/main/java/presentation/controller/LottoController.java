1bdbe8547414549d74e27ca744f0d41dcdf23626
rogers.t <rogers.t@kakaocorp.com>
2022-01-06 01:24:01 +0900

    refactor : 구조 개선
    * InputView, OutputView 인터페이스 추가
    * 기존 InputView를 PurchaseInputView, WinningNumberInputView로 분리 후 InputView의 해당 위치에 이 객체들을 이용
    * 다음 리팩토링을 위해 InputView을 InputViewz로 이름 변경

--- logers-lotto/src/main/java/presentation/controller/LottoController.java
+++ logers-lotto/src/main/java/presentation/controller/LottoController.java
@@ -1,67 +1,18 @@
 package presentation.controller;
 
 import domain.LottoOrder;
-<<<<<<< HEAD
-import domain.RewardResult;
-import domain.WinningNumbers;
-import dto.input.PurchaseDto;
-import dto.input.WinningNumbersDto;
-import dto.output.PurchaseResultDto;
-import dto.output.RewardResultDto;
-import domain.factory.LottoOrderFactory;
-import domain.factory.WinningNumberFactory;
-import presentation.view.output.ErrorOutputView;
-import presentation.view.output.OutputView;
-import presentation.view.output.PurchaseOutputView;
-import presentation.view.output.ResultOutputView;
-
-public class LottoController {
-    public OutputView getPurchaseResult(PurchaseDto purchaseDto){
-        try{
-            return getPurchaseResultView(purchaseDto);
-        }catch (RuntimeException exception){
-            return new ErrorOutputView(exception);
-        }
-    }
-
-    private OutputView getPurchaseResultView(PurchaseDto purchaseDto){
-        int purchaseAmount = purchaseDto.getPurchaseAmount();
-        LottoOrder lottoOrder = LottoOrderFactory.getInstance(purchaseAmount).orElseThrow();
-
-        PurchaseResultDto purchaseResultDto = new PurchaseResultDto(lottoOrder.getLottos());
-        return new PurchaseOutputView(purchaseResultDto);
-    }
-
-    public OutputView getLottoResult(WinningNumbersDto winningNumbersDto){
-        try{
-            return getLottoResultView(winningNumbersDto);
-        }catch (RuntimeException exception){
-            return new ErrorOutputView(exception);
-        }
-    }
-
-    private OutputView getLottoResultView(WinningNumbersDto winningNumbersDto){
-        LottoOrder lottoOrder = LottoOrderFactory.getInstance().orElseThrow();
-        WinningNumbers winningNumbers = WinningNumberFactory.getInstance(
-                winningNumbersDto.getWinningNumbers(), winningNumbersDto.getBonusNumbers()).orElseThrow();
-
-        RewardResult rewardResult = lottoOrder.getResult(winningNumbers);
-        RewardResultDto rewardResultDto = new RewardResultDto(rewardResult.getRewardToCount(), rewardResult.getProfitPercent());
-        return new ResultOutputView(rewardResultDto);
-=======
 import domain.WinningNumber;
 import dto.InputResultDto;
 import dto.ResultDto;
-import presentation.view.OutputView;
+import presentation.view.LottoResultOutputView;
 
 public class LottoController {
-    public OutputView getLottoResult(InputResultDto inputResultDto){
+    public LottoResultOutputView getLottoResult(InputResultDto inputResultDto){
         LottoOrder lottoOrder = inputResultDto.getLottoOrder();
         WinningNumber winningNumber = inputResultDto.getWinningNumber();
 
         ResultDto resultDto = lottoOrder.getResult(winningNumber);
 
-        return new OutputView(resultDto);
->>>>>>> b09511c (refactor : 구조 개선)
+        return new LottoResultOutputView(resultDto);
     }
 }
