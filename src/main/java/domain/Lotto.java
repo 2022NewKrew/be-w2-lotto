610f203878663135a532555ae969cf3638fb4dc3
tjddls1124 <tjddls1124@nate.com>
2022-01-05 19:05:01 +0900

    feature Lotto Class 생성

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,4 +1,27 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Lotto {
+
+    private static final List<Integer> numbers = new ArrayList<>();
+    public static final int LOTTO_MOD = 46;
+    public static final int NUM_OF_LOTTO = 6;
+
+    public Lotto(List<Integer> numbers){
+        numbers.addAll(numbers);
+    }
+
+    public Lotto(){
+        for (int i = 0; i < NUM_OF_LOTTO; i++) {
+            numbers.add(makeRandomLottoNum());
+        }
+    }
+
+    private int makeRandomLottoNum(){
+        return new Random().nextInt()%LOTTO_MOD;
+    }
+
 }
