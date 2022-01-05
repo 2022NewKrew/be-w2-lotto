403860dffbfeceaf79b7302336f10a5e32062ce1
tjddls1124 <tjddls1124@nate.com>
2022-01-05 18:26:44 +0900

    feature Input Class 생성

--- src/main/java/view/LottoInput.java
+++ src/main/java/view/LottoInput.java
@@ -1,5 +1,26 @@
 package view;
 
-public class LottoInput {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
+public class LottoInput {
+    private static Scanner sc = new Scanner(System.in);
+    private static final String NUMBER_SPLIT_REGEX = ", ";
+
+    public static void getBuyLottoPrize(){
+        System.out.println("구입 금액을 입력해 주세요.");
+        sc.nextInt();
+    }
+
+    public static void getLastPrizeNum(){
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        sc.next();
+
+    }
+    private List<Integer> parseLottoNumbers(String numberString){
+        String[] spt = numberString.split(NUMBER_SPLIT_REGEX);
+        return  Arrays.stream(spt).map(Integer::parseInt).collect(Collectors.toList());
+    }
 }
