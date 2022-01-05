eb176664e87eb2f8b71e35e8e1d7efeb4e898f50
vince.joe <vince.joe@kakaocorp.com>
2022-01-05 18:02:41 +0900

    docs: 주석 추가

--- src/main/java/lotto/domain/LottoMatchingResult.java
+++ src/main/java/lotto/domain/LottoMatchingResult.java
@@ -11,6 +11,9 @@ public class LottoMatchingResult {
         this.occurrencesByPrize = occurrencesByPrize;
     }
 
+    /**
+     * @return 로또 수익률(%)
+     */
     public float getEarningsRate() {
         long purchaseCosts = PRICE * occurrencesByPrize.values().stream()
                 .reduce(0L, Long::sum);
@@ -20,6 +23,10 @@ public class LottoMatchingResult {
         return ((float) (prizeMoneySum - purchaseCosts) / purchaseCosts) * 100;
     }
 
+    /**
+     * @param prize 몇번 나왔는지 확인할 {@link Prize}
+     * @return {@code prize}가 나온 횟수
+     */
     public Long getOccurrences(Prize prize) {
         return occurrencesByPrize.getOrDefault(prize, 0L);
     }
