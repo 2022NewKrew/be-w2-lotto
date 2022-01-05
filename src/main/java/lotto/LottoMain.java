8ece88f210c601e7369e27209483f3f2f714822a
anika.dovob <anika.dovob@kakaocorp.com>
2022-01-05 14:12:33 +0900

    refactor: LottoMain과 LottoController 분리

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -1,28 +1,10 @@
 package lotto;
 
-import lotto.domain.*;
 import lotto.view.ConsoleInputView;
 import lotto.view.ConsoleOutputView;
-import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class LottoMain {
     public static void main(String[] args) {
-        InputView inputView = new ConsoleInputView();
-        OutputView outputView = new ConsoleOutputView();
-
-        int inputPrice = inputView.getInputPrice();
-        outputView.printLottoTicketCount(inputPrice);
-
-        Money inputMoney = new Money(inputPrice);
-        LottoNumbersGenerator lottoNumbersGenerator = new RandomLottoNumberGenerator();
-        LottoTickets lottoTickets = LottoMachine.issue(inputMoney, lottoNumbersGenerator);
-        outputView.printLottoTickets(lottoTickets);
-
-        LottoNumbers lottoNumbers = inputView.getWinningNumbers();
-        LottoNumber bonusNumber = inputView.getBonusNumber();
-        WinningNumbers winningNumbers = WinningNumbers.from(lottoNumbers, bonusNumber);
-        LottoStatistics lottoStatistics = new LottoStatistics(winningNumbers, lottoTickets);
-        outputView.printLottoStatistics(inputMoney, lottoStatistics);
+        LottoController.start(new ConsoleInputView(), new ConsoleOutputView());
     }
 }
