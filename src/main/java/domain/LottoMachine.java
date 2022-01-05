aaf7c738e9348223b73805da085e56e1ad754c62
mimseong <parkseongmin98@gmail.com>
2022-01-05 23:59:44 +0900

    [chore] 숫자 6 대신 변수 사용

--- src/main/java/domain/LottoMachine.java
+++ src/main/java/domain/LottoMachine.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LottoMachine {
-    private  static final RandomMaker randomMaker = new RandomMaker();
+    private static final RandomMaker randomMaker = new RandomMaker();
     private static final int NUMBER_OF_LOTTERY_NUMBERS = 6;
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
@@ -29,10 +29,10 @@ public class LottoMachine {
     private static Lotto buy() {
         List<Integer> numbers = new ArrayList<>();
 
-        for(int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++)
+        for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++)
             numbers.add(i);
         Collections.shuffle(numbers);
-        List<Integer> sortedNumbers = numbers.subList(0,6);
+        List<Integer> sortedNumbers = numbers.subList(0, NUMBER_OF_LOTTERY_NUMBERS);
         Collections.sort(sortedNumbers);
         return new Lotto(sortedNumbers);
     }
