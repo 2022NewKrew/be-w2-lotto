fba97b295f7c84be5dd406615669b2ed9a50ad77
LeeA0 <lay0711@naver.com>
2022-01-05 19:39:38 +0900

    feat: RandomNumbers에서 숫자 6개를 뽑도록 로직 변경

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -5,12 +5,9 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import util.RandomUtil;
 
 public class Lotto {
-    private static final int LOTTO_SIZE_MIN = 0;
-    private static final int LOTTO_SIZE_MAX = 6;
 
     private final List<LottoNumber> lottoNumbers;
 
@@ -20,9 +17,8 @@ public class Lotto {
 
     public static Lotto purchaseLotto() {
         List<Integer> shuffledNumbers = RandomUtil.generateRandomNumbers();
-        List<LottoNumber> lottoNumbers = IntStream.range(LOTTO_SIZE_MIN, LOTTO_SIZE_MAX)
-            .map(shuffledNumbers::get)
-            .mapToObj(LottoNumber::new)
+        List<LottoNumber> lottoNumbers = shuffledNumbers.stream()
+            .map(LottoNumber::new)
             .sorted(Comparator.comparing(LottoNumber::getNumber))
             .collect(Collectors.toList());
         return new Lotto(lottoNumbers);
