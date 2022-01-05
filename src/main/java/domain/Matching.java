64998afa258903c5d701e2324052b9bf6779f10b
HYEONSEOK1 <gustir929@gmail.com>
2022-01-05 15:01:37 +0900

    - refacotr : Matching 생성자 for문 Prize.values()로 통합

--- src/main/java/domain/Matching.java
+++ src/main/java/domain/Matching.java
@@ -10,12 +10,18 @@ public class Matching {
 
     private EnumMap<Prize, Long> matchingMap = new EnumMap<>(Prize.class);
 
+    /*NO_PRIZE 는 초기값으로 넣어줄 필요가 없다.*/
     public Matching() {
-        for (Prize prize : List.of(FIRST_PRIZE, SECOND_PRIZE, THIRD_PRIZE, FOURTH_PRIZE, FIFTH_PRIZE)) {
-            matchingMap.putIfAbsent(prize, 0L);
+        for (Prize prize : Prize.values()) {
+            addInitialMathingMap(prize);
         }
     }
-
+    public void addInitialMathingMap(Prize prize)
+    {
+        if(prize == NO_PRIZE)
+            return;
+        matchingMap.putIfAbsent(prize, 0L);
+    }
     public void addMatchingMap(Prize prize) {
         if(prize == NO_PRIZE)
             return;
