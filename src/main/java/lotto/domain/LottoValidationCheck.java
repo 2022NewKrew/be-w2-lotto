8ed02d9a51167604d8735ee225a4172514070e5c
jun.dev <jun.dev@kakaocorp.com>
2022-01-05 16:19:49 +0900

    refactor: Validation로직 리팩토링

--- src/main/java/lotto/domain/LottoValidationCheck.java
+++ src/main/java/lotto/domain/LottoValidationCheck.java
@@ -4,6 +4,6 @@ import static lotto.domain.LottoSetting.LOTTO_PRICE;
 
 public class LottoValidationCheck {
     static Integer userMakeCountValidation(Integer payment, Integer userMakeCount){
-        return (Math.min(payment / LOTTO_PRICE, (Math.max(0, userMakeCount))   )); //유효한 input으로 수정
+        return (Math.min(payment / LOTTO_PRICE, (Math.max(0, userMakeCount))   )); //유효한 유저생성 로또번호 개수로 수정
     }
 }
