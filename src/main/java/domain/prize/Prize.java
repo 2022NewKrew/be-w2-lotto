a7d1e610b3629fe2eb3fd0206ef53ca7906020eb
devwihtpug <devwithpug@kakao.com>
2022-01-05 17:52:05 +0900

    step-3: Prize 클래스 생성자 리팩터링

--- src/main/java/domain/prize/Prize.java
+++ src/main/java/domain/prize/Prize.java
@@ -7,22 +7,16 @@ import java.util.stream.Collectors;
 
 public enum Prize {
 
-    FIRST_PRIZE(6, 2_000_000_000),
+    FIRST_PRIZE(6, false, 2_000_000_000),
     SECOND_PRIZE(5, true, 30_000_000),
-    THIRD_PRIZE(5, 1_500_000),
-    FOURTH_PRIZE(4, 50000),
-    FIFTH_PRIZE(3, 5000),
-    NO_PRIZE(0, 0);
+    THIRD_PRIZE(5, false, 1_500_000),
+    FOURTH_PRIZE(4, false, 50000),
+    FIFTH_PRIZE(3, false, 5000),
+    NO_PRIZE(0, false, 0);
 
     private final int matchedNum;
     private final boolean matchedBonus;
     private final int prizeMoney;
-
-    Prize(int matchedNum, int prizeMoney) {
-        this.matchedNum = matchedNum;
-        this.matchedBonus = false;
-        this.prizeMoney = prizeMoney;
-    }
 
     Prize(int matchedNum, boolean matchedBonus, int prizeMoney) {
         this.matchedNum = matchedNum;
