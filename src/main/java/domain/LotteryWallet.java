00b27381e96299f756f42b7870131fa4a73f62e1
frank.burger <frank.burger@kakaocorp.com>
2022-01-06 18:13:29 +0900

    refactor: LotteryWallet객체 추가
    충전(초기 생성)/지불하는 지갑 역할을 맡음.
    
    LotteryWallet객체는 TICKET_PRICE를 알고 있는 특수 Wallet이므로 LotteryWallet으로
    명명함.

---
+++ src/main/java/domain/LotteryWallet.java
@@ -0,0 +1,33 @@
+package domain;
+
+import static domain.util.LotteryConfigs.TICKET_PRICE;
+
+public class LotteryWallet {
+    private int budget;
+
+    public int getSpent() {
+        return spent;
+    }
+
+    private int spent;
+
+    public LotteryWallet(int budget) {
+        this.budget = budget;
+    }
+
+    public void buyTickets(int numberOfTickets) {
+        pay(TICKET_PRICE * numberOfTickets);
+    }
+
+    public int getNumberOfTicketsAffordable() {
+        return budget / TICKET_PRICE;
+    }
+
+    private void pay(int money) {
+        if (budget < money) {
+            throw new IllegalArgumentException("잔액 " + budget + "은 " + money + "을 지불하기에 부족합니다.");
+        }
+        budget -= money;
+        spent += money;
+    }
+}
