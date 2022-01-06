95d7e3415d7ef873c6f540647b3b5ea7fda7f674
frank.burger <frank.burger@kakaocorp.com>
2022-01-06 17:09:19 +0900

    refactor: LotteryNumbersFactory에서 검증 기능 완전 제거
    의존성 있는 부분은 이전 커밋에서 모두 교체하여 안전하게 제거합니다.

--- src/main/java/domain/LotteryNumbersFactory.java
+++ src/main/java/domain/LotteryNumbersFactory.java
@@ -2,7 +2,6 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -18,35 +17,5 @@ public class LotteryNumbersFactory {
         List<Integer> numbers = new ArrayList<>(NUMBERS_DOMAIN.subList(0, NUMBERS_LENGTH));
         Collections.sort(numbers);
         return numbers;
-    }
-
-    public List<Integer> getValidatedNumbers(List<Integer> numbers) {
-        validateNumbersLength(numbers);
-        validateNumbersNonRedundancy(numbers);
-        List<Integer> validatedNumbers = new ArrayList<>();
-        for (var number : numbers) {
-            validateNumberDomain(number);
-            validatedNumbers.add(number);
-        }
-        Collections.sort(validatedNumbers);
-        return validatedNumbers;
-    }
-
-    private void validateNumbersLength(List<Integer> numbers) {
-        if (numbers.size() != NUMBERS_LENGTH) {
-            throw new IllegalArgumentException("로또 번호 " + numbers + "는 " + NUMBERS_LENGTH + "자리가 아닙니다.");
-        }
-    }
-
-    private void validateNumbersNonRedundancy(List<Integer> numbers) {
-        if (!numbers.stream().sequential().allMatch(new HashSet<>()::add)) {
-            throw new IllegalArgumentException("로또 번호 " + numbers + "에 중복이 있습니다.");
-        }
-    }
-
-    private void validateNumberDomain(int number) {
-        if (!NUMBERS_DOMAIN.contains(number)) {
-            throw new IllegalArgumentException("숫자 " + number + " 는 로또 번호 범위(" + NUMBER_DOMAIN_START + "~" + NUMBER_DOMAIN_END + ") 밖입니다.");
-        }
     }
 }
