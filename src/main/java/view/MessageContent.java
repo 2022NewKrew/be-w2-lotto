9086cea6b8ca5e13bff99c91787430c63437b013
binary.yun <binary.yun@kakaocorp.com>
2022-01-07 01:15:00 +0900

    refactor: 입력 문구의 내용들과 관련된 필드를 MessageContent로 분리

---
+++ src/main/java/view/MessageContent.java
@@ -0,0 +1,18 @@
+package view;
+
+public class MessageContent {
+    static final String COMMA = ", ";
+    static final String OPEN_BRACKET = "[";
+    static final String CLOSING_BRACKET = "]";
+    static final String PURCHASE_AMOUNT_REQUEST = "구입 금액을 입력해 주세요.";
+    static final String PURCHASE_QUANTITY = "\n수동으로 %d장, 자동으로 %d개를 구매했습니다.\n";
+    static final String WINNING_LOTTO_REQUEST = "지난 주 당첨 번호를 입력해 주세요.";
+    static final String BONUS_BALL_REQUEST = "보너스 볼을 입력해 주세요.";
+    static final String WINNING_STATISTICS = "\n당첨 통계\n---------";
+    static final String MATCH_COUNT = "%d개 일치";
+    static final String IS_BONUS_MATCHED = ", 보너스 볼 일치";
+    static final String PRIZE_MONEY_AND_COUNT = "(%d원) - %d개\n";
+    static final String TOTAL_RETURN = "총 수익률: %.2f%%입니다.";
+    static final String MANUAL_PURCHASE_QUANTITY_REQUEST = "\n수동으로 구매할 로또 수를 입력해 주세요.";
+    static final String MANUAL_PURCHASE_LOTTO_LIST_REQUEST = "\n수동으로 구매할 번호를 입력해 주세요.";
+}
