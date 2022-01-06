24df8eaeb7e9f83e3b352394ccdfa2ff88ebb8be
anika.dovob <anika.dovob@kakaocorp.com>
2022-01-06 16:15:23 +0900

    feat: controller에 수동 로또 처리하는 부분 추가

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -4,46 +4,39 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.List;
 
 public class LottoController {
+    private static Money inputMoney;
+    private static LottoTicketCount ticketCount;
 
     private LottoController() {
     }
 
     public static void start(InputView inputView, OutputView outputView) {
-        int inputPrice = inputView.getInputPrice();
-        outputView.printLottoTicketCount(inputPrice);
+        calculateTicketCount(inputView);
 
-        Money inputMoney = new Money(inputPrice);
-        LottoNumbersGenerator lottoNumbersGenerator = new RandomLottoNumberGenerator();
-        LottoTickets lottoTickets = LottoMachine.issue(inputMoney, lottoNumbersGenerator);
+        List<String[]> manualLottoNumbers = inputView.getManualLottoNumbers(ticketCount.getManualTicketCount());
+        LottoTickets lottoTickets = LottoMachine.issue(ticketCount, manualLottoNumbers);
         outputView.printLottoTickets(lottoTickets);
 
         WinningNumbers winningNumbers = createWinningNumbers(inputView);
-
         LottoStatistics lottoStatistics = LottoStatistics.of(winningNumbers, lottoTickets, inputMoney);
         outputView.printLottoStatistics(inputMoney, lottoStatistics);
     }
 
+    private static void calculateTicketCount(InputView inputView) {
+        inputMoney = new Money(inputView.getInputPrice());
+        int totalTicketCount = inputMoney.getNumberOfTickets(LottoMachine.TICKET_PRICE);
+        int manualTicketCount = inputView.getManualTicketCount();
+        ticketCount = new LottoTicketCount(totalTicketCount, manualTicketCount);
+    }
+
     private static WinningNumbers createWinningNumbers(InputView inputView) {
-        LottoNumbers lottoNumbers = createLottoNumbers(inputView.getWinningNumbers());
-        LottoNumber bonusNumber = createLottoNumber(inputView.getBonusNumber());
+        String[] inputWinningNumbers = inputView.getWinningNumbers();
+        LottoNumbers lottoNumbers = new ManualLottoNumberGenerator().generateLottoNumbers(inputWinningNumbers);
+        LottoNumber bonusNumber = LottoNumber.from(inputView.getBonusNumber());
 
         return WinningNumbers.from(lottoNumbers, bonusNumber);
-    }
-
-    private static LottoNumbers createLottoNumbers(String[] strings) {
-        return LottoNumbers.from(
-                Arrays.stream(strings)
-                        .map(Integer::parseInt)
-                        .map(LottoNumber::from)
-                        .collect(Collectors.toSet())
-        );
-    }
-
-    private static LottoNumber createLottoNumber(int number) {
-        return LottoNumber.from(number);
     }
 }
