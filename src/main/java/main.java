cbaa9a22c32d128bf91680052f8a3a7d688c907c
HYEONSEOK1 <gustir929@gmail.com>
2022-01-06 12:23:43 +0900

    - fix : main에서 static 변수 삭제

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,24 +6,20 @@ import java.util.*;
 
 
 public class main {
-    private static int playerMoney;
-    private static int payManualCount;
-    private static int payAutoCount;
     public static final int LOTTO_PRICE = 1000;
-    private static Player player;
-    private static Matching matching = new Matching();
 
     public static void main(String[] args) {
-        playerMoney = InputView.getPayPriceInput();
-        payManualCount = InputView.getManualCountInput(playerMoney/LOTTO_PRICE);
-        payAutoCount = (playerMoney - payManualCount*LOTTO_PRICE) / LOTTO_PRICE;
+        int playerMoney = InputView.getPayPriceInput();
+        int payManualCount = InputView.getManualCountInput(playerMoney/LOTTO_PRICE);
+        int payAutoCount = (playerMoney - payManualCount*LOTTO_PRICE) / LOTTO_PRICE;
 
         List<Lotto> manualLottoList  = InputView.getManualLottoInput(payManualCount);
 
-        player = new Player(payAutoCount, manualLottoList);
+        Player player = new Player(payAutoCount, manualLottoList);
         OutputView.printLottoList(player);
         OutputView.PrintLottoSize(payAutoCount, payManualCount);
 
+        Matching matching = new Matching();
         matching.addMatchingLotto(player);
 
         OutputView.printMatchResult(matching, playerMoney);
