14609d5816c4030571dfd16f4b39db2e76d3ef24
jb.jang <jb.jang@kakaocorp.com>
2022-01-06 14:48:10 +0900

    refactor: LottoApplication에서의 작업을 정의

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,11 +1,27 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoApplication {
     public static void main(String[] args) {
-        LottoController lc = new LottoController();
-        lc.buyLotto();
-        lc.checkLotto();
+
+        // 구입금액 입력
+        InputView.getPurchaseAmount();
+        // 수동 구매 수 입력
+        InputView.getManualPurchaseCount();
+        // 로또 구매
+        LottoController.buyLotto();
+        // 구매 로또 출력
+        OutputView.printLottos();
+        // 당첨 번호 입력
+        InputView.getWinningNumberList();
+        // 보너스 볼 입력
+        InputView.getBonusNumber();
+        // 로또 당첨 확인
+        LottoController.checkLotto();
+        // 당첨 통계 출력
+        OutputView.printLottoResults();
     }
 }
