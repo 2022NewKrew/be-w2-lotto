18be0130334fad86c5160d58e1a0a6659fa8e5b1
edward.baek <edward.baek@kakaocorp.com>
2022-01-06 19:18:51 +0900

    refactor : 패키지 변경

---
+++ app/src/main/java/lotto/domain/lotto/result/WinningRanking.java
@@ -0,0 +1,44 @@
+package lotto.domain.lotto.result;
+
+import lotto.domain.lotto.MatchType;
+
+import java.util.function.Function;
+
+public enum WinningRanking {
+    FIRST(v -> v * 2_000_000_000),
+    SECOND(v -> v * 30_000_000),
+    THIRD(v -> v * 1_500_000),
+    FOURTH(v -> v * 50_000),
+    FIFTH(v -> v * 5_000),
+    UNRANKED(v -> 0);
+
+
+    private final Function<Integer, Integer> expression;
+
+    WinningRanking(Function<Integer, Integer> expression) {
+        this.expression = expression;
+    }
+
+    public static WinningRanking match(MatchType matchType) {
+        if (MatchType.SIX.equals(matchType)) {
+            return FIRST;
+        }
+        if (MatchType.FIVE_BONUS.equals(matchType)) {
+            return SECOND;
+        }
+        if (MatchType.FIVE.equals(matchType)) {
+            return THIRD;
+        }
+        if (MatchType.FOUR.equals(matchType)) {
+            return FOURTH;
+        }
+        if (MatchType.THREE.equals(matchType)) {
+            return FIFTH;
+        }
+        return UNRANKED;
+    }
+
+    public int calculatePrizeAccordingToRanking(long count) {
+        return expression.apply((int) count);
+    }
+}
