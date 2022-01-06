cb9b97b0ed7cb4e9c70725fbcc25e3c798e9bdf5
Hongjeongmin <wjdals0471@naver.com>
2022-01-06 15:33:12 +0900

    컨트롤러 분리 및 설정

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,11 +1,30 @@
 package controller;
+
 import static spark.Spark.*;
 
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainController {
     public void run() {
-        get("/hellowrld", (req, res) -> {
-            return "Hello World";
-        });
+        staticFiles.location("/static");
+        get("/lottoForm", this::lottoForm);
+    }
+
+    private String lottoForm(Request req, Response res) {
+        String name = "test1";
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", name);
+
+        return render(map, "lotto_form.html");
+    }
+
+    public static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 }
