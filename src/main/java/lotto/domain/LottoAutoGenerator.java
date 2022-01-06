a20e0ea2fc57051963cf01e5dbf4b3b21db5b784
champ.ion <champ.ion@kakaocorp.com>
2022-01-06 16:35:32 +0900

    Lotto STEP3 - Feedback(PR#268) - [Refactoring] 멀티쓰레딩 환경에서 호출될 시 randomNumbers에 동시에 접근하여 shuffle하는 것을 막기 위해 해당 method에서 clone하여 사용하도록 변경

--- src/main/java/lotto/domain/LottoAutoGenerator.java
+++ src/main/java/lotto/domain/LottoAutoGenerator.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,7 +25,8 @@ public class LottoAutoGenerator {
     }
 
     private @NotNull List<LottoNumber> getRandomLottoNumbers() throws IllegalArgumentException {
-        Collections.shuffle(randomNumbers);
+        List<Integer> tmpList = new ArrayList<>(randomNumbers);
+        Collections.shuffle(tmpList);
         return randomNumbers.subList(0, NUM_OF_LOTTO_NUMBERS_IN_LOTTO)
                 .stream()
                 .map(LottoNumber::new)
