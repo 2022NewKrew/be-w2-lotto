4a30c53156e3bc6a2b60f592b1caada8843f8d4e
Dongchan KIM <fromecha@gmail.com>
2022-01-06 16:29:44 +0900

    [refactor] LottoRank message 삭제

--- src/main/java/domain/LottoRank.java
+++ src/main/java/domain/LottoRank.java
@@ -3,23 +3,21 @@ package domain;
 import java.util.Arrays;
 
 public enum LottoRank {
-    MISS(0, 0, "0개 일치 (0원)- "),
-    ONE(1, 500, "1개 일치 (500원)- "),
-    TWO(2, 1_000, "2개 일치 (1000원)- "),
-    THREE(3, 5_000, "3개 일치 (5000원)- "),
-    FOUR(4, 50_000, "4개 일치 (50000원)- "),
-    FIFTH(5, 1_500_000, "5개 일치 (1500000원)- "),
-    FIFTHWITHBONUS(5, 30_000_000, "5개 일치, 보너스 볼 일치(30000000원) - "),
-    SIX(6, 2_000_000_000, "6개 일치 (2000000000원)- ");
+    MISS(0, 0),
+    ONE(1, 500),
+    TWO(2, 1_000),
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIFTH(5, 1_500_000),
+    FIFTHWITHBONUS(5, 30_000_000),
+    SIX(6, 2_000_000_000);
 
     private final int countOfMatch;
     private final int money;
-    private final String message;
 
-    LottoRank(int countOfMatch, int money, String message) {
+    LottoRank(int countOfMatch, int money) {
         this.countOfMatch = countOfMatch;
         this.money = money;
-        this.message = message;
     }
 
     public int getCountOfMatch() {
@@ -28,10 +26,6 @@ public enum LottoRank {
 
     public int getMoney() {
         return money;
-    }
-
-    public String getMessage() {
-        return message;
     }
 
     public static LottoRank valueOf(int matchedNumber, boolean bonus) {
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,12 +1,17 @@
 package view;
 
 import domain.Lotto;
+import domain.LottoRank;
 import domain.LottoRankMatch;
 import domain.LottoRepository;
+
+import java.util.Map;
 
 public class OutputView {
     private static final String STATISTICS = "당첨 통계";
     private static final String DASH = "-----------";
+    private static final String NUM_OF_MATCHED = "%d개 일치%s(%d원)- %d개";
+    public static final String SECOND_PRICE_ADDITION_STRING = ", 보너스 볼 일치";
 
     public static void printAutoLottos(LottoRepository autuLottos) {
         for (Lotto autoLotto : autuLottos.getLottos()) {
@@ -18,5 +23,10 @@ public class OutputView {
         System.out.println();
         System.out.println(STATISTICS);
         System.out.println(DASH);
+        for (Map.Entry<LottoRank, Integer> resultIndex : lottoRankMatch.getLottoResult().entrySet()) {
+
+            System.out.println(resultIndex.getKey().getCountOfMatch() + resultIndex.getValue() + "개");
+        }
+        System.out.println(String.format("%2f", profitRate));
     }
 }
