11158316ff9547963deea73cb1f7f1638e79a340
flip1945 <coghdus1016@naver.com>
2022-01-06 18:05:10 +0900

    feat: 로또 생성과 결과를 확인하는 클래스의 구조를 변경

--- src/main/java/lotto/service/LottoMachine.java
+++ src/main/java/lotto/service/LottoMachine.java
@@ -1,9 +1,8 @@
 package lotto.service;
 
-import lotto.domain.LottoNumbers;
 import lotto.domain.LottoTicket;
 import lotto.domain.PrizeType;
-import lotto.dto.LottoResultDTO;
+import lotto.domain.LottoResult;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,36 +11,26 @@ import java.util.stream.Stream;
 public class LottoMachine {
 
     public static List<LottoTicket> createAutoLottoTickets(int purchaseCount) {
-        return Stream.generate(LottoMachine::createAutoLottoTicket)
+        return Stream.generate(LottoTicketFactory::createAutoLottoTicket)
                 .limit(purchaseCount)
                 .collect(Collectors.toList());
     }
 
-    private static LottoTicket createAutoLottoTicket() {
-        return new LottoTicket(LottoNumbersFactory.createRandomLottoNumbers());
-    }
-
-    public static List<LottoTicket> createManualLottoTickets(List<LottoNumbers> lottoNumbers) {
-        return lottoNumbers.stream()
-                .map(LottoTicket::new)
-                .collect(Collectors.toList());
-    }
-
-    public static LottoResultDTO getLottoResult(List<LottoTicket> lottoTickets, LottoNumbers winningNumbers, int bonusNumber) {
-        LottoResultDTO lottoResultDTO = new LottoResultDTO();
+    public static LottoResult getLottoResult(List<LottoTicket> lottoTickets, LottoTicket winningNumbers, int bonusNumber) {
+        LottoResult lottoResult = new LottoResult();
         for (LottoTicket lottoTicket : lottoTickets) {
             int matchCount = lottoTicket.getLottoMatchCount(winningNumbers);
-            boolean matchBonus = lottoTicket.hasBonusNumber(bonusNumber);
+            boolean matchBonus = lottoTicket.hasNumber(bonusNumber);
 
-            countLottoResult(lottoResultDTO, matchCount, matchBonus);
+            countLottoResult(lottoResult, matchCount, matchBonus);
         }
-        return lottoResultDTO;
+        return lottoResult;
     }
 
-    private static void countLottoResult(LottoResultDTO lottoResultDTO, int matchCount, boolean matchBonus) {
+    private static void countLottoResult(LottoResult lottoResult, int matchCount, boolean matchBonus) {
         PrizeType prizeType = PrizeType.valueOf(matchCount, matchBonus);
         if (prizeType != null) {
-            prizeType.count(lottoResultDTO);
+            prizeType.count(lottoResult);
         }
     }
 }
