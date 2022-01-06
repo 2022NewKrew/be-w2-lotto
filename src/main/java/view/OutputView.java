d0cbfd4f7a1d23b48b4ade8a18ede272eaac5fd1
Dongchan KIM <fromecha@gmail.com>
2022-01-06 17:48:24 +0900

    [refactor] 출력부 % 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -12,7 +12,7 @@ public class OutputView {
     private static final String DASH = "-----------";
     private static final String NUM_OF_MATCHED = "%d개 일치%s(%d원)- %d개";
     private static final String SECOND_PRICE_ADDITION_STRING = ", 보너스 볼 일치";
-    private static final String TOTAL_PROFIT_RATE = "총 수익률은 %.2f 입니다.";
+    private static final String TOTAL_PROFIT_RATE = "총 수익률은 %.2f%s 입니다.";
 
     public static void printAutoLottos(LottoRepository autoLottos) {
         for (Lotto autoLotto : autoLottos.getLottos()) {
@@ -28,6 +28,6 @@ public class OutputView {
             String haveBonusBall = (resultIndex.getKey().getMoney() == 30_000_000) ? SECOND_PRICE_ADDITION_STRING : " ";
             System.out.println(String.format(NUM_OF_MATCHED, resultIndex.getKey().getCountOfMatch(), haveBonusBall, resultIndex.getKey().getMoney(), resultIndex.getValue()));
         }
-        System.out.println(String.format(TOTAL_PROFIT_RATE, profitRate));
+        System.out.println(String.format(TOTAL_PROFIT_RATE, profitRate, "%"));
     }
 }
