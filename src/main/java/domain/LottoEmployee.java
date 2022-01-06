5b83ae503e4effbc81e807758e22d428447bba90
Dongchan KIM <fromecha@gmail.com>
2022-01-06 17:03:40 +0900

    [validation] 최대 로또 구매 100장 검증

--- src/main/java/domain/LottoEmployee.java
+++ src/main/java/domain/LottoEmployee.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public class LottoEmployee {
     private static final int MINIMUM_PURCHASE_AMOUNT = 1;
+    private static final int MAXIMUM_PURCHASE_AMOUNT = 100;
     private static final int LOTTO_PRICE = 1000;
 
     public int buyLottos(int purchasedAmount) {
@@ -25,6 +26,9 @@ public class LottoEmployee {
     private static void validatePurchaseAmount(int purchasedAmount) {
         if ((purchasedAmount/LOTTO_PRICE) < MINIMUM_PURCHASE_AMOUNT) {
             throw new InvalidPurchaseAmount(InvalidPurchaseAmount.MINIMUM_PURCHASE_AMOUNT);
+        }
+        if ((purchasedAmount / LOTTO_PRICE) > MAXIMUM_PURCHASE_AMOUNT) {
+            throw new InvalidPurchaseAmount(InvalidPurchaseAmount.MAXIMUM_PURCHASE_AMOUNT);
         }
     }
 }
--- src/main/java/exception/InvalidPurchaseAmount.java
+++ src/main/java/exception/InvalidPurchaseAmount.java
@@ -2,6 +2,7 @@ package exception;
 
 public class InvalidPurchaseAmount extends RuntimeException {
     public static final String MINIMUM_PURCHASE_AMOUNT = "최소한 로또 1장 이상을 구매해야합니다.";
+    public static final String MAXIMUM_PURCHASE_AMOUNT = "최대 로또 100장까지만 구매가능합니다.";
 
     public InvalidPurchaseAmount(String message) {
         super(message);
