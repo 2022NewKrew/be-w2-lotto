95b7a4a66e53084b98973ec239a1702fc5a6899c
frank.burger <frank.burger@kakaocorp.com>
2022-01-06 18:30:14 +0900

    refactor: App에서 LotteryWallet사용하도록 함
    App에서 기존 int budget관련 코드 모두 LotteryWallet사용하도록 교체함.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,19 +2,18 @@ import domain.*;
 import domain.util.LotteryNumbersFactory;
 import view.View;
 
+import java.util.stream.IntStream;
+
 public class App {
     private static final int TICKET_PRICE = 1000;
 
     public static void main(String[] args) {
         View view = new View();
 
-        int budget = view.getBudget();
+        LotteryWallet lotteryWallet = new LotteryWallet(view.getBudget());
         int numberOfNonRandomTickets = view.getNumberOfNonRandomTickets();
 
-        budget -= TICKET_PRICE * numberOfNonRandomTickets;
-        if (budget < 0) {
-            throw new IllegalArgumentException();
-        }
+        lotteryWallet.buyTickets(numberOfNonRandomTickets);
 
         LotteryTickets nonRandomLotteryTickets = new LotteryTickets(TICKET_PRICE);
         view.promptToInputLotteryNumbersToBuy();
@@ -23,7 +22,7 @@ public class App {
         }
 
         LotteryTickets randomLotteryTickets = new LotteryTickets(TICKET_PRICE);
-        addRandomTicketsUnderBudget(randomLotteryTickets, budget);
+        addRandomTickets(randomLotteryTickets, lotteryWallet.getNumberOfTicketsAffordable());
 
         view.showBoughtTickets(nonRandomLotteryTickets.toDTO(), randomLotteryTickets.toDTO());
 
@@ -36,11 +35,8 @@ public class App {
         view.showReport(lotteryReport.toDTO());
     }
 
-    private static void addRandomTicketsUnderBudget(LotteryTickets lotteryTickets, int budget) {
+    private static void addRandomTickets(LotteryTickets lotteryTickets, int numberOfTickets) {
         LotteryNumbersFactory lotteryNumbersFactory = new LotteryNumbersFactory();
-        while (budget >= TICKET_PRICE) {
-            lotteryTickets.add(new LotteryTicket(lotteryNumbersFactory.getRandomNumbers()));
-            budget -= TICKET_PRICE;
-        }
+        IntStream.range(0, numberOfTickets).forEach(i -> lotteryTickets.add(new LotteryTicket(lotteryNumbersFactory.getRandomNumbers())));
     }
 }
