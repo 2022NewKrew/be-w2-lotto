506de2d54a45babfa5299c26e35d0ff09fb7f1cc
tjddls1124 <tjddls1124@nate.com>
2022-01-06 10:59:14 +0900

    feature Ranking 변수 타입 변경 및 SecondPrize 추가

--- src/main/java/domain/Ranking.java
+++ src/main/java/domain/Ranking.java
@@ -1,20 +1,21 @@
 package domain;
 
 public enum Ranking implements Comparable<Ranking> {
-    NONE(0,0),
-    THREE(3,5000),
-    FOUR(4,50000),
-    FIVE(5,1500000),
-    SIX(6,2000000000);
-    private final int matchCount;
+    NONE("0",0),
+    THREE("3개 일치",5000),
+    FOUR("4개 일치",50000),
+    FIVE("5개 일치",1500000),
+    SIX_SECOND("5개 일치, 보너스 볼 일치",30000000),
+    SIX("6개 일치",2000000000);
+    private final String matchCount;
     private final int winingPrize;
 
-    Ranking(int matchCount, int winingPrize){
+    Ranking(String matchCount, int winingPrize){
         this.matchCount = matchCount;
         this.winingPrize = winingPrize;
     }
 
-    public int getMatchCount() {
+    public String getMatchCount() {
         return matchCount;
     }
 
