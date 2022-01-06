8ca3d4921f4b62cf7fceea94453cb019c92547e3
Daecheol Shin <60842206+cheol7826@users.noreply.github.com>
2022-01-06 22:10:47 +0900

    로또 Step4 구현 (#323)
    * 로또 Step4 구현
    
    * 결과 출력 로직 변경 및 코드 리팩토링
    
    Co-authored-by: iron.shin <iron.shin@kakaocorp.com>

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,38 @@
-import controller.LottoController;
+import controller.WebLottoController;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
 public class App {
+    private static final WebLottoController controller = new WebLottoController();
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
-        lottoController.startLotto();
+        port(8080);
+
+        get("/", (req, res) -> render(new HashMap<>(), "index.html"));
+
+        post("/buyLotto", ((req, res) -> {
+            Map<String, Object> model = controller.buyLotto(req.queryParams("inputMoney"), req.queryParams("manualNumber"));
+            return render(model, "show.html");
+        }));
+
+        post("/matchLotto", ((req, res) -> {
+            Map<String, Object> model = controller.matchLotto(req.queryParams("winningNumber"), req.queryParams("bonusNumber"));
+            return render(model, "result.html");
+        }));
+
+        exception(RuntimeException.class, ((exception, req, res) ->  {
+            Map<String, Object> model = new HashMap<>();
+            model.put("error", true);
+            model.put("errorInfo", exception.getMessage());
+            res.body(render(model, "index.html"));
+        }));
     }
+
+    public static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
+
 }
---
+++ src/main/java/input/LottoConsoleInput.java
@@ -0,0 +1,50 @@
+package input;
+
+import domain.Lotto;
+import domain.LottoConst;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class LottoConsoleInput extends LottoInput {
+    private final Scanner sc = new Scanner(System.in);
+
+    public int inputPrice() throws IllegalArgumentException{
+        System.out.println("구입 금액을 입력해주세요.");
+        return super.inputPrice(sc.nextLine());
+    }
+
+    public int inputManualCount() throws IllegalArgumentException{
+        System.out.println("수동으로 구매할 로또 수를 입력하세요.");
+        int count = Integer.parseInt(sc.nextLine());
+        ec.checkPositiveNumber(count);
+
+        return count;
+    }
+
+    public List<Lotto> inputManualLotto(int totalPrice){
+        int manualCount = inputManualCount();
+        if(manualCount * LottoConst.ONE_LOTTO_PRICE > totalPrice){
+            throw new IllegalArgumentException("금액을 초과했습니다.");
+        }
+
+        System.out.println("수동으로 구매할 번호를 입력하세요.");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<manualCount; i++){
+            sb.append(sc.nextLine()).append("\n");
+        }
+
+        return super.inputManualLotto(sb.toString(), totalPrice);
+    }
+
+    public Lotto inputResultLotto(){
+        System.out.println("지난 주 당첨 번호를 입력해주세요.");
+        return super.inputResultLotto(sc.nextLine());
+    }
+
+    public int inputResultBonusLotto() throws IllegalArgumentException{
+        System.out.println("보너스 볼을 입력해주세요.");
+        return super.inputResultBonusLotto(sc.nextLine());
+    }
+}
