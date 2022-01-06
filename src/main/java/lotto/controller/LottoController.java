ea9fd638a982b9ed13516ed7d6ac75d7ed22e4de
jb.jang <jb.jang@kakaocorp.com>
2022-01-07 01:09:08 +0900

    refactor: Controller Dto 이름 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,7 @@
 package lotto.controller;
 
 import lotto.dto.LottoCheckRequestDto;
-import lotto.dto.LottoBundleCheckResponseDto;
+import lotto.dto.LottoCheckResponseDto;
 import lotto.dto.LottoPurchaseRequestDto;
 import lotto.dto.LottoPurchaseResponseDto;
 import lotto.service.LottoService;
@@ -29,7 +29,7 @@ public class LottoController {
     /**
      * 확인하고 싶은 내용<당첨번호, 보너스 볼, 복권 리스트>을 입력 받고, 당첨 정보 및 수익률을 계산한 뒤, 응답을 반환.
      */
-    public static LottoBundleCheckResponseDto checkLottoBundle(LottoCheckRequestDto lottoCheckRequestDto) {
+    public static LottoCheckResponseDto checkLottoBundle(LottoCheckRequestDto lottoCheckRequestDto) {
         return LottoService.checkLottoBundle(lottoCheckRequestDto);
     }
 }
