2da81bcbffeb62a271baeb2bc26c56cbd7f453df
anika.dovob <anika.dovob@kakaocorp.com>
2022-01-06 16:12:12 +0900

    feat: inputView에서 수동 로또 정보 입력 받기

--- src/main/java/lotto/view/ConsoleInputView.java
+++ src/main/java/lotto/view/ConsoleInputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ConsoleInputView implements InputView {
     private static final String LOTTO_NUMBER_SPLIT_DELIMITER = ", ";
@@ -9,6 +12,20 @@ public class ConsoleInputView implements InputView {
         System.out.println("구입금액을 입력해 주세요.");
 
         return new Scanner(System.in).nextInt();
+    }
+
+    public int getManualTicketCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+
+        return new Scanner(System.in).nextInt();
+    }
+
+    public List<String[]> getManualLottoNumbers(int manualTicketCount) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+
+        return IntStream.range(0, manualTicketCount)
+                .mapToObj(i -> new Scanner(System.in).nextLine().split(LOTTO_NUMBER_SPLIT_DELIMITER))
+                .collect(Collectors.toList());
     }
 
     public String[] getWinningNumbers() {
