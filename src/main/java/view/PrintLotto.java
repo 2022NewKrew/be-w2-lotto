4bf59601c7f07da52b89f4fe0d21e4c4e9125ef1
pi.raspberry <pi.raspberry@kakaocorp.com>
2022-01-06 23:51:02 +0900

    Extract lambda function for post
    - runServer 함수의 길이가 너무 길어서 post 함수의 lambda 함수를 별도의 함수로 분리함.
    - lottoMachine을 static 변수로 만들어, postBuyLotto에서 시작 시마다 재생성하도록 수정함.

--- src/main/java/view/PrintLotto.java
+++ src/main/java/view/PrintLotto.java
@@ -12,53 +12,54 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 public class PrintLotto {
 
     private static int money = 0;
+    private static LottoMachine lottoMachine = new LottoMachine();
 
     public static void runServer() {
         port(8080);
-
         staticFiles.location("/");
-        LottoMachine lottoMachine = new LottoMachine();
+        post("buyLotto", PrintLotto::postBuyLotto);
+        post("matchLotto", PrintLotto::postMatchLotto);
+    }
 
-        post("buyLotto", (request, response) -> {
-            try {
-                money = Integer.parseInt(request.queryParams("inputMoney"));
-                final List<String> manualNumbers = Arrays.asList(request.queryParams("manualNumber").split("\r?\n"));
-                List<UserLotto> manualLottos = manualNumbers.stream().map(x -> new UserLotto(splitNumbers(x))).collect(Collectors.toList());
+    private static String postBuyLotto(spark.Request request, spark.Response response) {
+        try {
+            money = Integer.parseInt(request.queryParams("inputMoney"));
+            final List<String> manualNumbers = Arrays.asList(request.queryParams("manualNumber").split("\r?\n"));
+            List<UserLotto> manualLottos = manualNumbers.stream().map(x -> new UserLotto(splitNumbers(x))).collect(Collectors.toList());
 
-                lottoMachine.addManualLottos(manualLottos);
-                lottoMachine.buyLotto(money - manualLottos.size() * 1000);
+            lottoMachine = new LottoMachine();
+            lottoMachine.addManualLottos(manualLottos);
+            lottoMachine.buyLotto(money - manualLottos.size() * 1000);
 
-                Map<String, Object> model = new HashMap<>();
-                model.put("lottosSize", money / 1000);
-                model.put("lottos", lottoMachine.getAllLottos().stream().map(x -> Collections.singletonMap("numbers", x.toString())).collect(Collectors.toList()));
-
-                return render(model, "/show.html");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return "";
-        });
-
-        post("matchLotto", (request, response) -> {
-            System.out.println(request.queryParams());
-            WinningLotto winningLotto = new WinningLotto(splitNumbers(request.queryParams("winningNumber")), LottoBall.values()[Integer.parseInt(request.queryParams("bonusNumber"))-1]);
-            RankCount rankCount = lottoMachine.getRankCount(winningLotto);
-
-            Map<String, List<String>> message = Collections.singletonMap("message",
-                    Arrays.asList(
-                            "3개 일치 (5000원)- " + rankCount.getFifthCount() + "개",
-                            "4개 일치 (50000원)- " + rankCount.getFourthCount() + "개",
-                            "5개 일치 (1500000원)- " + rankCount.getThirdCount() + "개",
-                            "5개 일치, 보너스 볼 일치(30000000원)- " + rankCount.getSecondCount() + "개",
-                            "6개 일치 (2000000000원)- " + rankCount.getFirstCount() + "개"
-                    )
-            );
             Map<String, Object> model = new HashMap<>();
-            model.put("lottosResult", message);
-            model.put("totalRateOfReturn", (calcProfit(rankCount) - money)  * 100 / money);
+            model.put("lottosSize", lottoMachine.getAllLottos().size());
+            model.put("lottos", lottoMachine.getAllLottos().stream().map(x -> Collections.singletonMap("numbers", x.toString())).collect(Collectors.toList()));
 
-            return render(model, "/result.html");
-        });
+            return render(model, "/show.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private static String postMatchLotto(spark.Request request, spark.Response response) {
+        WinningLotto winningLotto = new WinningLotto(splitNumbers(request.queryParams("winningNumber")), LottoBall.values()[Integer.parseInt(request.queryParams("bonusNumber"))-1]);
+        RankCount rankCount = lottoMachine.getRankCount(winningLotto);
+
+        Map<String, List<String>> message = Collections.singletonMap("message",
+                Arrays.asList(
+                        "3개 일치 (5000원)- " + rankCount.getFifthCount() + "개",
+                        "4개 일치 (50000원)- " + rankCount.getFourthCount() + "개",
+                        "5개 일치 (1500000원)- " + rankCount.getThirdCount() + "개",
+                        "5개 일치, 보너스 볼 일치(30000000원)- " + rankCount.getSecondCount() + "개",
+                        "6개 일치 (2000000000원)- " + rankCount.getFirstCount() + "개"
+                )
+        );
+        Map<String, Object> model = new HashMap<>();
+        model.put("lottosResult", message);
+        model.put("totalRateOfReturn", (calcProfit(rankCount) - money)  * 100 / money);
+
+        return render(model, "/result.html");
     }
 
     public static String render(Map<String, Object> model, String templatePath) {
