5656a7207e0b210f54e38800dbc96f8b02a44bfa
GoonLee <auteur.kor@gmail.com>
2022-01-07 15:09:15 +0900

    Fix: insufficient validation while creating Money class

--- src/main/java/business/domain/Money.java
+++ src/main/java/business/domain/Money.java
@@ -5,13 +5,15 @@ import java.util.Objects;
 public class Money {
 
     private static final long UNIT_VALUE = 1_000L;
+    private static final long MONEY_MAX = Integer.MAX_VALUE * UNIT_VALUE;
+    private static final long MONEY_MIN = -Integer.MAX_VALUE * UNIT_VALUE;
 
     private final long value;
 
     public Money(long value) {
         if (!isValueValid(value)) {
             throw new IllegalArgumentException(
-                String.format("%d원 단위의 값만 금액으로 사용 가능합니다.", UNIT_VALUE));
+                String.format("%,d원 이상, %,d원 이하, %,d원 단위의 값만 금액으로 사용 가능합니다.", MONEY_MIN, MONEY_MAX, UNIT_VALUE));
         }
 
         this.value = value;
@@ -22,7 +24,7 @@ public class Money {
     }
 
     private boolean isValueValid(long value) {
-        return value % UNIT_VALUE == 0;
+        return value % UNIT_VALUE == 0 && value >= MONEY_MIN && value <= MONEY_MAX;
     }
 
     public Money multiply(int multiplier) {
