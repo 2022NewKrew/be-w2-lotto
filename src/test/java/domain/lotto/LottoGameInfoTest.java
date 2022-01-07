60a2839534ceff6c70e427895398b2e4ee03d961
devwihtpug <devwithpug@kakao.com>
2022-01-07 16:07:18 +0900

    refactor: 테스트 케이스 추가

--- src/test/java/domain/lotto/LottoGameInfoTest.java
+++ src/test/java/domain/lotto/LottoGameInfoTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
+import static util.LottoConst.LOTTO_PRICE;
 
 class LottoGameInfoTest {
 
@@ -20,7 +21,6 @@ class LottoGameInfoTest {
     @MethodSource
     @DisplayName("입력한 금액의 최대 구입할 수 있는 로또 만큼 구입한다.")
     void testForPurchaseQuantityByInputMoney(int money, int expectedQuantity) {
-
         //when
         LottoGameInfo lottoGameInfo = new LottoGameInfo(money, EMPTY_MANUAL_LOTTO_ORDER);
 
@@ -45,5 +45,28 @@ class LottoGameInfoTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new LottoGameInfo(999, EMPTY_MANUAL_LOTTO_ORDER))
                 .withMessage("[에러] 구입 금액은 반드시 1000원 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("입력한 금액이 음수인 경우 예외가 발생한다.")
+    void failToCreateLottoGameInfoWithNegativeNumber() {
+        //when
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoGameInfo(-1, EMPTY_MANUAL_LOTTO_ORDER))
+                .withMessage("[에러] 음수 값을 입력할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("입력한 금액보다 많은 로또를 구매하려하면 예외가 발생한다.")
+    void failToCreateLottoGameInfoWithExceedPurchaseLimit() {
+        //given
+        LottoOrder lottoOrder = new LottoOrder(List.of(1,2,3,4,5,6));
+        List<LottoOrder> exceedLimitOrder = List.of(lottoOrder, lottoOrder);
+        int money = LOTTO_PRICE * 2 - 1;
+
+        //when
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoGameInfo(money, exceedLimitOrder))
+                .withMessage("[에러] 입력한 금액보다 많은 로또를 구매할 수 없습니다.");
     }
 }
