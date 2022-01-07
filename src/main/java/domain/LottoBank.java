c2a6d1ca187eacfe65b03f493133a8ae340df4fb
Dongchan KIM <fromecha@gmail.com>
2022-01-07 14:41:41 +0900

    [validation] 구입금액에 비해 수동로또수 검증

--- src/main/java/domain/LottoBank.java
+++ src/main/java/domain/LottoBank.java
@@ -10,7 +10,7 @@ public class LottoBank {
     private static final int LOTTO_PRICE = 1000;
 
     public int buyLottos(int purchasedAmount, int manualQuantity) {
-        validatePurchaseAmount(purchasedAmount);
+        validatePurchaseAmount(purchasedAmount, manualQuantity);
         return (purchasedAmount / LOTTO_PRICE) - manualQuantity;
     }
 
@@ -22,12 +22,15 @@ public class LottoBank {
         return (profit - purchasedAmount) / (purchasedAmount * 100);
     }
 
-    private static void validatePurchaseAmount(int purchasedAmount) {
+    private static void validatePurchaseAmount(int purchasedAmount, int manualQuantity) {
         if ((purchasedAmount/LOTTO_PRICE) < MINIMUM_PURCHASE_AMOUNT) {
             throw new InvalidPurchaseAmount(InvalidPurchaseAmount.MINIMUM_PURCHASE_AMOUNT);
         }
         if ((purchasedAmount / LOTTO_PRICE) > MAXIMUM_PURCHASE_AMOUNT) {
             throw new InvalidPurchaseAmount(InvalidPurchaseAmount.MAXIMUM_PURCHASE_AMOUNT);
+        }
+        if (manualQuantity > (purchasedAmount / LOTTO_PRICE)) {
+            throw new InvalidPurchaseAmount(InvalidPurchaseAmount.MANUAL_COST_SHOULD_LARGER_THAN_PURCHASE_AMOUNT);
         }
     }
 }
