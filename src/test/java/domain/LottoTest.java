7c2160b66c832d1ebfa7d77aea6ddd55019d530a
Dongchan KIM <fromecha@gmail.com>
2022-01-07 13:23:22 +0900

    [test] 로또 길이 검증

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -1,6 +1,11 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import exception.InvalidLottoLengthException;
+import org.junit.jupiter.api.Assertions;
+
+//import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +25,20 @@ class LottoTest {
         // when & then
         assertThat(lotto.contains(1)).isTrue();
         assertThat(lotto.contains(7)).isFalse();
+    }
+
+    @Test
+    @DisplayName("로또 내에 길이 6개 exception 검증")
+    void invalidLottoLengthException() {
+        // given
+        Set<Integer> numbers = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+
+        // when
+        InvalidLottoLengthException thrown = Assertions.assertThrows(InvalidLottoLengthException.class, () -> {
+            new Lotto(numbers);
+        });
+
+        // then
+        Assertions.assertEquals("Lotto number has wrong length.", thrown.getMessage());
     }
 }
