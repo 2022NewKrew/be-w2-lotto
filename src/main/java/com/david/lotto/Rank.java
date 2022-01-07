18cdf6e24bdc14cc7dff48afb20b24747a70db19
david.dh <david.dh@kakaocorp.com>
2022-01-07 18:58:29 +0900

    db 저장을 위해 등수 정보 추가

--- src/main/java/com/david/lotto/Rank.java
+++ src/main/java/com/david/lotto/Rank.java
@@ -3,16 +3,18 @@ package com.david.lotto;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000);
+    FIRST(1,6, 2000000000),
+    SECOND(2,5, 30000000),
+    THIRD(3,5, 1500000),
+    FOURTH(4,4, 50000),
+    FIFTH(5,3, 5000);
 
+    private final int rank;
     private final int countOfMatch;
     private final int winningMoney;
 
-    Rank(int countOfMatch, int winningMoney) {
+    Rank(int rank, int countOfMatch, int winningMoney) {
+        this.rank = rank;
         this.countOfMatch = countOfMatch;
         this.winningMoney = winningMoney;
     }
@@ -39,5 +41,9 @@ public enum Rank {
 
     public int getWinningMoney() {
         return winningMoney;
+    }
+
+    public int getRank() {
+        return rank;
     }
 }
