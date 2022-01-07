7ba6d13bbe901834508d3a9a1b76443ad95dd9aa
Dongchan KIM <fromecha@gmail.com>
2022-01-07 17:57:37 +0900

    [test] 수동로또 수 테스트

--- src/test/java/domain/LottoBankTest.java
+++ src/test/java/domain/LottoBankTest.java
@@ -29,11 +29,26 @@ class LottoBankTest {
         InvalidPurchaseAmount throw3 = Assertions.assertThrows(InvalidPurchaseAmount.class, () -> {
             lottoBank.buyLottos(money1, 16);
         });
+
         // then
         assertThat(lottoNumber1).isEqualTo(money1 / 1000);
         Assertions.assertEquals("최대 로또 100장까지만 구매가능합니다.", throw1.getMessage());
         Assertions.assertEquals("최소한 로또 1장 이상을 구매해야합니다.", throw2.getMessage());
         Assertions.assertEquals("구입금액보다 수동구매로또수가 많습니다.", throw3.getMessage());
+    }
 
+    @Test
+    @DisplayName("수동 로또 구매 갯수를 확인(최소0장 이상)")
+    void buyManualLottos() {
+        // given
+        LottoBank lottoBank = new LottoBank();
+        int money1 = 15999;
+
+        // when
+        InvalidPurchaseAmount thrown = Assertions.assertThrows(InvalidPurchaseAmount.class, () -> {
+            lottoBank.buyLottos(money1, -1);
+        });
+        // then
+        Assertions.assertEquals("수동 로또 구매수는 0이상만 가능합니다.", thrown.getMessage());
     }
 }
