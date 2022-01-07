547ed4a10641760d21efbf2c764c4ba901f7b515
binary.yun <binary.yun@kakaocorp.com>
2022-01-07 15:59:04 +0900

    test: Purchase 테스트 케이스 추가

--- src/test/java/domain/PurchaseTest.java
+++ src/test/java/domain/PurchaseTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PurchaseTest {
-    private final int ONE_LOTTO_PRICE = 1000;
-
     @DisplayName("구매 가능 수량이 정상적으로 반환되는지 검증")
     @Test
     void isCorrectPurchaseableQuantity() {
@@ -23,8 +23,24 @@ class PurchaseTest {
                 Purchase.purchaseableQuantity(negativeAmount)
         );
 
-        assertThat(purchaseableQuantity).isEqualTo(purchaseAmount / ONE_LOTTO_PRICE);
+        assertThat(purchaseableQuantity).isEqualTo(purchaseAmount / Purchase.ONE_LOTTO_PRICE);
         assertThat(nonPurchaseable)
                 .containsOnly(0);
+    }
+
+    @DisplayName("구매 금액이 올바른지 검증")
+    @Test
+    void isCorrectPurchaseAmount() {
+        assertDoesNotThrow(() -> new Purchase(14000, 4, 10));
+        assertThrows(IllegalArgumentException.class, () -> new Purchase(14500, 4, 10));
+        assertThrows(IllegalArgumentException.class, () -> new Purchase(-14000, 4, 10));
+    }
+
+    @DisplayName("구매 수량이 올바른지 검증")
+    @Test
+    void isCorrectPurchaseQuantity() {
+        assertDoesNotThrow(() -> new Purchase(14000, 4, 10));
+        assertThrows(IllegalArgumentException.class, () -> new Purchase(14000, -4, 10));
+        assertThrows(IllegalArgumentException.class, () -> new Purchase(14000, 4, 4));
     }
 }
