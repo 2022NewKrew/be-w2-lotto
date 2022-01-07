7f93569f0fd9a6561719f475567720c73a657215
Dongchan KIM <fromecha@gmail.com>
2022-01-07 09:56:11 +0900

    [test] lotto list -> set 으로 변경

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -5,16 +5,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 class LottoTest {
 
     @Test
     @DisplayName("로또 내에 넘버 포함 유무 확인")
     void contains() {
         // given
-//        Lotto lotto = new Lotto(Arrays.as(1, 2, 3, 4, 5, 6));
+        Set<Integer> numbers = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto = new Lotto(numbers);
 
         // when & then
-//        assertThat(lotto.contains(1)).isTrue();
-//        assertThat(lotto.contains(7)).isFalse();
+        assertThat(lotto.contains(1)).isTrue();
+        assertThat(lotto.contains(7)).isFalse();
     }
 }
