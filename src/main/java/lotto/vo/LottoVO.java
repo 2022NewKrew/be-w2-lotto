2d4bff0fd36d265ca835dea372e287fef6225a79
WOC <tom558@naver.com>
2022-01-09 16:36:06 +0900

    refactor : change LottoVO to unmodifiableList
    change LottoVO to make it immutable
    
    Signed-off-by: WOC <tom558@naver.com>

--- src/main/java/lotto/vo/LottoVO.java
+++ src/main/java/lotto/vo/LottoVO.java
@@ -3,6 +3,7 @@ package lotto.vo;
 import lotto.domain.Lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoVO {
@@ -15,7 +16,7 @@ public class LottoVO {
     private final List<Integer> numbers;
 
     public LottoVO(Lotto lotto) {
-        numbers = new ArrayList<>(lotto.getNumbers().orElseThrow());
+        numbers = Collections.unmodifiableList(lotto.getNumbers().orElseThrow());
     }
 
     public List<Integer> getNumbers() {
