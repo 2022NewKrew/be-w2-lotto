7ef1850c838857e413d08281b726b5397925a1d9
WOC <tom558@naver.com>
2022-01-09 17:46:36 +0900

    feat : add lotto validate
    Signed-off-by: WOC <tom558@naver.com>

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.configure.LottoConfigure;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -10,10 +12,19 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
+        validate();
     }
 
     public Optional<List<Integer>> getNumbers() {
         return Optional.ofNullable(numbers);
     }
+
+    private void validate(){
+        if (numbers.size() != LottoConfigure.NUMBERS_SIZE) throw new IllegalArgumentException();
+        for (int i = 0; i < LottoConfigure.NUMBERS_SIZE; i++){
+            if (numbers.get(i) < LottoConfigure.MIN_NUMBER || LottoConfigure.MAX_NUMBER < numbers.get(i)) throw new IllegalArgumentException();
+        }
+    }
+
 
 }
