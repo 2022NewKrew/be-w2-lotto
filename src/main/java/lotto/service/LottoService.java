97aedbb1b231fe9d1b0e848ca2049657d8c16b10
WOC <tom558@naver.com>
2022-01-09 17:24:23 +0900

    feat : add manual lotto method in service
    Signed-off-by: WOC <tom558@naver.com>

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -3,6 +3,7 @@ package lotto.service;
 import lotto.domain.Lotto;
 import lotto.domain.generator.LottoAutoGenerator;
 import lotto.domain.generator.LottoGenerator;
+import lotto.domain.generator.LottoManualGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +14,22 @@ public class LottoService {
         return new LottoAutoGenerator();
     }
 
+    private LottoGenerator getLottoGenerator(List<Integer> userNumber) {
+        return new LottoManualGenerator(userNumber);
+    }
+
     public List<Lotto> purchaseLottos(int count){
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < count; i++){
             lottos.add(getLottoGenerator().generateLotto());
+        }
+        return lottos;
+    }
+
+    public List<Lotto> purchaseLottoByUserNumbers(List<List<Integer>> userNumbers){
+        List<Lotto> lottos = new ArrayList<>();
+        for (List<Integer> userNumber : userNumbers){
+            lottos.add(getLottoGenerator(userNumber).generateLotto());
         }
         return lottos;
     }
