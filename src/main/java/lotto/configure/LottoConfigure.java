c9e5d8ab92626347e56dac00b0d072c1fae59855
WOC <tom558@naver.com>
2022-01-09 16:49:59 +0900

    refactor : move static variables from LottoVO to Lotto Configure
    Signed-off-by: WOC <tom558@naver.com>

---
+++ src/main/java/lotto/configure/LottoConfigure.java
@@ -0,0 +1,8 @@
+package lotto.configure;
+
+public class LottoConfigure {
+    public final static int MIN_NUMBER = 1;
+    public final static int MAX_NUMBER = 45;
+    public final static int NUMBERS_SIZE = 6;
+    public final static int LOTTO_PRICE = 1000;
+}
