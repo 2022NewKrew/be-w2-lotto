9cae49e94cb1c602098cc3cd79971c9b7d743cc9
anika.dovob <anika.dovob@kakaocorp.com>
2022-01-10 11:50:49 +0900

    refactor: LottoTicketCount 수정에 따른 변경 사항

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoMachine {
+
     public static final int TICKET_PRICE = 1000;
     private static final LottoNumbersGenerator randomGenerator = new RandomLottoNumberGenerator();
     private static final LottoNumbersGenerator manualGenerator = new ManualLottoNumberGenerator();
@@ -12,14 +13,14 @@ public class LottoMachine {
     private LottoMachine() {
     }
 
-    public static LottoTickets issue(LottoTicketCount ticketCount, List<String[]> manualLottoNumbers) {
-        List<LottoTicket> tickets = IntStream.range(0, ticketCount.getManualTicketCount())
+    public static LottoTickets issue(LottoTicketCount autoTicketCount, LottoTicketCount manualTicketCount, List<String[]> manualLottoNumbers) {
+        List<LottoTicket> tickets = IntStream.range(0, manualTicketCount.getCount())
                 .mapToObj(manualLottoNumbers::get)
                 .map(manualGenerator::generateLottoNumbers)
                 .map(LottoTicket::new)
                 .collect(Collectors.toList());
 
-        IntStream.range(0, ticketCount.getAutoTicketCount())
+        IntStream.range(0, autoTicketCount.getCount())
                 .mapToObj(i -> randomGenerator.generateLottoNumbers(null))
                 .map(LottoTicket::new)
                 .forEach(tickets::add);
--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,22 +1,23 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoMachineTest {
 
     @DisplayName("금액에 맞는 개수의 로또 티켓 발급 테스트")
     @ParameterizedTest
-    @ValueSource(ints = {100, 200, 300, 10000000})
-    void issue(int totalTicketCount) {
-        LottoTicketCount ticketCount = new LottoTicketCount(totalTicketCount, 0);
-        LottoTickets lottoTickets = LottoMachine.issue(ticketCount, new ArrayList<>());
+    @CsvSource({"10", "100", "10000"})
+    void issue(int autoCount) {
+        LottoTicketCount autoTicketCount = new LottoTicketCount(autoCount);
+        LottoTicketCount manualTicketCount = new LottoTicketCount(0);
 
-        assertThat(lottoTickets.getNumberOfTickets()).isEqualTo(totalTicketCount);
+        LottoTickets lottoTickets = LottoMachine.issue(autoTicketCount, manualTicketCount, new ArrayList<>());
+
+        assertThat(lottoTickets.getNumberOfTickets()).isEqualTo(autoCount);
     }
 }
