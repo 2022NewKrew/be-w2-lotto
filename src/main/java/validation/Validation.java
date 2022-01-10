0a1088883e906154be33eb599fa8fd0502c76052
mimseong <parkseongmin98@gmail.com>
2022-01-10 11:43:42 +0900

    [refactor] Supplier 타입 추가

--- src/main/java/validation/Validation.java
+++ src/main/java/validation/Validation.java
@@ -6,31 +6,31 @@ import java.util.function.Supplier;
 
 public class Validation {
 
-    public static void notLessThanInt(int object, int another, Supplier supplier) {
+    public static void notLessThanInt(int object, int another, Supplier<RuntimeException> supplier) {
         condition(Integer.compare(object, another) >= 0, supplier);
     }
 
-    public static void notMoreThanInt(int object, int another, Supplier supplier) {
+    public static void notMoreThanInt(int object, int another, Supplier<RuntimeException> supplier) {
         condition(Integer.compare(object, another) <= 0, supplier);
     }
 
-    public static void notLessThanLong(long object, long another, Supplier supplier) {
+    public static void notLessThanLong(long object, long another, Supplier<RuntimeException> supplier) {
         condition(Long.compare(object, another) >= 0, supplier);
     }
 
-    public static void notMoreThanLong(long object, long another, Supplier supplier) {
+    public static void notMoreThanLong(long object, long another, Supplier<RuntimeException> supplier) {
         condition(Long.compare(object, another) <= 0, supplier);
     }
 
-    public static <T> void sizeShouldBe(Set<T> object, int length, Supplier supplier) {
+    public static <T> void sizeShouldBe(Set<T> object, int length, Supplier<RuntimeException> supplier) {
         condition(object.size() == length, supplier);
     }
 
-    public static <T> void notContains(Set<T> object, T another, Supplier supplier) {
+    public static <T> void notContains(Set<T> object, T another, Supplier<RuntimeException> supplier) {
         condition(!object.contains(another), supplier);
     }
 
-    private static void condition(boolean predicate, Supplier supplier) {
+    private static void condition(boolean predicate, Supplier<RuntimeException> supplier) {
         Optional.ofNullable(predicate ? predicate : null).orElseThrow(supplier);
     }
 }
