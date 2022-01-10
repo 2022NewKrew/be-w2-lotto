d2367664422da91baa52ec9a980741828242a5af
anika.dovob <anika.dovob@kakaocorp.com>
2022-01-10 11:48:57 +0900

    refactor: LottoTicketCount에 수동 로또 개수, 자동 로또 개수 멤버 제거

--- src/test/java/lotto/domain/LottoTicketCountTest.java
+++ src/test/java/lotto/domain/LottoTicketCountTest.java
@@ -1,44 +1,56 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-
 class LottoTicketCountTest {
 
-    @DisplayName("자동 로또 개수 테스트")
+    @DisplayName("로또 개수 테스트")
     @ParameterizedTest
     @CsvSource({"1000, 1", "14000, 10", "2000000, 100"})
-    public void autoTicketCountTest(long price, int manualTicketCount) {
+    public void ticketCountTest(long price, int manualTicketCount) {
         Money inputMoney = new Money(price);
         int totalTicketCount = inputMoney.getNumberOfTickets(LottoMachine.TICKET_PRICE);
 
-        LottoTicketCount lottoTicketCount = new LottoTicketCount(totalTicketCount, manualTicketCount);
+        LottoTicketCount lottoTicketCount = new LottoTicketCount(totalTicketCount);
 
-        assertThat(lottoTicketCount.getAutoTicketCount()).isEqualTo(totalTicketCount - manualTicketCount);
+        assertThat(lottoTicketCount.getCount()).isEqualTo(totalTicketCount);
     }
 
     @DisplayName("로또 수 음수 입력 테스트")
     @ParameterizedTest
-    @CsvSource({"-1, 0", "0, -1", "-1, -1"})
-    public void negativeManualTicketCount(int totalTicketCount, int manualTicketCount) {
+    @CsvSource({"-1", "-10", "-100"})
+    public void negativeTicketCount(int count) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new LottoTicketCount(totalTicketCount, manualTicketCount))
+                .isThrownBy(() -> new LottoTicketCount(count))
                 .withMessage("구매할 로또 수는 0개 이상이어야 합니다.");
     }
 
-    @DisplayName("수동 로또 개수가 전체 로또 개수를 초과할 경우 테스트")
+    @DisplayName("로또 수 뺄셈 테스트")
     @ParameterizedTest
-    @CsvSource({"1000, 2", "14000, 100", "2000000, 2001"})
-    public void exceedManualTicketCount(long price, int manualTicketCount) {
-        Money inputMoney = new Money(price);
-        int totalTicketCount = inputMoney.getNumberOfTickets(LottoMachine.TICKET_PRICE);
+    @CsvSource({"10, 2", "100, 50", "10000, 2839"})
+    public void subTest(int totalCount, int manualCount) {
+        LottoTicketCount totalTicketCount = new LottoTicketCount(totalCount);
+        LottoTicketCount manualTicketCount = new LottoTicketCount(manualCount);
+
+        LottoTicketCount autoTicketCount = totalTicketCount.sub(manualTicketCount);
+
+        assertThat(autoTicketCount.getCount()).isEqualTo(totalCount - manualCount);
+    }
+
+    @DisplayName("로또 수를 뺄셈 하면 음수일 때 테스트")
+    @ParameterizedTest
+    @CsvSource({"10, 11", "100, 120", "10000, 1000000"})
+    public void subNegativeTest(int totalCount, int manualCount) {
+        LottoTicketCount totalTicketCount = new LottoTicketCount(totalCount);
+        LottoTicketCount manualTicketCount = new LottoTicketCount(manualCount);
 
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new LottoTicketCount(totalTicketCount, manualTicketCount))
-                .withMessage("구매할 수 있는 로또 수를 초과했습니다.");
+                .isThrownBy(() -> totalTicketCount.sub(manualTicketCount))
+                .withMessage("구매할 로또 수는 0개 이상이어야 합니다.");
     }
 }
